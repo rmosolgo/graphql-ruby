36d9d33350a910ff6989441e6f24c1d941c48c8b
Robert Mosolgo <rdmosolgo@gmail.com>
2015-07-14 23:03:48 -0700

    refactor(field-selections) less duplication

--- lib/graph_ql/static_validation/fields_have_appropriate_selections.rb
+++ lib/graph_ql/static_validation/fields_have_appropriate_selections.rb
@@ -14,12 +14,18 @@ class GraphQL::StaticValidation::FieldsHaveAppropriateSelections
 
   def validate_field_selections(ast_field, field_defn, errors)
     resolved_type = field_defn.type.kind.unwrap(field_defn.type)
+
     if resolved_type.kind.scalar? && ast_field.selections.any?
-      errors << message("Selections can't be made on scalars (field '#{ast_field.name}' returns #{resolved_type.name} but has selections [#{ast_field.selections.map(&:name).join(", ")}])", ast_field)
-      return GraphQL::Visitor::SKIP
+      error = message("Selections can't be made on scalars (field '#{ast_field.name}' returns #{resolved_type.name} but has selections [#{ast_field.selections.map(&:name).join(", ")}])", ast_field)
     elsif resolved_type.kind.object? && ast_field.selections.none?
-      errors << message("Objects must have selections (field '#{ast_field.name}' returns #{resolved_type.name} but has no selections)", ast_field)
-      return GraphQL::Visitor::SKIP
+      error = message("Objects must have selections (field '#{ast_field.name}' returns #{resolved_type.name} but has no selections)", ast_field)
+    else
+      error = nil
+    end
+
+    if !error.nil?
+      errors << error
+      GraphQL::Visitor::SKIP
     end
   end
 end
