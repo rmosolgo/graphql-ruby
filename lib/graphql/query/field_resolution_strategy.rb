bdef540d44e2d4efe104b7bba353aafaaf796e14
Robert Mosolgo <rdmosolgo@gmail.com>
2015-08-23 14:22:50 -0700

    refactor(FieldResolutionStrategy) break up logic into relevant methods

--- lib/graphql/query/field_resolution_strategy.rb
+++ lib/graphql/query/field_resolution_strategy.rb
@@ -1,27 +1,37 @@
 class GraphQL::Query::FieldResolutionStrategy
-  attr_reader :result, :result_value
+  attr_reader :field, :arguments, :ast_field, :query, :target, :result_name, :parent_type, :target
 
   def initialize(ast_field, parent_type, target, query)
-    field_name = ast_field.name
-    field = query.schema.get_field(parent_type, field_name) || raise("No field found on #{parent_type.name} '#{parent_type}' for '#{field_name}'")
-    arguments = GraphQL::Query::Arguments.new(ast_field.arguments, field.arguments, query.variables)
-    value = field.resolve(target, arguments, query.context)
-    if value.nil?
-      @result_value = value
-    else
-      if value == GraphQL::Query::DEFAULT_RESOLVE
-        begin
-          value = target.send(field_name)
-        rescue NoMethodError => err
-          raise("Couldn't resolve field '#{field_name}' to #{target.class} '#{target}' (resulted in #{err})")
-        end
-      end
-      resolved_type = field.type.kind.resolve(field.type, value)
-      strategy_class = GraphQL::Query::ValueResolution.get_strategy_for_kind(resolved_type.kind)
-      result_strategy = strategy_class.new(value, resolved_type, target, parent_type, ast_field, query)
-      @result_value = result_strategy.resolve
-    end
+    @ast_field = ast_field
+    @parent_type = parent_type
+    @target = target
+    @query = query
+    @field = query.schema.get_field(parent_type, ast_field.name) || raise("No field found on #{parent_type.name} '#{parent_type}' for '#{ast_field.name}'")
+    @arguments = GraphQL::Query::Arguments.new(ast_field.arguments, field.arguments, query.variables)
+  end
+
+  def result
     result_name = ast_field.alias || ast_field.name
-    @result = { result_name => @result_value}
+    { result_name => result_value}
+  end
+
+  private
+
+  def result_value
+    value = field.resolve(target, arguments, query.context)
+    return nil if value.nil?
+
+    if value == GraphQL::Query::DEFAULT_RESOLVE
+      begin
+        value = target.public_send(ast_field.name)
+      rescue NoMethodError => err
+        raise("Couldn't resolve field '#{ast_field.name}' to #{target.class} '#{target}' (resulted in #{err})")
+      end
+    end
+
+    resolved_type = field.type.kind.resolve(field.type, value)
+    strategy_class = GraphQL::Query::ValueResolution.get_strategy_for_kind(resolved_type.kind)
+    result_strategy = strategy_class.new(value, resolved_type, target, parent_type, ast_field, query)
+    @result_value = result_strategy.resolve
   end
 end
