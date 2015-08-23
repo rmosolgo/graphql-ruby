92bc6fe0c89244b208228e6dd37b5b83428c9ed7
Robert Mosolgo <rdmosolgo@gmail.com>
2015-08-23 14:31:03 -0700

    refactor(Query) split some resolution logic into relevant methods

--- lib/graphql/query/selection_resolver.rb
+++ lib/graphql/query/selection_resolver.rb
@@ -1,5 +1,5 @@
 class GraphQL::Query::SelectionResolver
-  attr_reader :result
+  attr_reader :target, :type, :selections, :query
 
   RESOLUTION_STRATEGIES = {
     GraphQL::Language::Nodes::Field =>          GraphQL::Query::FieldResolutionStrategy,
@@ -8,13 +8,27 @@ class GraphQL::Query::SelectionResolver
   }
 
   def initialize(target, type, selections, query)
-    @result = selections.reduce({}) do |memo, ast_field|
-      chain = GraphQL::Query::DirectiveChain.new(ast_field, query) {
-        strategy_class = RESOLUTION_STRATEGIES[ast_field.class]
-        strategy = strategy_class.new(ast_field, type, target, query)
-        strategy.result
-      }
-      memo.merge(chain.result)
+    @target = target
+    @type = type
+    @selections = selections
+    @query = query
+  end
+
+  def result
+    selections.reduce({}) do |memo, ast_field|
+      field_value = resolve_field(ast_field)
+      memo.merge(field_value)
     end
+  end
+
+  private
+
+  def resolve_field(ast_field)
+    chain = GraphQL::Query::DirectiveChain.new(ast_field, query) {
+      strategy_class = RESOLUTION_STRATEGIES[ast_field.class]
+      strategy = strategy_class.new(ast_field, type, target, query)
+      strategy.result
+    }
+    chain.result
   end
 end
