a8954620d121605da0e88269ba538f2679668ee7
Samuel Gwilym <sam@comique.co>
2015-09-15 20:06:27 +0200

    #edge_type

--- lib/graphql/relay/monkey_patches/base_type.rb
+++ lib/graphql/relay/monkey_patches/base_type.rb
@@ -2,4 +2,8 @@ class GraphQL::BaseType
   def connection_type
     @connection_type ||= GraphQL::Relay::BaseConnection.create_type(self)
   end
+
+  def edge_type
+    @edge_type ||= GraphQL::Relay::Edge.create_type(self)
+  end
 end
