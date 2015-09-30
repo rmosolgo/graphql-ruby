0b5f05fea51b7da3d41434c2883a2d6f7b58dbd3
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2015-09-30 16:57:34 -0400

    Replace print_type case statement with strategy pattern.

--- lib/graphql/schema/printer.rb
+++ lib/graphql/schema/printer.rb
@@ -42,90 +42,104 @@ module GraphQL
     end
 
     def print_type(type)
-      case type
-      when ScalarType
-        print_scalar(type)
-      when ObjectType
-        print_object(type)
-      when InterfaceType
-        print_interface(type)
-      when UnionType
-        print_union(type)
-      when EnumType
-        print_enum(type)
-      when InputObjectType
-        print_input_object(type)
-      else
-        raise NotImplementedError, "Unexpected type #{type.class}"
+      TypeKindPrinters::STRATEGIES.fetch(type.kind).print(type)
+    end
+
+    module TypeKindPrinters
+      module FieldPrinter
+        def print_fields(type)
+          type.fields.values.map{ |field| "  #{field.name}#{print_args(field)}: #{field.type}" }.join("\n")
+        end
+
+        def print_args(field)
+          return if field.arguments.empty?
+          "(#{field.arguments.values.map{ |arg| print_input_value(arg) }.join(", ")})"
+        end
+
+        def print_input_value(arg)
+          default_string = " = #{print_value(arg.default_value, arg.type)}" unless arg.default_value.nil?
+          "#{arg.name}: #{arg.type.to_s}#{default_string}"
+        end
+
+        def print_value(value, type)
+          case type
+          when FLOAT_TYPE
+            value.to_f.inspect
+          when INT_TYPE
+            value.to_i.inspect
+          when BOOLEAN_TYPE
+            (!!value).inspect
+          when ScalarType, ID_TYPE, STRING_TYPE
+            value.to_s.inspect
+          when EnumType
+            value.to_s
+          when InputObjectType
+            fields = value.to_h.map{ |field_name, field_value|
+              field_type = type.input_fields.fetch(field_name.to_s).type
+              "#{field_name}: #{print_value(field_value, field_type)}"
+            }.join(", ")
+            "{ #{fields} }"
+          when NonNullType
+            print_value(value, type.of_type)
+          when ListType
+            "[#{value.to_a.map{ |v| print_value(v, type.of_type) }.join(", ")}]"
+          else
+            raise NotImplementedError, "Unexpected value type #{type.inspect}"
+          end
+        end
       end
-    end
 
-    def print_scalar(type)
-      "scalar #{type.name}"
-    end
-
-    def print_object(type)
-      implementations = " implements #{type.interfaces.map(&:to_s).join(", ")}" unless type.interfaces.empty?
-      "type #{type.name}#{implementations} {\n#{print_fields(type)}\n}"
-    end
-
-    def print_interface(type)
-      "interface #{type.name} {\n#{print_fields(type)}\n}"
-    end
-
-    def print_union(type)
-      "union #{type.name} = #{type.possible_types.map(&:to_s).join(" | ")}\n}"
-    end
-
-    def print_enum(type)
-      values = type.values.values.map{ |v| "  #{v.name}" }.join("\n")
-      "enum #{type.name} {\n#{values}\n}"
-    end
-
-    def print_input_object(type)
-      fields = type.input_fields.values.map{ |field| "  #{print_input_value(field)}" }.join("\n")
-      "input #{type.name} {\n#{fields}\n}"
-    end
-
-    def print_fields(type)
-      type.fields.values.map{ |field| "  #{field.name}#{print_args(field)}: #{field.type}" }.join("\n")
-    end
-
-    def print_args(field)
-      return if field.arguments.empty?
-      "(#{field.arguments.values.map{ |arg| print_input_value(arg) }.join(", ")})"
-    end
-
-    def print_input_value(arg)
-      default_string = " = #{print_value(arg.default_value, arg.type)}" unless arg.default_value.nil?
-      "#{arg.name}: #{arg.type.to_s}#{default_string}"
-    end
-
-    def print_value(value, type)
-      case type
-      when FLOAT_TYPE
-        value.to_f.inspect
-      when INT_TYPE
-        value.to_i.inspect
-      when BOOLEAN_TYPE
-        (!!value).inspect
-      when ScalarType, ID_TYPE, STRING_TYPE
-        value.to_s.inspect
-      when EnumType
-        value.to_s
-      when InputObjectType
-        fields = value.to_h.map{ |field_name, field_value|
-          field_type = type.input_fields.fetch(field_name.to_s).type
-          "#{field_name}: #{print_value(field_value, field_type)}"
-        }.join(", ")
-        "{ #{fields} }"
-      when NonNullType
-        print_value(value, type.of_type)
-      when ListType
-        "[#{value.to_a.map{ |v| print_value(v, type.of_type) }.join(", ")}]"
-      else
-        raise NotImplementedError, "Unexpected value type #{type.inspect}"
+      class ScalarPrinter
+        def self.print(type)
+          "scalar #{type.name}"
+        end
       end
+
+      class ObjectPrinter
+        extend FieldPrinter
+        def self.print(type)
+          implementations = " implements #{type.interfaces.map(&:to_s).join(", ")}" unless type.interfaces.empty?
+          "type #{type.name}#{implementations} {\n#{print_fields(type)}\n}"
+        end
+      end
+
+      class InterfacePrinter
+        extend FieldPrinter
+        def self.print(type)
+          "interface #{type.name} {\n#{print_fields(type)}\n}"
+        end
+      end
+
+      class UnionPrinter
+        def self.print(type)
+          "union #{type.name} = #{type.possible_types.map(&:to_s).join(" | ")}\n}"
+        end
+      end
+
+      class EnumPrinter
+        def self.print(type)
+          values = type.values.values.map{ |v| "  #{v.name}" }.join("\n")
+          "enum #{type.name} {\n#{values}\n}"
+        end
+      end
+
+      class InputObjectPrinter
+        extend FieldPrinter
+        def self.print(type)
+          fields = type.input_fields.values.map{ |field| "  #{print_input_value(field)}" }.join("\n")
+          "input #{type.name} {\n#{fields}\n}"
+        end
+      end
+
+      STRATEGIES = {
+        GraphQL::TypeKinds::SCALAR =>       ScalarPrinter,
+        GraphQL::TypeKinds::OBJECT =>       ObjectPrinter,
+        GraphQL::TypeKinds::INTERFACE =>    InterfacePrinter,
+        GraphQL::TypeKinds::UNION =>        UnionPrinter,
+        GraphQL::TypeKinds::ENUM =>         EnumPrinter,
+        GraphQL::TypeKinds::INPUT_OBJECT => InputObjectPrinter,
+      }
     end
+    private_constant :TypeKindPrinters
   end
 end
