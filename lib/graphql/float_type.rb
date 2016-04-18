949f50258f271322383ec57bd3c1d0f3ced0f2b0
Evan Huus <eapache@gmail.com>
2016-04-18 09:06:47 -0400

    Pass through nils on all the other scalar types

--- lib/graphql/float_type.rb
+++ lib/graphql/float_type.rb
@@ -1,5 +1,5 @@
 GraphQL::FLOAT_TYPE = GraphQL::ScalarType.define do
   name "Float"
   coerce_input -> (value) { value.is_a?(Numeric) ? value.to_f : nil }
-  coerce_result -> (value) { value.to_f }
+  coerce_result -> (value) { value.nil? ? nil : value.to_f }
 end
