0e5f1792b9c6ceda4998cf5252cc316da9a2e1c6
Keith Pitt <me@keithpitt.com>
2016-06-08 10:29:31 +1000

    Allow `clientMutationId` on Mutations to be null as per the spec.
    > https://github.com/rmosolgo/graphql-relay-ruby/issues/45
    > That argument must be a String. That argument may be non‐null.

--- spec/graphql/relay/mutation_spec.rb
+++ spec/graphql/relay/mutation_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe GraphQL::Relay::Mutation do
   let(:query_string) {%|
-    mutation addBagel {
-      introduceShip(input: {shipName: "Bagel", factionId: "1", clientMutationId: "1234"}) {
+    mutation addBagel($clientMutationId: String) {
+      introduceShip(input: {shipName: "Bagel", factionId: "1", clientMutationId: $clientMutationId}) {
         clientMutationId
         ship { name, id }
         faction { name }
@@ -18,8 +18,13 @@ describe GraphQL::Relay::Mutation do
     }
   |}
 
+  after do
+    STAR_WARS_DATA["Ship"].delete("9")
+    STAR_WARS_DATA["Faction"]["1"]["ships"].delete("9")
+  end
+
   it "returns the result & clientMutationId" do
-    result = query(query_string)
+    result = query(query_string, "clientMutationId" => "1234")
     expected = {"data" => {
       "introduceShip" => {
         "clientMutationId" => "1234",
@@ -31,8 +36,20 @@ describe GraphQL::Relay::Mutation do
       }
     }}
     assert_equal(expected, result)
-    # Cleanup:
-    STAR_WARS_DATA["Ship"].delete("9")
-    STAR_WARS_DATA["Faction"]["1"]["ships"].delete("9")
+  end
+
+  it "doesn't require a clientMutationId to perform mutations" do
+    result = query(query_string)
+    expected = {"data" => {
+      "introduceShip" => {
+        "clientMutationId" => nil,
+        "ship" => {
+          "name" => "Bagel",
+          "id" => NodeIdentification.to_global_id("Ship", "9"),
+        },
+        "faction" => {"name" => STAR_WARS_DATA["Faction"]["1"].name }
+      }
+    }}
+    assert_equal(expected, result)
   end
 end
