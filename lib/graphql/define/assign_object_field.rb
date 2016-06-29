abaea05717e296254dce1dc22e7ba66c8446053b
Robert Mosolgo <rdmosolgo@gmail.com>
2016-06-28 21:08:19 -0400

    Support #to_proc-style field definitions

--- lib/graphql/define/assign_object_field.rb
+++ lib/graphql/define/assign_object_field.rb
@@ -2,7 +2,7 @@ module GraphQL
   module Define
     # Turn field configs into a {GraphQL::Field} and attach it to a {GraphQL::ObjectType} or {GraphQL::InterfaceType}
     module AssignObjectField
-      def self.call(fields_type, name, type = nil, desc = nil, field: nil, deprecation_reason: nil, property: nil, &block)
+      def self.call(fields_type, name = nil, type = nil, desc = nil, field: nil, deprecation_reason: nil, property: nil, &block)
         if block_given?
           field = GraphQL::Field.define(&block)
         else
@@ -13,7 +13,7 @@ module GraphQL
         property && field.property = property
         deprecation_reason && field.deprecation_reason = deprecation_reason
         field.name ||= name.to_s
-        fields_type.fields[name.to_s] = field
+        fields_type.fields[field.name.to_s] = field
       end
     end
   end
