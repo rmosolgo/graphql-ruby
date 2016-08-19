466aa66cc34ecbc495cbf94d495572f5e7b66d00
Robert Mosolgo <rdmosolgo@gmail.com>
2016-08-19 16:50:10 -0400

    Add some test cases

--- spec/graphql/execution/typecast_spec.rb
+++ spec/graphql/execution/typecast_spec.rb
@@ -1,32 +1,49 @@
 require "spec_helper"
 
 describe GraphQL::Execution::Typecast do
+  let(:milk_value) { MILKS[1] }
+  let(:cheese_value) { CHEESES[1] }
 
   let(:schema) { DummySchema }
   let(:context) { GraphQL::Query::Context.new(query: OpenStruct.new(schema: schema), values: nil) }
 
+  def compatible?(*args)
+     GraphQL::Execution::Typecast.compatible?(*args)
+   end
   it "resolves correctly when both types are the same" do
-    assert GraphQL::Execution::Typecast.compatible?(MILKS[1], MilkType, MilkType, context)
+    assert compatible?(milk_value, MilkType, MilkType, context)
+    assert !compatible?(milk_value, MilkType, CheeseType, context)
   end
 
-  it "resolves correcty when current type is UnionType and value resolves to potential type" do
-    assert GraphQL::Execution::Typecast.compatible?(MILKS[1], DairyProductUnion, MilkType, context)
+  it "resolves a union type to a matching member" do
+    assert compatible?(milk_value, DairyProductUnion, MilkType, context)
+    assert compatible?(cheese_value, DairyProductUnion, CheeseType, context)
+
+    assert !compatible?(cheese_value, DairyProductUnion, MilkType, context)
+    assert !compatible?(nil, DairyProductUnion, MilkType, context)
   end
 
   it "resolves correcty when potential type is UnionType and current type is a member of that union" do
-    assert GraphQL::Execution::Typecast.compatible?(MILKS[1], MilkType, DairyProductUnion, context)
+    assert compatible?(milk_value, MilkType, DairyProductUnion, context)
+    assert compatible?(cheese_value, CheeseType, DairyProductUnion, context)
+
+    # assert !compatible?(nil, CheeseType, DairyProductUnion, context)
+    # assert !compatible?(cheese_value, MilkType, DairyProductUnion, context)
   end
 
-  it "resolve correctly when current type is an Interface and it resolves to potential type" do
-    assert GraphQL::Execution::Typecast.compatible?(MILKS[1], CheeseType, EdibleInterface, context)
+  it "resolves an object type to one of its interfaces" do
+    assert compatible?(cheese_value, CheeseType, EdibleInterface, context)
+    assert compatible?(milk_value, MilkType, EdibleInterface, context)
+
+    # assert !compatible?(nil, MilkType, EdibleInterface, context)
+    # assert !compatible?(milk_value, CheeseType, EdibleInterface, context)
   end
 
-  it "resolve correctly when potential type is an Interface and current type implements it" do
-    assert GraphQL::Execution::Typecast.compatible?(CHEESES[1], EdibleInterface, CheeseType, context)
-  end
+  it "resolves an interface to a matching member" do
+    assert compatible?(cheese_value, EdibleInterface, CheeseType, context)
+    assert compatible?(milk_value, EdibleInterface, MilkType, context)
 
-  it "resolve correctly when potential type is an Interface and current type implements it" do
-    assert GraphQL::Execution::Typecast.compatible?(MILKS[1], EdibleInterface, CheeseType, context)
+    assert !compatible?(nil, EdibleInterface, MilkType, context)
+    assert !compatible?(cheese_value, EdibleInterface, MilkType, context)
   end
-
 end
