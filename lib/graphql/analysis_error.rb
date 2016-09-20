1021a86b2aba6c2f9c51501a35054da911be5b99
Garen Torikian <gjtorikian@gmail.com>
2016-09-19 19:44:47 -0700

    Not sure what this deal is

--- lib/graphql/analysis_error.rb
+++ lib/graphql/analysis_error.rb
@@ -2,7 +2,10 @@ module GraphQL
   class AnalysisError < GraphQL::ExecutionError
     def initialize(message, ast_node = nil)
       err = super(message)
-      err.ast_node = ast_node
+      # resolves an issue in rbx
+      unless err.nil?
+        err.ast_node = ast_node
+      end
       err
     end
   end
