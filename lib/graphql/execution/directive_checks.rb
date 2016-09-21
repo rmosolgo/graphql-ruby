a1872817323fbc8f8d9defdc77d2f3a90fe3fde0
jden <jden@zendesk.com>
2016-09-21 09:15:54 -0700

    Update @skip and @include directive specs

--- lib/graphql/execution/directive_checks.rb
+++ lib/graphql/execution/directive_checks.rb
@@ -11,21 +11,22 @@ module GraphQL
       # This covers `@include(if:)` & `@skip(if:)`
       # @return [Boolean] Should this node be skipped altogether?
       def skip?(irep_node, query)
-        irep_node.directives.each do |directive_node|
-          if directive_node.name == SKIP || directive_node.name == INCLUDE
-            directive_defn = directive_node.definitions.first
-            args = query.arguments_for(directive_node, directive_defn)
-            if !directive_defn.include?(args)
-              return true
-            end
-          end
-        end
-        false
+        !include?(irep_node, query)
       end
 
       # @return [Boolean] Should this node be included in the query?
       def include?(irep_node, query)
-        !skip?(irep_node, query)
+        irep_node.directives.each do |directive|
+          name = directive.name
+          return false if name == SKIP && args(directive, query)['if'] == true
+          return false if name == INCLUDE && args(directive, query)['if'] == false
+        end
+        true
+      end
+
+      def args(directive_node, query)
+        directive_defn = directive_node.definitions.first
+        query.arguments_for(directive_node, directive_defn)
       end
     end
   end
