d277b7c9767a59df5eedd4580feda3e051cccf41
Christian Joudrey <cmallette@gmail.com>
2016-09-29 16:56:53 -0400

    Refactor to remove KIND_NAMES

--- lib/graphql/introspection/type_kind_enum.rb
+++ lib/graphql/introspection/type_kind_enum.rb
@@ -1,7 +1,7 @@
 GraphQL::Introspection::TypeKindEnum = GraphQL::EnumType.define do
   name "__TypeKind"
   description "An enum describing what kind of type a given `__Type` is."
-  GraphQL::TypeKinds::KIND_NAMES.each do |kind_name|
-    value(kind_name, GraphQL::TypeKinds::TYPE_KIND_DESCRIPTIONS[kind_name.to_sym])
+  GraphQL::TypeKinds::TYPE_KINDS.each do |type_kind|
+    value(type_kind.name, type_kind.description)
   end
 end
--- lib/graphql/type_kinds.rb
+++ lib/graphql/type_kinds.rb
@@ -3,14 +3,15 @@ module GraphQL
   module TypeKinds
     # These objects are singletons, eg `GraphQL::TypeKinds::UNION`, `GraphQL::TypeKinds::SCALAR`.
     class TypeKind
-      attr_reader :name
-      def initialize(name, resolves: false, fields: false, wraps: false, input: false)
+      attr_reader :name, :description
+      def initialize(name, resolves: false, fields: false, wraps: false, input: false, description: nil)
         @name = name
         @resolves = resolves
         @fields = fields
         @wraps = wraps
         @input = input
         @composite = fields? || resolves?
+        @description = description
       end
 
       # Does this TypeKind have multiple possible implementors?
@@ -27,30 +28,18 @@ module GraphQL
     end
 
     TYPE_KINDS = [
-      SCALAR =        TypeKind.new("SCALAR", input: true),
-      OBJECT =        TypeKind.new("OBJECT", fields: true),
-      INTERFACE =     TypeKind.new("INTERFACE", resolves: true, fields: true),
-      UNION =         TypeKind.new("UNION", resolves: true),
-      ENUM =          TypeKind.new("ENUM", input: true),
-      INPUT_OBJECT =  TypeKind.new("INPUT_OBJECT", input: true),
-      LIST =          TypeKind.new("LIST", wraps: true),
-      NON_NULL =      TypeKind.new("NON_NULL", wraps: true),
+      SCALAR =        TypeKind.new("SCALAR", input: true, description: 'Indicates this type is a scalar.'),
+      OBJECT =        TypeKind.new("OBJECT", fields: true, description: 'Indicates this type is an object. `fields` and `interfaces` are valid fields.'),
+      INTERFACE =     TypeKind.new("INTERFACE", resolves: true, fields: true, description: 'Indicates this type is an interface. `fields` and `possibleTypes` are valid fields.'),
+      UNION =         TypeKind.new("UNION", resolves: true, description: 'Indicates this type is a union. `possibleTypes` is a valid field.'),
+      ENUM =          TypeKind.new("ENUM", input: true, description: 'Indicates this type is an enum. `enumValues` is a valid field.'),
+      INPUT_OBJECT =  TypeKind.new("INPUT_OBJECT", input: true, description: 'Indicates this type is an input object. `inputFields` is a valid field.'),
+      LIST =          TypeKind.new("LIST", wraps: true, description: 'Indicates this type is a list. `ofType` is a valid field.'),
+      NON_NULL =      TypeKind.new("NON_NULL", wraps: true, description: 'Indicates this type is a non-null. `ofType` is a valid field.'),
     ]
 
-    TYPE_KIND_DESCRIPTIONS = {
-      SCALAR:       'Indicates this type is a scalar.',
-      OBJECT:       'Indicates this type is an object. `fields` and `interfaces` are valid fields.',
-      INTERFACE:    'Indicates this type is an interface. `fields` and `possibleTypes` are valid fields.',
-      UNION:        'Indicates this type is a union. `possibleTypes` is a valid field.',
-      ENUM:         'Indicates this type is an enum. `enumValues` is a valid field.',
-      INPUT_OBJECT: 'Indicates this type is an input object. `inputFields` is a valid field.',
-      LIST:         'Indicates this type is a list. `ofType` is a valid field.',
-      NON_NULL:     'Indicates this type is a non-null. `ofType` is a valid field.',
-    }
-
-    KIND_NAMES = TYPE_KINDS.map(&:name)
     class TypeKind
-      KIND_NAMES.each do |kind_name|
+      TYPE_KINDS.map(&:name).each do |kind_name|
         define_method("#{kind_name.downcase}?") do
           self.name == kind_name
         end
