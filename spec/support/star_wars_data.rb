8b0bc92a8296db2203813b8287b1bb26ccdde03b
Jérémie Astori <jeremie@astori.fr>
2016-10-12 12:56:40 -0400

    Use dot-notation for OpenStructs as [] is not available in Ruby 1.9.3

--- spec/support/star_wars_data.rb
+++ spec/support/star_wars_data.rb
@@ -73,6 +73,6 @@ def STAR_WARS_DATA.create_ship(name, faction_id)
   new_id = (self["Ship"].keys.map(&:to_i).max + 1).to_s
   new_ship = OpenStruct.new(id: new_id, name: name)
   self["Ship"][new_id] = new_ship
-  self["Faction"][faction_id]["ships"] << new_id
+  self["Faction"][faction_id].ships << new_id
   new_ship
 end
