01852bfb98dc81bda97d235e262ab7f796a78086
Christian Joudrey <cmallette@gmail.com>
2016-11-05 16:21:17 -0400

    Refactor AssignArgument spec

--- spec/graphql/define/assign_argument_spec.rb
+++ spec/graphql/define/assign_argument_spec.rb
@@ -2,67 +2,47 @@ require "spec_helper"
 
 describe GraphQL::Define::AssignArgument do
   it "it accepts default_value" do
-    query_type = GraphQL::ObjectType.define do
-      name "Query"
-      field :a, types.String do
-        argument :a, types.String, default_value: "Default"
-      end
-    end
+    arg = define_argument(:a, GraphQL::STRING_TYPE, default_value: 'Default')
 
-    arg = query_type.fields['a'].arguments['a']
     assert_equal "Default", arg.default_value
     assert arg.default_value?
   end
 
   it "default_value is optional" do
-    query_type = GraphQL::ObjectType.define do
-      name "Query"
-      field :a, types.String do
-        argument :a, types.String
-      end
-    end
+    arg = define_argument(:a, GraphQL::STRING_TYPE)
 
-    arg = query_type.fields['a'].arguments['a']
     assert arg.default_value.nil?
     assert !arg.default_value?
   end
 
   it "default_value can be explicitly set to nil" do
-    query_type = GraphQL::ObjectType.define do
-      name "Query"
-      field :a, types.String do
-        argument :a, types.String, default_value: nil
-      end
-    end
+    arg = define_argument(:a, GraphQL::STRING_TYPE, default_value: nil)
 
-    arg = query_type.fields['a'].arguments['a']
     assert arg.default_value.nil?
     assert arg.default_value?
   end
 
   it "passing unknown keyword arguments will raise" do
     err = assert_raises ArgumentError do
-      query_type = GraphQL::ObjectType.define do
-        name "Query"
-        field :a, types.String do
-          argument :a, types.String, blah: nil
-        end
-      end
-      assert query_type.fields['a'].arguments
+      define_argument(:a, GraphQL::STRING_TYPE, blah: nil)
     end
 
     assert_equal 'unknown keyword: blah', err.message
 
     err = assert_raises ArgumentError do
-      query_type = GraphQL::ObjectType.define do
-        name "Query"
-        field :a, types.String do
-          argument :a, types.String, blah: nil, blah2: nil
-        end
-      end
-      assert query_type.fields['a'].arguments
+      define_argument(:a, GraphQL::STRING_TYPE, blah: nil, blah2: nil)
     end
 
     assert_equal 'unknown keywords: blah, blah2', err.message
+  end
+
+  def define_argument(*args)
+    type = GraphQL::ObjectType.define do
+      field :a, types.String do
+        argument(*args)
+      end
+    end
+
+    type.fields['a'].arguments[args.first.to_s]
   end
 end
