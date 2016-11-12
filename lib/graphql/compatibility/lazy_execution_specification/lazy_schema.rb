f9f21de5173cbf7ecafbb4c04fc1ef97fee96fcd
Robert Mosolgo <rdmosolgo@gmail.com>
2016-11-11 18:02:28 -0600

    feat(LazyExecutionSpecification) add compat suite

---
+++ lib/graphql/compatibility/lazy_execution_specification/lazy_schema.rb
@@ -0,0 +1,54 @@
+module GraphQL
+  module Compatibility
+    module LazyExecutionSpecification
+      module LazySchema
+        class LazyPush
+          attr_reader :value
+          def initialize(ctx, value)
+            @value = value
+            @context = ctx
+            pushes = @context[:lazy_pushes] ||= []
+            pushes << @value
+          end
+
+          def push
+            if @context[:lazy_pushes].include?(@value)
+              @context[:pushes] << @context[:lazy_pushes]
+              @context[:lazy_pushes] = []
+            end
+            self
+          end
+        end
+
+        def self.build(execution_strategy)
+          lazy_push_type = GraphQL::ObjectType.define do
+            name "LazyPush"
+            field :value, types.Int
+            field :push, lazy_push_type do
+              argument :value, types.Int
+              resolve ->(o, a, c) {
+                LazyPush.new(c, a[:value])
+              }
+            end
+          end
+
+          query_type = GraphQL::ObjectType.define do
+            name "Query"
+            field :push, lazy_push_type do
+              argument :value, types.Int
+              resolve ->(o, a, c) {
+                LazyPush.new(c, a[:value])
+              }
+            end
+          end
+
+          GraphQL::Schema.define do
+            query(query_type)
+            query_execution_strategy(execution_strategy)
+            lazy_resolve(LazyPush, :push)
+          end
+        end
+      end
+    end
+  end
+end
--- spec/graphql/execution/execute_spec.rb
+++ spec/graphql/execution/execute_spec.rb
@@ -1,3 +1,4 @@
 require "spec_helper"
 
 ExecuteSuite = GraphQL::Compatibility::ExecutionSpecification.build_suite(GraphQL::Execution::Execute)
+LazyExecuteSuite = GraphQL::Compatibility::LazyExecutionSpecification.build_suite(GraphQL::Execution::Execute)
