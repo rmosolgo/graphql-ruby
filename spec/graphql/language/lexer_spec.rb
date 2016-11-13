0d37bcc12978e994b8154b546969210b67eb1448
Robert Mosolgo <rdmosolgo@gmail.com>
2016-11-13 09:53:40 -0500

    fix(lexer) don't preserve state between runs

--- spec/graphql/language/lexer_spec.rb
+++ spec/graphql/language/lexer_spec.rb
@@ -18,5 +18,11 @@ describe GraphQL::Language::Lexer do
     it "keeps track of previous_token" do
       assert_equal tokens[0], tokens[1].prev_token
     end
+
+    it "clears the previous_token between runs" do
+      tok_1 = subject.tokenize(query_string)
+      tok_2 = subject.tokenize(query_string)
+      assert_equal nil, tok_2[0].prev_token
+    end
   end
 end
