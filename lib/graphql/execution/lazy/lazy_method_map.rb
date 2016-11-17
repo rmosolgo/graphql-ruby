283fc19d72eb9890ea6254f7fc79600f3f0bfbeb
Robert Mosolgo <rdmosolgo@gmail.com>
2016-11-16 19:44:09 -0500

    refactor(LazyMethodMap) avoid slow #key? method, call #class once

--- lib/graphql/execution/lazy/lazy_method_map.rb
+++ lib/graphql/execution/lazy/lazy_method_map.rb
@@ -6,7 +6,14 @@ module GraphQL
       # The result of this lookup is cached for future resolutions.
       class LazyMethodMap
         def initialize
-          @storage = {}
+          @storage = Hash.new do |h, value_class|
+            registered_superclass = h.each_key.find { |lazy_class| value_class < lazy_class }
+            if registered_superclass.nil?
+              h[value_class] = nil
+            else
+              h[value_class] = h[registered_superclass]
+            end
+          end
         end
 
         # @param lazy_class [Class] A class which represents a lazy value (subclasses may also be used)
@@ -18,13 +25,7 @@ module GraphQL
         # @param value [Object] an object which may have a `lazy_value_method` registered for its class or superclasses
         # @return [Symbol, nil] The `lazy_value_method` for this object, or nil
         def get(value)
-          if @storage.key?(value.class)
-            @storage[value.class]
-          else
-            value_class = value.class
-            registered_superclass = @storage.each_key.find { |lazy_class| value_class < lazy_class }
-            @storage[value_class] = @storage[registered_superclass]
-          end
+          @storage[value.class]
         end
       end
     end
