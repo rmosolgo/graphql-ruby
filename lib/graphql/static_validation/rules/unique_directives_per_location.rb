4d6872dd799bb6a0b5b660188f28ddca242eef0d
Robert Mosolgo <rdmosolgo@gmail.com>
2016-11-22 12:27:37 -0500

    Merge pull request #409 from cjoudrey/unique-directives-per-location
    Add validation rule for unique directives per location

---
+++ lib/graphql/static_validation/rules/unique_directives_per_location.rb
@@ -0,0 +1,38 @@
+module GraphQL
+  module StaticValidation
+    class UniqueDirectivesPerLocation
+      include GraphQL::StaticValidation::Message::MessageHelper
+
+      NODES_WITH_DIRECTIVES = GraphQL::Language::Nodes.constants
+        .map{|c| GraphQL::Language::Nodes.const_get(c)}
+        .select{|c| c.is_a?(Class) && c.instance_methods.include?(:directives)}
+
+      def validate(context)
+        NODES_WITH_DIRECTIVES.each do |node_class|
+          context.visitor[node_class] << ->(node, _) {
+            validate_directives(node, context) unless node.directives.empty?
+          }
+        end
+      end
+
+      private
+
+      def validate_directives(node, context)
+        used_directives = {}
+
+        node.directives.each do |ast_directive|
+          directive_name = ast_directive.name
+          if used_directives[directive_name]
+            context.errors << message(
+              "The directive \"#{directive_name}\" can only be used once at this location.",
+              [used_directives[directive_name], ast_directive],
+              context: context
+            )
+          else
+            used_directives[directive_name] = ast_directive
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/graphql/static_validation/rules/unique_directives_per_location_spec.rb
@@ -0,0 +1,180 @@
+require "spec_helper"
+
+describe GraphQL::StaticValidation::UniqueDirectivesPerLocation do
+  include StaticValidationHelpers
+
+  let(:schema) { GraphQL::Schema.from_definition("
+    type Query {
+      type: Type
+    }
+
+    type Type {
+      field: String
+    }
+
+    directive @A on FIELD
+    directive @B on FIELD
+  ") }
+
+  describe "query with no directives" do
+    let(:query_string) {"
+      {
+        type {
+          field
+        }
+      }
+    "}
+
+    it "passes rule" do
+      assert_equal [], errors
+    end
+  end
+
+  describe "query with unique directives in different locations" do
+    let(:query_string) {"
+      {
+        type @A {
+          field @B
+        }
+      }
+    "}
+
+    it "passes rule" do
+      assert_equal [], errors
+    end
+  end
+
+  describe "query with unique directives in same locations" do
+    let(:query_string) {"
+      {
+        type @A @B {
+          field @A @B
+        }
+      }
+    "}
+
+    it "passes rule" do
+      assert_equal [], errors
+    end
+  end
+
+  describe "query with same directives in different locations" do
+    let(:query_string) {"
+      {
+        type @A {
+          field @A
+        }
+      }
+    "}
+
+    it "passes rule" do
+      assert_equal [], errors
+    end
+  end
+
+  describe "query with same directives in similar locations" do
+    let(:query_string) {"
+      {
+        type {
+          field @A
+          field @A
+        }
+      }
+    "}
+
+    it "passes rule" do
+      assert_equal [], errors
+    end
+  end
+
+  describe "query with duplicate directives in one location" do
+    let(:query_string) {"
+      {
+        type {
+          field @A @A
+        }
+      }
+    "}
+
+    it "fails rule" do
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 17 }, { "line" => 4, "column" => 20 }],
+        "fields" => ["query", "type", "field"],
+      }
+    end
+  end
+
+
+  describe "query with many duplicate directives in one location" do
+    let(:query_string) {"
+      {
+        type {
+          field @A @A @A
+        }
+      }
+    "}
+
+    it "fails rule" do
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 17 }, { "line" => 4, "column" => 20 }],
+        "fields" => ["query", "type", "field"],
+      }
+
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 17 }, { "line" => 4, "column" => 23 }],
+        "fields" => ["query", "type", "field"],
+      }
+    end
+  end
+
+  describe "query with different duplicate directives in one location" do
+    let(:query_string) {"
+      {
+        type {
+          field @A @B @A @B
+        }
+      }
+    "}
+
+    it "fails rule" do
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 17 }, { "line" => 4, "column" => 23 }],
+        "fields" => ["query", "type", "field"],
+      }
+
+      assert_includes errors, {
+        "message" => 'The directive "B" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 20 }, { "line" => 4, "column" => 26 }],
+        "fields" => ["query", "type", "field"],
+      }
+    end
+  end
+
+  describe "query with duplicate directives in many locations" do
+    let(:query_string) {"
+      {
+        type @A @A {
+          field @A @A
+        }
+      }
+    "}
+
+    it "fails rule" do
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 3, "column" => 14 }, { "line" => 3, "column" => 17 }],
+        "fields" => ["query", "type"],
+      }
+
+      assert_includes errors, {
+        "message" => 'The directive "A" can only be used once at this location.',
+        "locations" => [{ "line" => 4, "column" => 17 }, { "line" => 4, "column" => 20 }],
+        "fields" => ["query", "type", "field"],
+      }
+    end
+  end
+end
