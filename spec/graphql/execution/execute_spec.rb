c4cfdfe6c1504b4acabf83311962c517b89675d9
Robert Mosolgo <rdmosolgo@gmail.com>
2017-03-06 07:23:26 -0500

    fix(Execute) if a selection would be nullified, quit resolving children

--- spec/graphql/execution/execute_spec.rb
+++ spec/graphql/execution/execute_spec.rb
@@ -8,7 +8,7 @@ describe GraphQL::Execution::Execute do
   describe "null values on mutation roots" do
     module MutationNullTestRoot
       INTS = []
-      def self.pushInt(args, ctx)
+      def self.push(args, ctx)
         if args[:int] == 13
           nil
         else
@@ -24,7 +24,7 @@ describe GraphQL::Execution::Execute do
 
     let(:schema) { GraphQL::Schema.from_definition <<-GRAPHQL
       type Mutation {
-        pushInt(int: Int!): Int!
+        push(int: Int!): Int!
       }
 
       type Query {
@@ -42,20 +42,15 @@ describe GraphQL::Execution::Execute do
     it "returns values for other mutations" do
       query_str = <<-GRAPHQL
       mutation {
-        one: pushInt(int: 1)
-        thirteen: pushInt(int: 13)
-        two: pushInt(int: 2)
+        one: push(int: 1)
+        thirteen: push(int: 13)
+        two: push(int: 2)
       }
       GRAPHQL
 
       res = schema.execute(query_str, root_value: root)
-      assert_equal [1, 2], MutationNullTestRoot::INTS
-      expected_data = {
-        "one" => 1,
-        "thirteen" => nil,
-        "two" => 2,
-      }
-      assert_equal(expected_data, res["data"])
+      assert_equal [1], MutationNullTestRoot::INTS
+      assert_equal(nil, res["data"])
     end
   end
 end
