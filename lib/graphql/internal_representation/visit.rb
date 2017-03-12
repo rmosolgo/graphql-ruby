c34bdbe0195ab8f026fab1e4c9139bf78189494b
Robert Mosolgo <rdmosolgo@gmail.com>
2017-03-12 16:16:05 -0400

    refactor(Rewrite) remove type-keyed operation hash

--- lib/graphql/internal_representation/visit.rb
+++ lib/graphql/internal_representation/visit.rb
@@ -6,15 +6,13 @@ module GraphQL
       module_function
       def visit_each_node(operations, handlers)
         # Post-validation: make some assertions about the rewritten query tree
-        operations.each do |obj_type, ops|
-          ops.each do |op_name, op_node|
-            # Yield each node to listeners which were attached by validators
-            op_node.typed_children.each do |obj_type, children|
-              children.each do |name, op_child_node|
-                each_node(op_child_node) do |node|
-                  for h in handlers
-                    h.call(node)
-                  end
+        operations.each do |op_name, op_node|
+          # Yield each node to listeners which were attached by validators
+          op_node.typed_children.each do |obj_type, children|
+            children.each do |name, op_child_node|
+              each_node(op_child_node) do |node|
+                for h in handlers
+                  h.call(node)
                 end
               end
             end
