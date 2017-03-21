0cca0cc446b1dbdefb59c8d49041e86f5267038a
Robert Mosolgo <rdmosolgo@gmail.com>
2017-03-20 22:15:08 -0400

    benchmark(Rewrite) add a case for fragments on abstract types

--- benchmark/run.rb
+++ benchmark/run.rb
@@ -8,14 +8,20 @@ module GraphQLBenchmark
   DOCUMENT = GraphQL.parse(QUERY_STRING)
   SCHEMA = Dummy::Schema
 
+  BENCHMARK_PATH = File.expand_path("../", __FILE__)
+  CARD_SCHEMA = GraphQL::Schema.from_definition(File.read(File.join(BENCHMARK_PATH, "schema.graphql")))
+  ABSTRACT_FRAGMENTS = GraphQL.parse(File.read(File.join(BENCHMARK_PATH, "abstract_fragments.graphql")))
+
   module_function
   def self.run(task)
+
     Benchmark.ips do |x|
       case task
       when "query"
         x.report("query") { SCHEMA.execute(document: DOCUMENT) }
       when "validate"
-        x.report("validate") { SCHEMA.validate(DOCUMENT) }
+        x.report("validate - introspection ") { CARD_SCHEMA.validate(DOCUMENT) }
+        x.report("validate - abstract fragments") { CARD_SCHEMA.validate(ABSTRACT_FRAGMENTS) }
       else
         raise("Unexpected task #{task}")
       end
