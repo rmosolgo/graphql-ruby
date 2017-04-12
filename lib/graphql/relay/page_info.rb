fd2ba4be7e208972442e94fe7854cb29770bc012
Tieg Zaharia <tieg.zaharia@gmail.com>
2017-04-12 14:28:50 -0400

    Add some details to hasNextPage/hasPreviousPage descriptions.

--- lib/graphql/relay/page_info.rb
+++ lib/graphql/relay/page_info.rb
@@ -5,8 +5,8 @@ module GraphQL
     PageInfo = GraphQL::ObjectType.define do
       name("PageInfo")
       description("Information about pagination in a connection.")
-      field :hasNextPage, !types.Boolean, "Indicates if there are more pages to fetch", property: :has_next_page
-      field :hasPreviousPage, !types.Boolean, "Indicates if there are any pages prior to the current page", property: :has_previous_page
+      field :hasNextPage, !types.Boolean, "Indicates if there are more pages to fetch (only meaningful if `first` argument is present)", property: :has_next_page
+      field :hasPreviousPage, !types.Boolean, "Indicates if there are any pages prior to the current page (only meaningful if `last` argument is present)", property: :has_previous_page
       field :startCursor, types.String, "When paginating backwards, the cursor to continue", property: :start_cursor
       field :endCursor, types.String, "When paginating forwards, the cursor to continue", property: :end_cursor
       default_relay true
