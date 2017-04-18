77aa26dc5eb81e8d81cf7aaa68a34ba1ad16df59
Tieg Zaharia <tieg.zaharia@gmail.com>
2017-04-18 09:45:12 -0400

    Match PageInfo descriptions to graphql-relay-js library.

--- lib/graphql/relay/page_info.rb
+++ lib/graphql/relay/page_info.rb
@@ -5,10 +5,10 @@ module GraphQL
     PageInfo = GraphQL::ObjectType.define do
       name("PageInfo")
       description("Information about pagination in a connection.")
-      field :hasNextPage, !types.Boolean, "Indicates if there are more pages to fetch (only meaningful if `first` argument is present)", property: :has_next_page
-      field :hasPreviousPage, !types.Boolean, "Indicates if there are any pages prior to the current page (only meaningful if `last` argument is present)", property: :has_previous_page
-      field :startCursor, types.String, "When paginating backwards, the cursor to continue", property: :start_cursor
-      field :endCursor, types.String, "When paginating forwards, the cursor to continue", property: :end_cursor
+      field :hasNextPage, !types.Boolean, "When paginating forwards, are there more items?", property: :has_next_page
+      field :hasPreviousPage, !types.Boolean, "When paginating backwards, are there more items?", property: :has_previous_page
+      field :startCursor, types.String, "When paginating backwards, the cursor to continue.", property: :start_cursor
+      field :endCursor, types.String, "When paginating forwards, the cursor to continue.", property: :end_cursor
       default_relay true
     end
   end
