8657487dd4c15127dfcb28d89da78cb481b34a9f
Robert Mosolgo <rdmosolgo@gmail.com>
2017-05-21 10:36:07 -0400

    feat(Argument#prepare) update test examples to two-argument

--- spec/graphql/query/literal_input_spec.rb
+++ spec/graphql/query/literal_input_spec.rb
@@ -8,13 +8,13 @@ describe GraphQL::Query::LiteralInput do
         query = GraphQL::ObjectType.define do
           name "Query"
 
-          field :addOneToArgumentValue do
+          field :addToArgumentValue do
             type !types.Int
             argument :value do
               type !types.Int
-              prepare ->(arg) do
+              prepare ->(arg, ctx) do
                 return GraphQL::ExecutionError.new("Can't return more than 3 digits") if arg > 998
-                arg + 1
+                arg + ctx[:val]
               end
             end
             resolve ->(t, a, c) { a[:value] }
@@ -25,26 +25,26 @@ describe GraphQL::Query::LiteralInput do
       }
 
       it "prepares values from query literals" do
-        result = schema.execute("{ addOneToArgumentValue(value: 1) }")
-        assert_equal(result["data"]["addOneToArgumentValue"], 2)
+        result = schema.execute("{ addToArgumentValue(value: 1) }", context: { val: 1 })
+        assert_equal(result["data"]["addToArgumentValue"], 2)
       end
 
       it "prepares values from variables" do
-        result = schema.execute("query ($value: Int!) { addOneToArgumentValue(value: $value) }", variables: { "value" => 1} )
-        assert_equal(result["data"]["addOneToArgumentValue"], 2)
+        result = schema.execute("query ($value: Int!) { addToArgumentValue(value: $value) }", variables: { "value" => 1}, context: { val: 2 } )
+        assert_equal(result["data"]["addToArgumentValue"], 3)
       end
 
       it "prepares values correctly if called multiple times with different arguments" do
-        result = schema.execute("{ first: addOneToArgumentValue(value: 1) second: addOneToArgumentValue(value: 2) }")
-        assert_equal(result["data"]["first"], 2)
-        assert_equal(result["data"]["second"], 3)
+        result = schema.execute("{ first: addToArgumentValue(value: 1) second: addToArgumentValue(value: 2) }", context: { val: 3 })
+        assert_equal(result["data"]["first"], 4)
+        assert_equal(result["data"]["second"], 5)
       end
 
       it "adds message to errors key if an ExecutionError is returned from the prepare function" do
-        result = schema.execute("{ addOneToArgumentValue(value: 999) }")
+        result = schema.execute("{ addToArgumentValue(value: 999) }")
         assert_equal(result["errors"][0]["message"], "Can't return more than 3 digits")
         assert_equal(result["errors"][0]["locations"][0]["line"], 1)
-        assert_equal(result["errors"][0]["locations"][0]["column"], 25)
+        assert_equal(result["errors"][0]["locations"][0]["column"], 22)
       end
     end
   end
