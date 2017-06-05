351107b664da9ec17cadf9449cf5585b7c24fd1c
Robert Mosolgo <rdmosolgo@gmail.com>
2017-06-05 16:32:36 -0400

    fix(Query::ArgumentsCache) actually cache args; use NO_ARGS when none are possible

--- lib/graphql/query/arguments_cache.rb
+++ lib/graphql/query/arguments_cache.rb
@@ -5,14 +5,18 @@ module GraphQL
       # @return [Hash<InternalRepresentation::Node, GraphQL::Language::NodesDirectiveNode => Hash<GraphQL::Field, GraphQL::Directive => GraphQL::Query::Arguments>>]
       def self.build(query)
         Hash.new do |h1, irep_or_ast_node|
-          Hash.new do |h2, definition|
+          h1[irep_or_ast_node] = Hash.new do |h2, definition|
             ast_node = irep_or_ast_node.is_a?(GraphQL::InternalRepresentation::Node) ? irep_or_ast_node.ast_node : irep_or_ast_node
             ast_arguments = ast_node.arguments
-            GraphQL::Query::LiteralInput.from_arguments(
-              ast_arguments,
-              definition.arguments,
-              query.variables,
-            )
+            h2[definition] = if definition.arguments.none?
+              GraphQL::Query::Arguments::NO_ARGS
+            else
+              GraphQL::Query::LiteralInput.from_arguments(
+                ast_arguments,
+                definition.arguments,
+                query.variables,
+              )
+            end
           end
         end
       end
