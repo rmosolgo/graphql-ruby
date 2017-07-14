08808dc438e7b8741734c38aa9dbae1a19d835d6
Siebe Jan <siebejan@hackerone.com>
2017-07-14 16:06:20 +0200

    test: add a sanity check to make sure Rails is not installed

---
+++ spec/rails_dependency_sanity_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe 'Rails dependency sanity check' do
+  if rails_should_be_installed?
+    it "should have rails installed" do
+      assert defined?(Rails)
+    end
+  else
+    it "should not have rails installed" do
+      refute defined?(Rails)
+    end
+  end
+end
