795ec0f55cd1f39e66cefe587cd6d6c710e29a7d
Siebe Jan <siebejan@hackerone.com>
2017-08-17 14:43:28 +0200

    move sql-related gems in the "with-rails"-block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,12 +10,13 @@ if rails_should_be_installed?
   require "rake"
   require "rails/all"
   require "rails/generators"
+
+  require "jdbc/sqlite3" if RUBY_ENGINE == 'jruby'
+  require "sqlite3" if RUBY_ENGINE == 'ruby'
+  require "pg" if RUBY_ENGINE == 'ruby'
+  require "sequel"
 end
 
-require "jdbc/sqlite3" if RUBY_ENGINE == 'jruby'
-require "sqlite3" if RUBY_ENGINE == 'ruby'
-require "pg" if RUBY_ENGINE == 'ruby'
-require "sequel"
 require "graphql"
 require "graphql/rake_task"
 require "benchmark"
