40d08989498d2e8d0b6f3aa6ae43d65201f98900
Robert Mosolgo <rdmosolgo@gmail.com>
2017-09-14 10:48:44 -0400

    refactor(GraphQL::Backtrace) re-raise with an error wrapper

--- lib/graphql/backtrace.rb
+++ lib/graphql/backtrace.rb
@@ -11,20 +11,47 @@ module GraphQL
   #   GraphQL::Backtrace.disable
   #
   module Backtrace
-    GRAPHQL_GEM_PATH = File.expand_path(File.join(__FILE__, ".."))
+    # When {Backtrace} is enabled,
+    # raised errors are wrapped with `TracedError`.
+    class TracedError < GraphQL::Error
+      # @return [Array<String>] Printable backtrace of GraphQL error context
+      attr_reader :graphql_backtrace
+
+      # @return [Array<GraphQL::Execution::Multiplex, GraphQL::Query, GraphQL::Query::Context>] Objects which represent context
+      attr_reader :graphql_context
+
+      MESSAGE_TEMPLATE = <<-MESSAGE
+Unhandled error during GraphQL execution: %{cause_message}.
+Use #cause to access the original exception (including #cause.backtrace).
+
+GraphQL Backtrace:
+
+  %{graphql_backtrace}
+MESSAGE
+
+      def initialize(err, graphql_context)
+        @graphql_context = graphql_context
+        @graphql_backtrace = graphql_context.map do |key, ctx_entry|
+          "#{(key + ":").ljust(20)} #{Backtrace.serialize_context_entry(ctx_entry)}"
+        end
+        message = MESSAGE_TEMPLATE % {
+          cause_message: err.message,
+          graphql_backtrace: graphql_backtrace.map { |l| "  " + l }.join("\n"),
+        }
+        super(message)
+      end
+    end
     module_function
     # Turn on annotation
     def enable
-      clear_context_state
+      execution_context.clear
       GraphQL::Tracing.install(self)
-      TRACE_POINT.enable
       nil
     end
 
     # Turn off annotation
     def disable
       GraphQL::Tracing.uninstall(self)
-      TRACE_POINT.disable
       nil
     end
 
@@ -47,29 +74,27 @@ module GraphQL
 
       if push_data
         if key == "execute_multiplex"
-          clear_context_state
+          execution_context.clear
+          execution_context.push([key, push_data])
+          begin
+            yield
+          rescue StandardError => err
+            # This is an unhandled error from execution,
+            # Re-raise it with a GraphQL trace.
+            graphql_backtrace = execution_context.dup.uniq.reverse
+            raise TracedError.new(err, graphql_backtrace)
+          ensure
+            execution_context.clear
+          end
+        else
+          execution_context.push([key, push_data])
+          res = yield
+          execution_context.pop
+          res
         end
-        execution_context.push(push_data)
-        yield
       else
         yield
       end
-
-    ensure
-      if push_data
-        execution_context.pop
-      end
-    end
-
-    # A stack of objects, where each entry corresponds to an entry
-    # in the backtrace.
-    # The top of the stack corresponds to the top-most entry of the backtrace.
-    # The backtrace may be longer than this stack; those extra
-    # backtrace entries have no GraphQL context. (It may be longer
-    # because the backtrace is non-zero when `.enable` is called.)
-    # @return [Array<Object>]
-    def backtrace_context
-      Thread.current[:graphql_backtrace_context] ||= []
     end
 
     # A stack of objects corresponding to the GraphQL context.
@@ -79,27 +104,6 @@ module GraphQL
     # @return [Array<Object>]
     def execution_context
       Thread.current[:graphql_execution_context] ||= []
-    end
-
-    def clear_context_state
-      execution_context.clear
-      backtrace_context.clear
-    end
-
-    # - Gather info about method calls when they happen
-    # - Remove trace info for this method when it returns
-    # - Modify errors as they're being raised
-    # @api private
-    TRACE_POINT = TracePoint.new do |tp|
-      case tp.event
-      when :call, :b_call
-        backtrace_context.push(execution_context.last)
-      when :return, :b_return
-        backtrace_context.pop
-      when :raise
-        annotate_error(tp.raised_exception)
-        clear_context_state
-      end
     end
 
     # Format `context_entry` into a readable string.
@@ -129,41 +133,6 @@ module GraphQL
       else
         raise "Unexpected context entry: #{context_entry}"
       end
-    end
-
-    # Apply styles to the string before it's added to the backtrace
-    # @param context_message [String] A prepared string for the backtrace
-    # @return [String] `context_message`, with styles added
-    def format_context_message(context_message)
-      " \e[1mGraphQL: #{context_message}\e[22m "
-    end
-
-    # Update `err`'s backtrace to include GraphQL-related annotations
-    # @param err [StandardError]
-    # @return [void]
-    def annotate_error(err)
-      ctx = backtrace_context
-      if err.is_a?(NoMethodError)
-        # Since it's a no-method-error, the `:call`
-        # tracepoint was never entered.
-        ctx.push(execution_context.last)
-      end
-
-      ctx_offset = ctx.length - 1
-      new_backtrace = err.backtrace.each_with_index.map do |line,idx|
-        ctx_entry = idx < ctx_offset && ctx[ctx_offset - idx]
-        if ctx_entry
-          ctx_msg = format_context_message(serialize_context_entry(ctx_entry))
-          "#{line}#{ctx_msg}"
-        else
-          line
-        end
-      end
-      err.set_backtrace(new_backtrace)
-    rescue StandardError => err
-      # We should never reach here, only if there's a bug in the block above!
-      warn("GraphQL::Backtrace bug: #{err}")
-      puts err.backtrace
     end
   end
 end
--- spec/graphql/backtrace_spec.rb
+++ spec/graphql/backtrace_spec.rb
@@ -52,79 +52,88 @@ describe GraphQL::Backtrace do
     }
   }
 
-  describe "graphql context in backtraces" do
-    it "annotates when enabled" do
-      err = assert_raises(NoMethodError) {
+  describe "GraphQL backtrace helpers" do
+    it "raises a TracedError when enabled" do
+      err = assert_raises(GraphQL::Backtrace::TracedError) {
         schema.execute("query BrokenList { field1 { listField { strField } } }")
       }
-      assert err.backtrace.any? { |s| s.include?("GraphQL: ")}
+
       GraphQL::Backtrace.disable
 
       err = assert_raises(NoMethodError) {
         schema.execute("query BrokenList { field1 { listField { strField } } }")
       }
-      refute err.backtrace.any? { |s| s.include?("GraphQL: ")}
     end
 
     it "cleans up its own stack" do
       # Starts clean
-      assert_equal 0, GraphQL::Backtrace.backtrace_context.compact.size
       assert_equal 0, GraphQL::Backtrace.execution_context.size
 
       # Cleaned up after normal query
       schema.execute("{__schema{types{name}}}")
 
-      assert_equal 0, GraphQL::Backtrace.backtrace_context.compact.size
       assert_equal 0, GraphQL::Backtrace.execution_context.size
 
       # Cleaned up after error
-      assert_raises(NoMethodError) {
+      assert_raises(GraphQL::Backtrace::TracedError) {
         schema.execute("query BrokenList { field1 { listField { strField } } }")
       }
 
-      assert_equal 0, GraphQL::Backtrace.backtrace_context.compact.size
       assert_equal 0, GraphQL::Backtrace.execution_context.size
     end
 
-    it "annotates NoMethodErrors" do
-      schema
-      err = assert_raises(NoMethodError) {
-        schema.execute("query BrokenList { field1 { listField { strField } } }")
-      }
-      assert_backtrace_includes(err.backtrace, file: "execute.rb", method: "resolve_value", annotation: "Thing.listField")
-      assert_backtrace_includes(err.backtrace, file: "execute.rb", method: "resolve_field", annotation: "Thing.listField")
-      assert_backtrace_includes(err.backtrace, file: "execute.rb", method: "resolve_value", annotation: "Query.field1")
-      assert_backtrace_includes(err.backtrace, file: "execute.rb", method: "resolve_field", annotation: "Query.field1")
-      assert_backtrace_includes(err.backtrace, file: "execute.rb", method: "resolve_root_selection", annotation: "query BrokenList")
-      assert_backtrace_includes(err.backtrace, file: "multiplex.rb", method: "begin_query", annotation: "query BrokenList")
-    end
-
     it "annotates crashes from user code" do
-      err = assert_raises(RuntimeError) {
-        pp schema.execute("{ field1 { boomError: raiseField(message: \"Boom\") } }")
+      err = assert_raises(GraphQL::Backtrace::TracedError) {
+        schema.execute("{ field1 { boomError: raiseField(message: \"Boom\") } }")
       }
-      b = err.backtrace
-      assert_backtrace_includes(b, file: "backtrace_spec.rb", method: "block", annotation: "Thing.raiseField(message: \"Boom\") (as boomError)" )
-      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_field", annotation: "Thing.raiseField(message: \"Boom\") (as boomError)")
-      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_field", annotation: "Query.field1")
-      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_root_selection", annotation: "query <Anonymous>")
+
+      # The original error info is present
+      assert_instance_of RuntimeError, err.cause
+      b = err.cause.backtrace
+      assert_backtrace_includes(b, file: "backtrace_spec.rb", method: "block")
+      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_field")
+      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_field")
+      assert_backtrace_includes(b, file: "execute.rb", method: "resolve_root_selection")
+
+      # GraphQL backtrace is present
+      expected_graphql_backtrace = [
+        "execute_field:       Thing.raiseField(message: \"Boom\") (as boomError)",
+        "execute_field:       Query.field1",
+        "execute_query:       query <Anonymous>",
+        "execute_multiplex:   query <Anonymous>",
+      ]
+      assert_equal expected_graphql_backtrace, err.graphql_backtrace
+
+      # The message includes the GraphQL context
+      assert_includes err.message, expected_graphql_backtrace.join("\n  ")
+      # The message includes the original error message
+      assert_includes err.message, "This is broken: Boom"
     end
 
     it "annotates errors inside lazy resolution" do
-      err = assert_raises(RuntimeError) {
-        pp schema.execute("{ field2 { strField } }")
+      err = assert_raises(GraphQL::Backtrace::TracedError) {
+      schema.execute("query StrField { field2 { strField } }")
       }
-      b = err.backtrace
-      assert_backtrace_includes(b, file: "backtrace_spec.rb", method: "raise_err", annotation: "OtherThing.strField")
-      assert_backtrace_includes(b, file: "field.rb", method: "lazy_resolve", annotation: "OtherThing.strField")
-      assert_backtrace_includes(b, file: "lazy/resolve.rb", method: "block", annotation: "OtherThing.strField")
+      assert_instance_of RuntimeError, err.cause
+      b = err.cause.backtrace
+      assert_backtrace_includes(b, file: "backtrace_spec.rb", method: "raise_err")
+      assert_backtrace_includes(b, file: "field.rb", method: "lazy_resolve")
+      assert_backtrace_includes(b, file: "lazy/resolve.rb", method: "block")
+
+      expected_graphql_backtrace = [
+        "execute_field_lazy:  OtherThing.strField",
+        "execute_query_lazy:  query StrField",
+        "execute_multiplex:   query StrField",
+      ]
+
+        assert_equal(expected_graphql_backtrace, err.graphql_backtrace)
     end
   end
 
   # This will get brittle when execution code moves between files
   # but I'm not sure how to be sure that the backtrace contains the right stuff!
-  def assert_backtrace_includes(backtrace, file:, method:, annotation:)
-    is_annotated = backtrace.any? { |s| s.include?(file) && s.include?("`" + method) && s.include?("GraphQL: " + annotation) }
-    assert is_annotated, "Backtrace should tag of '#{annotation}' from #{file} inside method #{method}"
+  def assert_backtrace_includes(backtrace, file:, method:)
+    includes_tag = backtrace.any? { |s| s.include?(file) && s.include?("`" + method) }
+    assert includes_tag, "Backtrace should include #{file} inside method #{method}"
   end
 end
