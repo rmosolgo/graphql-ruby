6c71e36e94635cde8793e4dd3bd5fe9a2e504b2e
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2017-09-20 00:23:02 -0400

    Allow syncTest to account for new graphql fixture files in project
    Use jest snapshots to compare expected objects with actuals.

--- javascript_client/__tests__/syncTest.js
+++ javascript_client/__tests__/syncTest.js
@@ -44,21 +44,13 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      // Digest::MD5.hexdigest("query GetStuff {\n  stuff\n}\n")
-      // => "f7f65309043352183e905e1396e51078"
-      var expectedOperations = [
-        {
-          alias: "f7f65309043352183e905e1396e51078",
-          name: "GetStuff",
-          body: "query GetStuff {\n  stuff\n}\n",
-        }
-      ]
-      expect(payload.operations).toEqual(expectedOperations)
+      expect(payload.operations).toMatchSnapshot()
 
       options.glob += "**/*.graphql"
       sync(options)
+
       // Get the same result, even when the glob already has a file extension
-      expect(payload.operations).toEqual(expectedOperations)
+      expect(payload.operations).toMatchSnapshot()
     })
 
     it("Uses a custom hash function if provided", () => {
@@ -76,15 +68,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      expectedOperations = [
-        {
-          "body": "query GetStuff {\n  stuff\n}\n",
-          "alias": "GETSTUFF",
-          "name": "GetStuff",
-        },
-      ]
-
-      expect(payload.operations).toEqual(expectedOperations)
+      expect(payload.operations).toMatchSnapshot()
     })
   })
 
@@ -100,15 +84,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      expectedOperations = [
-        {
-          "body": "query AppFeedQuery {\n  feed(type: NEW, limit: 5) {\n    ...Feed\n  }\n}\n\nfragment Feed on Entry {\n  repository {\n    owner {\n      login\n    }\n    name\n  }\n  ...FeedEntry\n}\n\nfragment FeedEntry on Entry {\n  repository {\n    owner {\n      login\n    }\n    name\n    stargazers_count\n  }\n  postedBy {\n    login\n  }\n}\n",
-          "name": "AppFeedQuery",
-          "alias": "353e010cb78d082b29cb63ee7e9027b3",
-        },
-      ]
-
-      expect(payload.operations).toEqual(expectedOperations)
+      expect(payload.operations).toMatchSnapshot()
     })
   })
 
@@ -125,15 +101,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      expectedAliases = [
-        "5f0da489cf508a7c65ff5fa144e50545",
-        "c944b08d15eb94cf93dd124b7d664b62",
-      ]
-
-      expectedNames = ["GetStuff", "GetStuff2"]
-
-      expect(payload.operations.map((op) => op.name)).toEqual(expectedNames)
-      expect(payload.operations.map((op) => op.alias)).toEqual(expectedAliases)
+      expect(payload.operations).toMatchSnapshot()
     })
 
     it("Uses mode: file to process each file separately", () => {
@@ -148,19 +116,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      // One alias per file, different than above
-      expectedAliases = [
-        "26c44bfe42872860da112b6177355bfa",
-        "7de9e7bf1d6ea1f527de07a25983086c",
-        "8bc9b9922a7fbb66f4ac6c58d5d5c357",
-        "0a6add7303775e2487f2c2235ecb1c80",
-        "2f26b770ded2a04279bc4bf824ca54ac",
-      ]
-
-      expectedNames = [null, null, null, "GetStuff", "GetStuff2"]
-
-      expect(payload.operations.map((op) => op.name)).toEqual(expectedNames)
-      expect(payload.operations.map((op) => op.alias)).toEqual(expectedAliases)
+      expect(payload.operations).toMatchSnapshot()
     })
   })
 
@@ -213,13 +169,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      var expectedCalls = [
-        ["Syncing 2 operations to " + logger.bright("bogus") + "..."],
-        ["Generating client module in " + logger.bright("OperationStoreClient.js") + "..."],
-        [logger.green("✓ Done!")],
-      ]
-
-      expect(spy.mock.calls).toEqual(expectedCalls)
+      expect(spy.mock.calls).toMatchSnapshot()
     })
 
     it("Can be quieted with quiet: true", () => {
@@ -234,8 +184,7 @@ describe("sync operations", () => {
       }
       sync(options)
 
-      var expectedCalls = []
-      expect(spy.mock.calls).toEqual(expectedCalls)
+      expect(spy.mock.calls).toMatchSnapshot()
     })
   })
 
@@ -265,21 +214,8 @@ describe("sync operations", () => {
       var syncPromise = sync(options)
 
       return syncPromise.then(() => {
-
-        var expectedLogCalls = [
-          ["Syncing 2 operations to " + logger.bright("http://example.com/stored_operations/sync") + "..."],
-          ["  " + logger.dim("0 added")],
-          ["  " + logger.dim("0 not modified")],
-          ["  " + logger.red("1 failed")]
-        ]
-
-        var expectedErrorCalls = [
-          ["Sync failed, errors:"],
-          ["  GetStuff:"],
-          ["    " + logger.red("✘") + " something"],
-        ]
-        expect(spyConsoleLog.mock.calls).toEqual(expectedLogCalls)
-        expect(spyConsoleError.mock.calls).toEqual(expectedErrorCalls)
+        expect(spyConsoleLog.mock.calls).toMatchSnapshot()
+        expect(spyConsoleError.mock.calls).toMatchSnapshot()
         jest.clearAllMocks();
       })
     })
@@ -304,23 +240,12 @@ describe("sync operations", () => {
 
       var syncPromise = sync(options)
 
-      var expectedLogCalls = [
-        ["Syncing 2 operations to " + logger.bright("http://example.com/stored_operations/sync") + "..."],
-      ]
-      expect(spyConsoleLog.mock.calls).toEqual(expectedLogCalls)
+      expect(spyConsoleLog.mock.calls).toMatchSnapshot()
       jest.clearAllMocks();
 
       return syncPromise.then(() => {
-        var expectedLogCalls = [
-          ["  " + logger.green("1 added")],
-          ["  " + logger.reset("2 not modified")],
-          ["  " + logger.dim("0 failed")],
-          ["Generating client module in " + logger.bright("OperationStoreClient.js") + "..."],
-          [logger.green("✓ Done!")],
-        ]
-        var expectedErrorCalls = []
-        expect(spyConsoleLog.mock.calls).toEqual(expectedLogCalls)
-        expect(spyConsoleError.mock.calls).toEqual(expectedErrorCalls)
+        expect(spyConsoleLog.mock.calls).toMatchSnapshot()
+        expect(spyConsoleError.mock.calls).toMatchSnapshot()
         jest.clearAllMocks();
       })
     })
