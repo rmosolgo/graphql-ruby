10bad131198b559a29aa20a2d817a522ee2e4c41
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2017-09-20 00:17:52 -0400

    Update prepareProjectTest to use snapshots (simplifies verification)
    Jest provides a snapshot verification mechanism, which simplifies
    comparing expects to actuals.

--- javascript_client/sync/__tests__/prepareProjectTest.js
+++ javascript_client/sync/__tests__/prepareProjectTest.js
@@ -9,21 +9,8 @@ describe("merging a project", () => {
       "./__tests__/project/frag_2.graphql",
       "./__tests__/project/frag_3.graphql",
     ]
-    var ops = prepareProject(filenames)
-
-    var expectedOps = [
-      {
-        body: 'query GetStuff2 {\n  stuff\n  ...Frag1\n  ...Frag2\n}\n\nfragment Frag1 on Query {\n  moreStuff\n}\n\nfragment Frag2 on Query {\n  ...Frag3\n}\n\nfragment Frag3 on Query {\n  evenMoreStuff\n}\n',
-        name: 'GetStuff2',
-        alias: null,
-      },
-      {
-        body: 'query GetStuff {\n  ...Frag1\n}\n\nfragment Frag1 on Query {\n  moreStuff\n}\n',
-        name: 'GetStuff',
-        alias: null,
-      }
-    ]
-    expect(ops).toEqual(expectedOps)
+    var ops = prepareProject(filenames, false)
+    expect(ops).toMatchSnapshot()
   })
 
   it("blows up on duplicate names", () => {
