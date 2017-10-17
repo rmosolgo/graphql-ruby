ae659c2dc67ad2d0bfd6a9ba78b6a30d63918f15
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-17 15:28:06 -0400

    Annotate the example a bit

--- spec/support/jazz.rb
+++ spec/support/jazz.rb
@@ -1,29 +1,33 @@
 # frozen_string_literal: true
+
+# Here's the "application"
 module Jazz
+  # Here are some classes that the application deals with
   module Models
     Ensemble = Struct.new(:name)
     Instrument = Struct.new(:name)
   end
 
+  # Here's a new-style GraphQL type definition
   class Ensemble < GraphQL::Object
     model Models::Ensemble
     description "A group of musicians playing together"
     field :name, "String", null: false
   end
 
+  # Lives side-by-side with an old-style definition
   InstrumentType = GraphQL::ObjectType.define do
     name "Instrument"
     field :name, !types.String
   end
 
+  # Another new-style definition, with method overrides
   class Query < GraphQL::Object
     field :ensembles, "[Ensemble]"
     field :instruments, "[Instrument]"
 
     def ensembles
-      [
-        Models::Ensemble.new("Bela Fleck and the Flecktones"),
-      ]
+      [Models::Ensemble.new("Bela Fleck and the Flecktones")]
     end
 
     def instruments
@@ -31,11 +35,13 @@ module Jazz
     end
   end
 
+  # New-style Schema definition
   class Schema < GraphQL::Schema
     query(Query)
     namespace(Jazz)
   end
 
-  # Prep the schema
+  # Prep the schema, now a required step,
+  # but can be rebooted during Rails development
   Schema.boot
 end
