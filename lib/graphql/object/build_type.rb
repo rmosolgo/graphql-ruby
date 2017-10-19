e72fe2935e5ea3224b772f05e63e51ccb1882656
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-19 14:27:04 -0400

    Don't use a deterministic resolve; support direct-reference field types

--- lib/graphql/object/build_type.rb
+++ lib/graphql/object/build_type.rb
@@ -10,13 +10,13 @@ module GraphQL
       # @param schema [GraphQL::Schema]
       # @param graphql_obj_class [GraphQL::Object]
       # @return [GraphQL::ObjectType]
-      def build_object_type(schema, graphql_obj_class)
+      def build_object_type(graphql_obj_class)
         obj_type = GraphQL::ObjectType.define do
           name(graphql_obj_class.graphql_type_name)
           description(graphql_obj_class.description)
           interfaces(graphql_obj_class.interfaces)
           graphql_obj_class.fields.each do |field_inst|
-            field_defn = field_inst.to_graphql(schema: schema)
+            field_defn = field_inst.to_graphql
             # Based on the return type of the field, determine whether
             # we should wrap it with connection helpers or not.
             field_defn_method = if field_defn.type.unwrap.name =~ /Connection\Z/
@@ -32,38 +32,58 @@ module GraphQL
         end
       end
 
-      def parse_type(schema, type_expr, null:)
+      # @param type_expr
+      # @return [GraphQL::BaseType]
+      def parse_type(type_expr, null:)
         list_type = false
-        if type_expr.is_a?(Array)
-          list_type = true
-          type_expr = type_expr.first
-        end
 
-        type_name = to_type_name(type_expr)
-        return_type = case type_name
-        when "String"
-          GraphQL::STRING_TYPE
-        when "Int"
-          GraphQL::INT_TYPE
-        when "Float"
-          GraphQL::FLOAT_TYPE
-        when "Boolean"
-          GraphQL::BOOLEAN_TYPE
-        when "ID"
-          GraphQL::BOOLEAN_TYPE
-        when /\A\[.*\]\Z/
+        return_type = case type_expr
+        when String
+          case type_expr
+          when "String"
+            GraphQL::STRING_TYPE
+          when "Int"
+            GraphQL::INT_TYPE
+          when "Float"
+            GraphQL::FLOAT_TYPE
+          when "Boolean"
+            GraphQL::BOOLEAN_TYPE
+          when "ID"
+            GraphQL::BOOLEAN_TYPE
+          when /\A\[.*\]\Z/
+            list_type = true
+            parse_type(type_expr[1..-2], null: true)
+          when /.*!\Z/
+            null = false
+            parse_type(type_expr[1..-2], null: true)
+          else
+            maybe_type = Object.const_get(type_expr)
+            case maybe_type
+            when GraphQL::BaseType
+              maybe_type
+            when Class
+              if maybe_type < GraphQL::Object
+                maybe_type.to_graphql
+              else
+                raise "Unexpected class found for GraphQL type: #{type_expr} (must be GraphQL::Object)"
+              end
+            end
+          end
+        when GraphQL::BaseType
+          type_expr
+        when Array
+          if type_expr.length != 1
+            raise "Use an array of length = 1 for list types; other arrays are not supported"
+          end
           list_type = true
-          parse_type(schema, type_name[1..-2], null: true)
-        when /.*!\Z/
-          null = false
-          parse_type(schema, type_name[1..-2], null: true)
-        else
-          schema.find_type(type_name)
-        end
-
-        # TODO This isn't going to handle circular dependencies
-        if return_type.is_a?(Class) && return_type < GraphQL::Object
-          return_type = return_type.to_graphql(schema: schema)
+          parse_type(type_expr.first, null: true)
+        when Class
+          if Class < GraphQL::Object
+            type_expr.to_graphql
+          else
+            # Eg `String` => GraphQL::STRING_TYPE
+            parse_type(type_expr.name, null: true)
+          end
         end
 
         if !null
@@ -79,15 +99,24 @@ module GraphQL
 
       def to_type_name(something)
         case something
+        when GraphQL::BaseType
+          something.name
         when Array
           to_type_name(something.first)
         when Module
-          something.name
+          something.name.split("::").last
         when String
-          something
+          something.gsub(/\]\[\!/, "").split("::").last
         else
           raise "Unhandled to_type_name input: #{something} (#{something.class})"
         end
+      end
+
+      def underscore(string)
+        string
+          .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2') # URLDecoder -> URL_Decoder
+          .gsub(/([a-z\d])([A-Z])/,'\1_\2')     # someThing -> some_Thing
+          .downcase
       end
     end
   end
