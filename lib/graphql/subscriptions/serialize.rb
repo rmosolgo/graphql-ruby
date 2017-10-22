d7bb70dd18d17cf9eae119a0ba88340b85ff9044
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-22 09:20:22 -0400

    refactor(Serialize) make some methods private

--- lib/graphql/subscriptions/serialize.rb
+++ lib/graphql/subscriptions/serialize.rb
@@ -15,40 +15,10 @@ module GraphQL
         load_value(parsed_obj)
       end
 
-      # @param value [Object] A parsed JSON object
-      # @return [Object] An object that load Golbal::Identification recursive
-      def load_value(value)
-        if value.is_a?(Array)
-          value.map{|item| load_value(item)}
-        elsif value.is_a?(Hash)
-          if value.size == 1 && value.key?(GLOBALID_KEY)
-            GlobalID::Locator.locate(value[GLOBALID_KEY])
-          else
-            Hash[value.map{|k, v| [k, load_value(v)]}]
-          end
-        else
-          value
-        end
-      end
-
       # @param obj [Object] Some subscription-related data to dump
       # @return [String] The stringified object
       def dump(obj)
         JSON.generate(dump_value(obj), quirks_mode: true)
-      end
-
-      # @param obj [Object] Some subscription-related data to dump
-      # @return [Object] The object that converted Global::Identification
-      def dump_value(obj)
-        if obj.is_a?(Array)
-          obj.map{|item| dump_value(item)}
-        elsif obj.is_a?(Hash)
-          Hash[obj.map{|k, v| [k, dump_value(v)]}]
-        elsif obj.respond_to?(:to_gid_param)
-          {GLOBALID_KEY => obj.to_gid_param}
-        else
-          obj
-        end
       end
 
       # This is for turning objects into subscription scopes.
@@ -67,6 +37,40 @@ module GraphQL
           obj.to_param
         else
           obj.to_s
+        end
+      end
+
+      class << self
+        private
+
+        # @param value [Object] A parsed JSON object
+        # @return [Object] An object that load Global::Identification recursive
+        def load_value(value)
+          if value.is_a?(Array)
+            value.map{|item| load_value(item)}
+          elsif value.is_a?(Hash)
+            if value.size == 1 && value.key?(GLOBALID_KEY)
+              GlobalID::Locator.locate(value[GLOBALID_KEY])
+            else
+              Hash[value.map{|k, v| [k, load_value(v)]}]
+            end
+          else
+            value
+          end
+        end
+
+        # @param obj [Object] Some subscription-related data to dump
+        # @return [Object] The object that converted Global::Identification
+        def dump_value(obj)
+          if obj.is_a?(Array)
+            obj.map{|item| dump_value(item)}
+          elsif obj.is_a?(Hash)
+            Hash[obj.map{|k, v| [k, dump_value(v)]}]
+          elsif obj.respond_to?(:to_gid_param)
+            {GLOBALID_KEY => obj.to_gid_param}
+          else
+            obj
+          end
         end
       end
     end
