de9bff7c66f55b723d390b7dc1e11f2bcac6728f
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-23 15:57:47 -0700

    refactor(GraphQL::Object) don't use .define inside #to_graphql

--- lib/graphql/object/field.rb
+++ lib/graphql/object/field.rb
@@ -18,39 +18,30 @@ module GraphQL
 
       # @return [GraphQL::Field]
       def to_graphql
-        # read ivars to cope with instance_eval in .define {...}
-        field_name = @name
-        return_type_expr = @return_type_expr
-        return_type_null = @return_type_null
-        desc = @description
-        depr_reason = @deprecation_reason
-        return_type_name = BuildType.to_type_name(return_type_expr)
+        return_type_name = BuildType.to_type_name(@return_type_expr)
         connection = return_type_name.end_with?("Connection")
         method_name = @method || BuildType.underscore(@name)
-        args_block = @args_block
 
-        field_defn = GraphQL::Field.define do
-          name(field_name)
-          type(-> {
-            Object::BuildType.parse_type(return_type_expr, null: return_type_null)
-          })
-          description(desc)
-          deprecation_reason(depr_reason)
-          resolve(GraphQL::Object::Resolvers::Dynamic.new({
-            method_name: method_name,
-          }))
-          # apply this first, so it can be overriden below
-          if connection
-            FieldProxy.new(self).instance_eval do
-              argument :after, "String", "Returns the elements in the list that come after the specified global ID.", null: true
-              argument :before, "String", "Returns the elements in the list that come before the specified global ID.", null: true
-              argument :first, "Int", "Returns the first _n_ elements from the list.", null: true
-              argument :last, "Int", "Returns the last _n_ elements from the list.", null: true
-            end
-          end
-          if args_block
-            FieldProxy.new(self).instance_eval(&args_block)
-          end
+        field_defn = GraphQL::Field.new
+        field_defn.name = @name
+        field_defn.type = -> {
+          Object::BuildType.parse_type(@return_type_expr, null: @return_type_null)
+        }
+        field_defn.description = @description
+        field_defn.deprecation_reason = @deprecation_reason
+        field_defn.resolve = GraphQL::Object::Resolvers::Dynamic.new({
+          method_name: method_name,
+        })
+        # apply this first, so it can be overriden below
+        if connection
+          conn_args = FieldProxy.new(field_defn)
+          conn_args.argument :after, "String", "Returns the elements in the list that come after the specified global ID.", null: true
+          conn_args.argument :before, "String", "Returns the elements in the list that come before the specified global ID.", null: true
+          conn_args.argument :first, "Int", "Returns the first _n_ elements from the list.", null: true
+          conn_args.argument :last, "Int", "Returns the last _n_ elements from the list.", null: true
+        end
+        if @args_block
+          FieldProxy.new(field_defn).instance_eval(&@args_block)
         end
 
         field_defn
@@ -58,25 +49,27 @@ module GraphQL
 
 
       class FieldProxy
-        def initialize(defn)
-          @defn = defn
+        def initialize(field)
+          @field = field
         end
 
         def argument(arg_name, type_expr, desc = nil, null:, default_value: :__no_default__)
+          arg_name = arg_name.to_s
           default_value_was_provided = default_value != :__no_default__
           # Rename to avoid naming conflict below
           provided_default_value = default_value
 
-          @defn.argument do
-            name(arg_name.to_s)
-            type(-> {
-              Object::BuildType.parse_type(type_expr, null: null)
-            })
-            description(desc)
-            if default_value_was_provided
-              default_value(provided_default_value)
-            end
+          argument = GraphQL::Argument.new
+          argument.name = arg_name
+          argument.type = -> {
+            Object::BuildType.parse_type(type_expr, null: null)
+          }
+          argument.description = desc
+          if default_value_was_provided
+            argument.default_value = provided_default_value
           end
+
+          @field.arguments[arg_name] = argument
         end
       end
 
