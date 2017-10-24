1e8b4848b5ae9221e2df5a5cc79eebb8ff35b2b5
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-24 10:37:46 -0700

    Support overriding .field_class

--- spec/graphql/object_spec.rb
+++ spec/graphql/object_spec.rb
@@ -7,7 +7,7 @@ describe GraphQL::Object do
     it "tells type data" do
       assert_equal "Ensemble", object_class.graphql_name
       assert_equal "A group of musicians playing together", object_class.description
-      assert_equal 3, object_class.fields.size
+      assert_equal 4, object_class.fields.size
       # Compatibility methods are delegated to the underlying BaseType
       assert object_class.respond_to?(:connection_type)
     end
@@ -18,7 +18,7 @@ describe GraphQL::Object do
     it "returns a matching GraphQL::ObjectType" do
       assert_equal "Ensemble", obj_type.name
       assert_equal "A group of musicians playing together", obj_type.description
-      assert_equal 3, obj_type.all_fields.size
+      assert_equal 4, obj_type.all_fields.size
 
       name_field = obj_type.all_fields[1]
       assert_equal "name", name_field.name
@@ -28,6 +28,17 @@ describe GraphQL::Object do
 
     it "has a custom implementation" do
       assert_equal obj_type.metadata[:config], :configged
+    end
+
+    it "uses the custom field class" do
+      query_str = <<-GRAPHQL
+      {
+        ensembles { upcaseName }
+      }
+      GRAPHQL
+
+      res = Jazz::Schema.execute(query_str)
+      assert_equal ["BELA FLECK AND THE FLECKTONES"], res["data"]["ensembles"].map { |e| e["upcaseName"] }
     end
   end
 
