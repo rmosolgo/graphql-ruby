96c6a5be747a521f41c75f0ef415944ef40cd08a
Robert Mosolgo <rdmosolgo@gmail.com>
2017-10-24 10:37:46 -0700

    Support overriding .field_class

--- lib/graphql/interface.rb
+++ lib/graphql/interface.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 module GraphQL
   class Interface < GraphQL::SchemaMember
+    Field = GraphQL::Object::Field
     class << self
-      # Define a field on this object
-      def field(*args, &block)
-        fields << GraphQL::Object::Field.new(*args, &block)
-      end
-
-      # Fields defined on this class
-      # TODO should this inherit?
-      def fields
-        @fields ||= []
-      end
+      include GraphQL::SchemaMember::HasFields
 
       # Inherited methods go here
       def implemented(&block)
@@ -23,26 +15,14 @@ module GraphQL
       end
 
       def to_graphql
-        @to_graphql ||= begin
-          interface_class = self
-          GraphQL::InterfaceType.define do
-            name(interface_class.graphql_name)
-            description(interface_class.description)
-            # TODO dedup with object
-            interface_class.fields.each do |field_inst|
-              field_defn = field_inst.graphql_definition
-              # Based on the return type of the field, determine whether
-              # we should wrap it with connection helpers or not.
-              field_defn_method = if field_defn.type.unwrap.name =~ /Connection\Z/
-                :connection
-              else
-                :field
-              end
-              field_name = field_defn.name
-              public_send(field_defn_method, field_name, field: field_defn)
-            end
-          end
+        type_defn = GraphQL::InterfaceType.new
+        type_defn.name = graphql_name
+        type_defn.description = description
+        fields.each do |field_inst|
+          field_defn = field_inst.graphql_definition
+          type_defn.fields[field_defn.name] = field_defn
         end
+        type_defn
       end
     end
   end
