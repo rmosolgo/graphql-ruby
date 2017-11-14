89bab2d03fb2c4247b979cd2fadb673de2f5a9c3
Robert Mosolgo <rdmosolgo@gmail.com>
2017-11-14 10:05:28 -0500

    Add custom arguments API

---
+++ spec/graphql/schema/field_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+describe GraphQL::Schema::Field do
+  describe "graphql definition" do
+    let(:object_class) { Jazz::Query }
+    let(:field) { object_class.fields.find { |f| f.name == "find" } }
+
+    it "uses the argument class" do
+      arg_defn = field.graphql_definition.arguments.values.first
+      assert_equal :ok, arg_defn.metadata[:custom]
+    end
+  end
+end
