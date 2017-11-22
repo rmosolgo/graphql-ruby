60fa0c729658083fb90009967c4f3f7e6ff08c45
willian <mail@willian.io>
2017-11-22 11:37:20 +0100

    null should be null not not null :D

--- lib/graphql/railtie.rb
+++ lib/graphql/railtie.rb
@@ -6,20 +6,20 @@ module GraphQL
   class Railtie < Rails::Railtie
     rake_tasks do
       namespace :graphql do
-        task :update, [:dir] do |t, args|
+        task :upgrade, [:dir] do |t, args|
           unless (dir = args[:dir])
             fail 'You have to give me a directory where your GraphQL schema and types live. ' \
-             'For example: `bin/rake graphql:update[app/graphql/**/*]`'
+             'For example: `bin/rake graphql:upgrade[app/graphql/**/*]`'
           end
 
           Dir[dir].each do |file|
             # Members (types, interfaces, etc.)
             if file =~ /.*_(type|interface|enum|union|)\.rb$/
-              transformer = GraphQL::Upgrader::Member.new File.read(file)
-              next unless transformer.transformable?
+              upgrader = GraphQL::Upgrader::Member.new File.read(file)
+              next unless upgrader.upgradeable?
 
               puts "- Transforming #{file}"
-              File.open(file, 'w') { |f| f.write transformer.transform }
+              File.open(file, 'w') { |f| f.write upgrader.upgrade }
             end
           end
         end
