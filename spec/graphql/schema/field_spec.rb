8f1c9923b5bdfb44174000167e1f863e1234515d
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2017-11-23 12:39:31 -0500

    Camelcase field names by default
    This allows fields to be defined with underscored names while keeping
    them camelCased in the schema.

--- spec/graphql/schema/field_spec.rb
+++ spec/graphql/schema/field_spec.rb
@@ -4,11 +4,15 @@ require "spec_helper"
 describe GraphQL::Schema::Field do
   describe "graphql definition" do
     let(:object_class) { Jazz::Query }
-    let(:field) { object_class.fields.find { |f| f.name == "find" } }
+    let(:field) { object_class.fields.find { |f| f.name == "inspect_input" } }
 
     it "uses the argument class" do
       arg_defn = field.graphql_definition.arguments.values.first
       assert_equal :ok, arg_defn.metadata[:custom]
+    end
+
+    it "camelizes the field name" do
+      assert_equal 'inspectInput', field.graphql_definition.name
     end
   end
 end
