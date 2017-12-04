5cb38842fa41aa28b22457ee683b4109860a5ff1
Robert Mosolgo <rdmosolgo@gmail.com>
2017-12-04 15:27:25 -0500

    Convert Introspection to class-based API

--- lib/graphql/introspection/of_type_field.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-GraphQL::Introspection::OfTypeField = GraphQL::Field.define do
-  name "ofType"
-  introspection true
-  type -> { GraphQL::Introspection::TypeType }
-  resolve ->(obj, args, ctx) { obj.kind.wraps? ? obj.of_type : nil }
-end
