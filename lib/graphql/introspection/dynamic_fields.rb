37d5c1b3ed7eb2869a7105d95aba0269a4ae8b64
Robert Mosolgo <rdmosolgo@gmail.com>
2017-12-09 11:49:47 -0500

    feat(Introspection) implement custom dynamic fields

--- lib/graphql/introspection/dynamic_fields.rb
+++ lib/graphql/introspection/dynamic_fields.rb
@@ -2,15 +2,9 @@
 module GraphQL
   module Introspection
     class DynamicFields < GraphQL::Schema::Object
-      field :__typename, String, "The name of this type", null: false
-      def __typename(field_ctx:)
-        # TODO how to get the irep_node?
-        # - Add something to the `field` signature, like
-        #   extra: [:irep_node, :ast_node]
-        # - Auto-detect extra args in the method signature?
-        # - Add something to dynamic resolve, where it uses a class?
-        irep_node = ??
-        irep_node.owner_type
+      field :__typename, String, "The name of this type", null: false, extras: [:irep_node]
+      def __typename(irep_node:)
+        irep_node.owner_type.name
       end
     end
   end
