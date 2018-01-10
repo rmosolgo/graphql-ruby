bb7e2b6f36d8960ee9284c516dade79d7922313f
Robert Mosolgo <rdmosolgo@gmail.com>
2018-01-10 10:41:23 -0500

    Remove proc wrapper which isn't supported in class API

--- spec/fixtures/upgrader/blame_range.transformed.rb
+++ spec/fixtures/upgrader/blame_range.transformed.rb
@@ -19,7 +19,7 @@ module Platform
         @object.lines.first[:lineno] + (@object.lines.length - 1)
       end
 
-      field :commit, -> { Objects::Commit }, description: "Identifies the line author", null: false
+      field :commit, Objects::Commit, description: "Identifies the line author", null: false
 
       field :age, Integer, method: :scale, description: "Identifies the recency of the change, from 1 (new) to 10 (old). This is calculated as a 2-quantile and determines the length of distance between the median age of all the changes in the file and the recency of the current range's change.", null: false
     end
