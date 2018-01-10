f5ea1a5fc30480244500fb5170fd882ad4869921
Robert Mosolgo <rdmosolgo@gmail.com>
2018-01-10 11:13:32 -0500

    Add type factory upgrade example

---
+++ spec/fixtures/upgrader/type_x.transformed.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Platform
+  module Objects
+    class X < Platform::Objects::Base
+      model_name "X"
+      description "An x on a y."
+      visibility :internal
+      minimum_accepted_scopes ["z"]
+
+      global_id_field :id
+      interfaces [GraphQL::Relay::Node.interface]
+
+      field :f1, Objects::O1, "The x being y.", null: false
+      field :f2, Enums::E1, "x for the y.", method: :field_2, null: false
+      field :f3, Enums::E2, "x for y.", null: true
+      field :details, String, "Details.", null: true
+
+      field :f4, Objects::O2, "x as a y inside the z.", null: false
+
+      def f4
+        Class1.new(
+          a: Class2.new(
+            b: @object.b_1,
+            c: @object.c_1
+          ),
+          d: Class3.new(
+            b: @object.b_2,
+            c: @object.c_3,
+          )
+        )
+      end
+    end
+  end
+end
