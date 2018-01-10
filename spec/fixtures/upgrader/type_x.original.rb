5a68c9581c2a2696cc930d02a65158677914b4c3
Robert Mosolgo <rdmosolgo@gmail.com>
2018-01-10 11:13:32 -0500

    Add type factory upgrade example

---
+++ spec/fixtures/upgrader/type_x.original.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Platform
+  module Objects
+    X = define_active_record_type(-> { ::X }) do
+      name "X"
+      description "An x on a y."
+      visibility :internal
+      minimum_accepted_scopes ["z"]
+
+      global_id_field :id
+      interfaces [GraphQL::Relay::Node.interface]
+
+      field :f1, !Objects::O1, "The x being y."
+      field :f2, !Enums::E1, "x for the y.", property: :field_2
+      field :f3, Enums::E2, "x for y."
+      field :details, types.String, "Details."
+
+      field :f4, !Objects::O2, "x as a y inside the z." do
+        resolve -> (obj_x, arguments, context) do
+          Class1.new(
+            a: Class2.new(
+              b: obj_x.b_1,
+              c: obj_x.c_1
+            ),
+            d: Class3.new(
+              b: obj_x.b_2,
+              c: obj_x.c_3,
+            )
+          )
+        end
+      end
+    end
+  end
+end
