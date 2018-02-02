61c5bd3e2acdbaed7c493765c5fac811f9a84a34
Robert Mosolgo <rdmosolgo@gmail.com>
2018-02-02 16:16:23 -0500

    Test enum

---
+++ spec/fixtures/upgrader/gist_order_field.original.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Platform
+  module Enums
+    GistOrderField = GraphQL::EnumType.define do
+      name "GistOrderField"
+      description "Properties by which gist connections can be ordered."
+
+      value "CREATED_AT", "Order gists by creation time", value: "created_at"
+      value "UPDATED_AT", "Order gists by update time", value: "updated_at"
+      value "PUSHED_AT", "Order gists by push time", value: "pushed_at"
+    end
+  end
+end
