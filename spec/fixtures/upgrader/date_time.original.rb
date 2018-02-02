b55b254d9a53058c8c5884cd7ac53e3f7ca168bc
Robert Mosolgo <rdmosolgo@gmail.com>
2018-02-02 12:23:12 -0500

    Fix lint error

--- spec/fixtures/upgrader/date_time.original.rb
+++ spec/fixtures/upgrader/date_time.original.rb
@@ -6,14 +6,16 @@ module Platform
       name "DateTime"
       description "An ISO-8601 encoded UTC date string."
 
+      # rubocop:disable Layout/SpaceInLambdaLiteral
       coerce_input -> (value, context) do
         begin
           Time.iso8601(value)
         rescue ArgumentError, ::TypeError
         end
       end
+      # rubocop:enable Layout/SpaceInLambdaLiteral
 
-      coerce_result -> (value, context) do
+      coerce_result ->(value, context) do
         return nil unless value
         value.utc.iso8601
       end
