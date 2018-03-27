233999d1df750f0bd256585c90b48aea043c81c1
Joseph An <joseph.an@thescore.com>
2018-03-27 14:42:54 -0400

    Change deprecation arg from camel case to snake case

--- lib/graphql/introspection/fields_field.rb
+++ lib/graphql/introspection/fields_field.rb
@@ -6,7 +6,7 @@ GraphQL::Introspection::FieldsField = GraphQL::Field.define do
   resolve ->(object, arguments, context) {
     return nil if !object.kind.fields?
     fields = context.warden.fields(object)
-    if !arguments["includeDeprecated"]
+    if !arguments["include_deprecated"]
       fields = fields.select {|f| !f.deprecation_reason }
     end
     fields.sort_by(&:name)
