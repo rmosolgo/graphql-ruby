ed9d0d10ece740c623ca4db8970a285b027c8fb2
Christian Joudrey <cmallette@gmail.com>
2018-06-04 22:53:53 -0400

    Validate directive argument names are unique

--- lib/graphql/static_validation/rules/argument_names_are_unique.rb
+++ lib/graphql/static_validation/rules/argument_names_are_unique.rb
@@ -6,17 +6,25 @@ module GraphQL
 
       def validate(context)
         context.visitor[GraphQL::Language::Nodes::Field] << ->(node, parent) {
-          argument_defns = node.arguments
-          if argument_defns.any?
-            args_by_name = Hash.new { |h, k| h[k] = [] }
-            argument_defns.each { |a| args_by_name[a.name] << a }
-            args_by_name.each do |name, defns|
-              if defns.size > 1
-                context.errors << message("There can be only one argument named \"#{name}\"", defns, context: context)
-              end
+          validate_arguments(node, context)
+        }
+
+        context.visitor[GraphQL::Language::Nodes::Directive] << ->(node, parent) {
+          validate_arguments(node, context)
+        }
+      end
+
+      def validate_arguments(node, context)
+        argument_defns = node.arguments
+        if argument_defns.any?
+          args_by_name = Hash.new { |h, k| h[k] = [] }
+          argument_defns.each { |a| args_by_name[a.name] << a }
+          args_by_name.each do |name, defns|
+            if defns.size > 1
+              context.errors << message("There can be only one argument named \"#{name}\"", defns, context: context)
             end
           end
-        }
+        end
       end
     end
   end
