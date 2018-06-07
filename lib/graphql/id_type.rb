cae5f0f7ce2abcd1043cfd62fe2e395ed3d14e6b
Robert Mosolgo <rdmosolgo@gmail.com>
2018-06-07 12:26:48 -0700

    Move all scalars to classes

--- lib/graphql/id_type.rb
+++ lib/graphql/id_type.rb
@@ -1,5 +1,3 @@
 # frozen_string_literal: true
 require "graphql/types/id"
-module GraphQL
-  ID_TYPE = GraphQL::Types::ID.graphql_definition
-end
+GraphQL::ID_TYPE = GraphQL::Types::ID.graphql_definition
