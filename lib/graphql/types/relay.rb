4373779f8463a1c7e2e1afccccaa9b15be116c4c
Robert Mosolgo <rdmosolgo@gmail.com>
2018-06-08 14:50:12 -0400

    Add class-based Relay types

---
+++ lib/graphql/types/relay.rb
@@ -0,0 +1,4 @@
+require "graphql/types/relay/page_info"
+require "graphql/types/relay/base_connection"
+require "graphql/types/relay/base_edge"
+require "graphql/types/relay/node"
---
+++ lib/graphql/types/relay/node.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module GraphQL
+  module Types
+    module Relay
+      # This can be used for Relay's `Node` interface,
+      # or you can take it as inspiration for your own implementation
+      # of the `Node` interface.
+      module Node
+        description "An object with an ID."
+        field(:id, ID, null: false, description: "ID of the object.")
+        # TODO Should I implement `id` here to call the schema's hook?
+      end
+    end
+  end
+end
