7cc18c9554f9cb8b46c32c785f12440e8c47ddf7
Eddy Tan <dweichn@gmail.com>
2018-06-29 02:59:22 +0700

    #1385 - better handling of boolean attributes

--- lib/graphql/name_validator.rb
+++ lib/graphql/name_validator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module GraphQL
   class NameValidator
-    VALID_NAME_REGEX = /^[_a-zA-Z][_a-zA-Z0-9]*$/
+    VALID_NAME_REGEX = /^[_a-zA-Z][_a-zA-Z0-9]*\??$/
 
     def self.validate!(name)
       raise GraphQL::InvalidNameError.new(name, VALID_NAME_REGEX) unless valid?(name)
