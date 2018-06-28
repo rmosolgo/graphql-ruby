f867bacd4fa8206109afb9fc4b3b46661ea495fe
Eddy Tan <dweichn@gmail.com>
2018-06-29 02:51:49 +0700

    fix Boolean field type declaration in spec

--- spec/graphql/schema/field_spec.rb
+++ spec/graphql/schema/field_spec.rb
@@ -29,8 +29,9 @@ describe GraphQL::Schema::Field do
       assert_equal 'underscored_arg', arg_defn.name
     end
 
+    focus
     it "removes `?` name suffix but preserve it in method name" do
-      boolean_field = GraphQL::Schema::Field.from_options(:boolean_field?, Boolean, null: false, camelize: false, owner: nil)
+      boolean_field = GraphQL::Schema::Field.from_options(:boolean_field?, GraphQL::Types::Boolean, null: false, camelize: false, owner: nil)
       assert_equal 'boolean_field', boolean_field.to_graphql.name
       assert_equal 'boolean_field?', boolean_field.method_str
       assert_equal :boolean_field?, boolean_field.method_sym
