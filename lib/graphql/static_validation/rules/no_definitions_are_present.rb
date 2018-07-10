68c56143662f7e86f3696c67e2a1bd9da2d87e23
Robert Mosolgo <rdmosolgo@gmail.com>
2018-07-10 11:45:02 -0400

    update no_definitions_are_present for extension methods

--- lib/graphql/static_validation/rules/no_definitions_are_present.rb
+++ lib/graphql/static_validation/rules/no_definitions_are_present.rb
@@ -13,7 +13,6 @@ module GraphQL
         @schema_definition_nodes << node
       end
 
-      # TODO Add extensions
       alias :on_directive_definition :on_invalid_node
       alias :on_schema_definition :on_invalid_node
       alias :on_scalar_type_definition :on_invalid_node
@@ -22,6 +21,13 @@ module GraphQL
       alias :on_interface_type_definition :on_invalid_node
       alias :on_union_type_definition :on_invalid_node
       alias :on_enum_type_definition :on_invalid_node
+      alias :on_schema_extension :on_invalid_node
+      alias :on_scalar_type_extension :on_invalid_node
+      alias :on_object_type_extension :on_invalid_node
+      alias :on_input_object_type_extension :on_invalid_node
+      alias :on_interface_type_extension :on_invalid_node
+      alias :on_union_type_extension :on_invalid_node
+      alias :on_enum_type_extension :on_invalid_node
 
       def on_document(node, parent)
         super
