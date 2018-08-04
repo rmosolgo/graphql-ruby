9901e94a959d2f8b4d5b6e9472af30711af6ebc1
Hibariya <hibariya@gmail.com>
2018-08-04 18:37:10 +0900

    Remove duplicated method definitions from spec/, too
    * Remove duplicated methods
    * Remove CustomPrinter from toplevel
    * Move the GlobalIDUser class into spec/support/global_id.rb

---
+++ spec/support/global_id.rb
@@ -0,0 +1,21 @@
+if defined?(GlobalID)
+  GlobalID.app = "graphql-ruby-test"
+
+  class GlobalIDUser
+    include GlobalID::Identification
+
+    attr_reader :id
+
+    def initialize(id)
+      @id = id
+    end
+
+    def self.find(id)
+      GlobalIDUser.new(id)
+    end
+
+    def ==(that)
+      self.id == that.id
+    end
+  end
+end
