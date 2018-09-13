7e2150df29c5c7b1b75784c89067c257e6fc987f
Hibariya <hibariya@gmail.com>
2018-09-14 06:31:07 +0900

    Help people figure out what to go next

--- spec/integration/rails/generators/graphql/scalar_generator_spec.rb
+++ spec/integration/rails/generators/graphql/scalar_generator_spec.rb
@@ -10,13 +10,13 @@ class GraphQLGeneratorsScalarGeneratorTest < BaseGeneratorTest
 module Types
   class DateType < Types::BaseScalar
     def self.coerce_input(input_value, context)
-      # TODO: implement me
-      raise NotImplementedError
+      # Override this to prepare a client-provided GraphQL value for your Ruby code
+      input_value
     end
 
     def self.coerce_result(ruby_value, context)
-      # TODO: implement me
-      raise NotImplementedError
+      # Override this to serialize a Ruby value for the GraphQL response
+      ruby_value.to_s
     end
   end
 end
