3286c910e91130973f8bccaeb029258ab7e78d79
Robert Mosolgo <rdmosolgo@gmail.com>
2018-09-28 14:53:28 -0500

    Keep a reference to trace instead of passing it everywhere

--- lib/graphql/execution/interpreter/visitor.rb
+++ lib/graphql/execution/interpreter/visitor.rb
@@ -12,27 +12,30 @@ module GraphQL
       # I think it would be even better if we could somehow make
       # `continue_field` not recursive. "Trampolining" it somehow.
       class Visitor
+        attr_reader :trace
+
         def visit(trace)
+          @trace = trace
           root_operation = trace.query.selected_operation
           root_type = trace.schema.root_type_for_operation(root_operation.operation_type || "query")
           root_type = root_type.metadata[:type_class]
           object_proxy = root_type.authorized_new(trace.query.root_value, trace.query.context)
 
           path = []
-          evaluate_selections(path, object_proxy, root_type, root_operation.selections, trace)
+          evaluate_selections(path, object_proxy, root_type, root_operation.selections)
         end
 
-        def gather_selections(owner_type, selections, trace, selections_by_name)
+        def gather_selections(owner_type, selections, selections_by_name)
           selections.each do |node|
             case node
             when GraphQL::Language::Nodes::Field
-              if passes_skip_and_include?(trace, node)
+              if passes_skip_and_include?(node)
                 response_key = node.alias || node.name
                 s = selections_by_name[response_key] ||= []
                 s << node
               end
             when GraphQL::Language::Nodes::InlineFragment
-              if passes_skip_and_include?(trace, node)
+              if passes_skip_and_include?(node)
                 include_fragmment = if node.type
                   type_defn = trace.schema.types[node.type.name]
                   type_defn = type_defn.metadata[:type_class]
@@ -42,17 +45,17 @@ module GraphQL
                   true
                 end
                 if include_fragmment
-                  gather_selections(owner_type, node.selections, trace, selections_by_name)
+                  gather_selections(owner_type, node.selections, selections_by_name)
                 end
               end
             when GraphQL::Language::Nodes::FragmentSpread
-              if passes_skip_and_include?(trace, node)
+              if passes_skip_and_include?(node)
                 fragment_def = trace.query.fragments[node.name]
                 type_defn = trace.schema.types[fragment_def.type.name]
                 type_defn = type_defn.metadata[:type_class]
                 possible_types = trace.schema.possible_types(type_defn).map { |t| t.metadata[:type_class] }
                 if possible_types.include?(owner_type)
-                  gather_selections(owner_type, fragment_def.selections, trace, selections_by_name)
+                  gather_selections(owner_type, fragment_def.selections, selections_by_name)
                 end
               end
             else
@@ -61,10 +64,10 @@ module GraphQL
           end
         end
 
-        def evaluate_selections(path, owner_object, owner_type, selections, trace)
+        def evaluate_selections(path, owner_object, owner_type, selections)
           selections_by_name = {}
-          owner_type = resolve_if_late_bound_type(owner_type, trace)
-          gather_selections(owner_type, selections, trace, selections_by_name)
+          owner_type = resolve_if_late_bound_type(owner_type)
+          gather_selections(owner_type, selections, selections_by_name)
           selections_by_name.each do |result_name, fields|
             ast_node = fields.first
             field_name = ast_node.name
@@ -87,7 +90,7 @@ module GraphQL
               field_defn = field_defn.metadata[:type_class]
             end
 
-            return_type = resolve_if_late_bound_type(field_defn.type, trace)
+            return_type = resolve_if_late_bound_type(field_defn.type)
 
             next_path = [*path, result_name]
             # TODO this seems janky, but we need to know
@@ -113,17 +116,17 @@ module GraphQL
               app_result = field_defn.resolve_field_2(object, kwarg_arguments, trace.context)
 
               trace.after_lazy(app_result) do |inner_result|
-                if continue_value(next_path, inner_result, field_defn, return_type, ast_node, trace)
+                if continue_value(next_path, inner_result, field_defn, return_type, ast_node)
                   # TODO will this be a perf issue for scalar fields?
                   next_selections = fields.map(&:selections).inject(&:+)
-                  continue_field(next_path, inner_result, field_defn, return_type, ast_node, trace, next_selections)
+                  continue_field(next_path, inner_result, field_defn, return_type, ast_node, next_selections)
                 end
               end
             end
           end
         end
 
-        def continue_value(path, value, field, as_type, ast_node, trace)
+        def continue_value(path, value, field, as_type, ast_node)
           if value.nil? || value.is_a?(GraphQL::ExecutionError)
             if value.nil?
               if as_type.non_null?
@@ -152,8 +155,8 @@ module GraphQL
           end
         end
 
-        def continue_field(path, value, field, type, ast_node, trace, next_selections)
-          type = resolve_if_late_bound_type(type, trace)
+        def continue_field(path, value, field, type, ast_node, next_selections)
+          type = resolve_if_late_bound_type(type)
 
           case type.kind
           when TypeKinds::SCALAR, TypeKinds::ENUM
@@ -170,14 +173,14 @@ module GraphQL
               trace.write(path, nil, propagating_nil: field.type.non_null?)
             else
               resolved_type = resolved_type.metadata[:type_class]
-              continue_field(path, value, field, resolved_type, ast_node, trace, next_selections)
+              continue_field(path, value, field, resolved_type, ast_node, next_selections)
             end
           when TypeKinds::OBJECT
             object_proxy = type.authorized_new(value, trace.query.context)
             trace.after_lazy(object_proxy) do |inner_object|
-              if continue_value(path, inner_object, field, type, ast_node, trace)
+              if continue_value(path, inner_object, field, type, ast_node)
                 trace.write(path, {})
-                evaluate_selections(path, inner_object, type, next_selections, trace)
+                evaluate_selections(path, inner_object, type, next_selections)
               end
             end
           when TypeKinds::LIST
@@ -187,8 +190,8 @@ module GraphQL
               next_path = [*path, idx]
               trace.set_type_at_path(next_path, inner_type)
               trace.after_lazy(inner_value) do |inner_inner_value|
-                if continue_value(next_path, inner_inner_value, field, inner_type, ast_node, trace)
-                  continue_field(next_path, inner_inner_value, field, inner_type, ast_node, trace, next_selections)
+                if continue_value(next_path, inner_inner_value, field, inner_type, ast_node)
+                  continue_field(next_path, inner_inner_value, field, inner_type, ast_node, next_selections)
                 end
               end
             end
@@ -196,13 +199,13 @@ module GraphQL
             inner_type = type.of_type
             # Don't `set_type_at_path` because we want the static type,
             # we're going to use that to determine whether a `nil` should be propagated or not.
-            continue_field(path, value, field, inner_type, ast_node, trace, next_selections)
+            continue_field(path, value, field, inner_type, ast_node, next_selections)
           else
             raise "Invariant: Unhandled type kind #{type.kind} (#{type})"
           end
         end
 
-        def passes_skip_and_include?(trace, node)
+        def passes_skip_and_include?(node)
           # TODO call out to directive here
           node.directives.each do |dir|
             dir_defn = trace.schema.directives.fetch(dir.name)
@@ -215,7 +218,7 @@ module GraphQL
           true
         end
 
-        def resolve_if_late_bound_type(type, trace)
+        def resolve_if_late_bound_type(type)
           if type.is_a?(GraphQL::Schema::LateBoundType)
             trace.query.warden.get_type(type.name).metadata[:type_class]
           else
