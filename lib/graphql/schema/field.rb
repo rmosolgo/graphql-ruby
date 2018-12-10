54c2c4673e01c1eab11641afa0146f9267506b9a
Ellis Berner <bernee@alum.rpi.edu>
2018-12-10 14:16:33 -0800

    Revert "Remove needless nil check"
    This reverts commit 843a7bc3ad62b92e37a729222473b50ab423c0b3.

--- lib/graphql/schema/field.rb
+++ lib/graphql/schema/field.rb
@@ -342,7 +342,7 @@ module GraphQL
         ctx.schema.after_lazy(obj) do |after_obj|
           # First, apply auth ...
           query_ctx = ctx.query.context
-          inner_obj = after_obj.object
+          inner_obj = after_obj && after_obj.object
           if authorized?(inner_obj, query_ctx) && arguments.each_value.all? { |a| a.authorized?(inner_obj, query_ctx) }
             # Then if it passed, resolve the field
             v = if @resolve_proc
