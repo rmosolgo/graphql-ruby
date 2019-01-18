09ad6a9eda4f475733c18ba696144ff2e07d715d
Robert Mosolgo <rdmosolgo@gmail.com>
2019-01-18 15:36:13 -0500

    Start on test schema

--- spec/graphql/schema/subscription_spec.rb
+++ spec/graphql/schema/subscription_spec.rb
@@ -2,6 +2,84 @@
 require "spec_helper"
 
 describe GraphQL::Schema::Subscription do
+  class SubscriptionFieldSchema < GraphQL::Subscription
+    TOOTS = []
+    ALL_USERS = {
+      "dhh" => {handle: "dhh"}
+      "matz" => {handle: "matz"}
+      "_why" => {handle: "_why", private: true}
+    }
+
+    USERS = {}
+
+    class Toot < GraphQL::Schema::Object
+      field :handle, String, null: false
+      field :body, String, null: false
+    end
+
+    class Query < GraphQL::Schema::Object
+      field :toots, [Toot], null: false
+
+      def toots
+        TOOTS
+      end
+    end
+
+    class BaseSubscription < GraphQL::Schema::Resolver
+    end
+
+    class TootWasTooted < BaseSubscription
+      argument :handle, String, required: true, loads: true, as: :user
+      field :toot, Toot, null: false
+
+      # Can't subscribe to private users
+      def authorized?(user:)
+        !user[:private]
+      end
+
+      # TODO maybe this is default
+      def subscribe(user:)
+        :no_response
+      end
+
+      def update(user:)
+        if context[:viewer] == user
+          # don't update for one's own toots
+          :no_update
+        else
+          super
+        end
+      end
+    end
+
+    class Subscription < GraphQL::Schema::Object
+      field :toot_was_tooted, subscription: TootWasTooted
+    end
+
+    class Mutation < GraphQL::Schema::Object
+      field :toot, Toot, null: false do
+        argument :body, String, required: true
+      end
+
+      def toot(body:)
+        handle = context[:viewer][:handle]
+        toot = { handle: handle, body: body }
+        TOOTS << toot
+        SubscriptionFieldSchema.trigger(:toot_was_tooted, {handle: handle}, toot)
+      end
+    end
+
+    query(Query)
+    mutation(Mutation)
+    subscription(Subscription)
+  end
+
+  before do
+    # Reset databases
+    SubscriptionFieldSchema::TOOTS.clear
+    SubscriptionFieldSchema::USERS.merge!(SubscriptionFieldSchema::ALL_USERS)
+  end
+
   it "generates a return type"
   it "can use a premade `payload_type`"
 
