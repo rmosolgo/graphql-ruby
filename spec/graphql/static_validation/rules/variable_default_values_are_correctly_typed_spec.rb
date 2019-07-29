61fbc549a2c9d77bb9a8ab0e25887b48b32043f9
Chris Herbst <chris.herbst@dailypay.com>
2019-07-29 16:22:56 -0400

    Fix Int/Float typecast issue.

--- spec/graphql/static_validation/rules/variable_default_values_are_correctly_typed_spec.rb
+++ spec/graphql/static_validation/rules/variable_default_values_are_correctly_typed_spec.rb
@@ -7,7 +7,6 @@ describe GraphQL::StaticValidation::VariableDefaultValuesAreCorrectlyTyped do
   let(:query_string) {%|
     query getCheese(
       $id:        Int = 1,
-      $int:       Int = 3.4e24, # can be coerced
       $str:       String!,
       $badInt:    Int = "abc",
       $input:     DairyProductInput = {source: YAK, fatContent: 1},
@@ -15,7 +14,6 @@ describe GraphQL::StaticValidation::VariableDefaultValuesAreCorrectlyTyped do
       $nonNull:  Int! = 1,
     ) {
       cheese1: cheese(id: $id) { source }
-      cheese4: cheese(id: $int) { source }
       cheese2: cheese(id: $badInt) { source }
       cheese3: cheese(id: $nonNull) { source }
       search1: searchDairy(product: [$input]) { __typename }
@@ -28,19 +26,19 @@ describe GraphQL::StaticValidation::VariableDefaultValuesAreCorrectlyTyped do
     expected = [
       {
         "message"=>"Default value for $badInt doesn't match type Int",
-        "locations"=>[{"line"=>6, "column"=>7}],
+        "locations"=>[{"line"=>5, "column"=>7}],
         "path"=>["query getCheese"],
         "extensions"=>{"code"=>"defaultValueInvalidType", "variableName"=>"badInt", "typeName"=>"Int"}
       },
       {
         "message"=>"Default value for $badInput doesn't match type DairyProductInput",
-        "locations"=>[{"line"=>8, "column"=>7}],
+        "locations"=>[{"line"=>7, "column"=>7}],
         "path"=>["query getCheese"],
         "extensions"=>{"code"=>"defaultValueInvalidType", "variableName"=>"badInput", "typeName"=>"DairyProductInput"}
       },
       {
         "message"=>"Non-null variable $nonNull can't have a default value",
-        "locations"=>[{"line"=>9, "column"=>7}],
+        "locations"=>[{"line"=>8, "column"=>7}],
         "path"=>["query getCheese"],
         "extensions"=>{"code"=>"defaultValueInvalidOnNonNullVariable", "variableName"=>"nonNull"}
       }
