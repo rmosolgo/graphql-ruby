4a175b34f393075360aa9ec00417e5ac09ce5622
Robert Mosolgo <rdmosolgo@gmail.com>
2019-11-20 08:54:30 -0600

    Fix MaxQueryDepth when used with a Multiplex
    Co-authored-by: Patrick Vice <patrickgvice@gmail.com>

--- lib/graphql/analysis/ast.rb
+++ lib/graphql/analysis/ast.rb
@@ -23,7 +23,7 @@ module GraphQL
       #
       # @param multiplex [GraphQL::Execution::Multiplex]
       # @param analyzers [Array<GraphQL::Analysis::AST::Analyzer>]
-      # @return [void]
+      # @return [Array<Any>] Results from multiplex analyzers
       def analyze_multiplex(multiplex, analyzers)
         multiplex_analyzers = analyzers.map { |analyzer| analyzer.new(multiplex) }
 
@@ -46,8 +46,8 @@ module GraphQL
           multiplex.queries.each_with_index do |query, idx|
             query.analysis_errors = multiplex_errors + analysis_errors(query_results[idx])
           end
+          multiplex_results
         end
-        nil
       end
 
       # @param query [GraphQL::Query]
