802ee68851c0815051acf86c49a8ff643ddebf2c
Newton Garcia <newxhost@gmail.com>
2019-12-02 14:32:03 -0300

    GraphQL::Schema::InputObject explicit type check

--- lib/graphql/schema/argument.rb
+++ lib/graphql/schema/argument.rb
@@ -153,11 +153,7 @@ module GraphQL
       # Used by the runtime.
       # @api private
       def prepare_value(obj, value, context: nil)
-        # NOTE: This fixes the issue on queries.
-        # QUESTION: Should an InputObject #prepare method be called and then
-        # later the obj#prepare (GraphQL::Schema::Object) be called upon the
-        # InputObject prepared value? Or should it be one OR the another?
-        if value.respond_to?(:prepare)
+        if value.is_a?(GraphQL::Schema::InputObject)
           value = value.prepare
         end
 
