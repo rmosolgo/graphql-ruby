9ecc89b928e62f7e629cd82d5964bd656b6a7525
Robert Mosolgo <rdmosolgo@gmail.com>
2020-04-16 17:16:04 -0400

    Fix for ruby 2.2

--- lib/graphql/execution/interpreter/runtime.rb
+++ lib/graphql/execution/interpreter/runtime.rb
@@ -348,7 +348,8 @@ module GraphQL
                 end
               end
             rescue NoMethodError => err
-              if err.name == :each && err.receiver == value
+              # Ruby 2.2 doesn't have NoMethodError#receiver, can't check that one in this case. (It's been EOL since 2017.)
+              if err.name == :each && (err.respond_to?(:receiver) ? err.receiver == value : true)
                 # This happens when the GraphQL schema doesn't match the implementation. Help the dev debug.
                 raise ListResultFailedError.new(value: value, field: field, path: path)
               else
