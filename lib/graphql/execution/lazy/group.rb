2bcd87d390be67858688428d81b1b44cb823f7f9
Robert Mosolgo <rdmosolgo@gmail.com>
2020-09-24 13:31:20 -0400

    Fix Lazy.all returning nested lazies

--- lib/graphql/execution/lazy/group.rb
+++ lib/graphql/execution/lazy/group.rb
@@ -14,14 +14,22 @@ module GraphQL
         def wait
           if !@waited
             @waited = true
+            any_lazies = false
             results = @maybe_lazies.map { |maybe_lazy|
               if maybe_lazy.respond_to?(:wait)
                 maybe_lazy.wait
-                maybe_lazy.value
+                res = maybe_lazy.value
+                any_lazies ||= res.is_a?(Lazy)
+                res
               else
                 maybe_lazy
               end
             }
+
+            if any_lazies
+              results = Lazy.all(results)
+            end
+
             lazy.fulfill(results)
           end
         end
