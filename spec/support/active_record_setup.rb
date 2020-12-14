09eeffcf30be4d271810b22d7b265134139bc26d
Robert Mosolgo <rdmosolgo@gmail.com>
2020-12-14 14:43:26 -0500

    More postgres setup

--- spec/support/active_record_setup.rb
+++ spec/support/active_record_setup.rb
@@ -16,9 +16,10 @@ if testing_rails?
     ActiveRecord::Base.establish_connection(
       adapter: "postgresql",
       username: "postgres",
+      password: ENV["POSTGRES_PASSWORD"], # empty in development, populated for GH Actions
       database: "graphql_ruby_test"
     )
-    SequelDB = Sequel.connect('postgres://postgres:@localhost:5432/graphql_ruby_test')
+    SequelDB = Sequel.connect("postgres://postgres:#{ENV["POSTGRES_PASSWORD"]}@localhost:5432/graphql_ruby_test")
   else
     ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./_test_.db")
     SequelDB = Sequel.sqlite("./_test_.db")
