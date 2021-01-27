5133a157e951b13531be38092b319546bca4cd3b
Robert Mosolgo <rdmosolgo@gmail.com>
2021-01-27 11:01:37 -0500

    fix legacy constant names

--- lib/graphql/define/assign_global_id_field.rb
+++ lib/graphql/define/assign_global_id_field.rb
@@ -4,7 +4,7 @@ module GraphQL
     module AssignGlobalIdField
       def self.call(type_defn, field_name, **field_kwargs)
         resolve = GraphQL::Relay::GlobalIdResolve.new(type: type_defn)
-        GraphQL::Define::AssignObjectField.call(type_defn, field_name, **field_kwargs, type: GraphQL::Deprecation_ID_TYPE.to_non_null_type, resolve: resolve)
+        GraphQL::Define::AssignObjectField.call(type_defn, field_name, **field_kwargs, type: GraphQL::DEPRECATED_ID_TYPE.to_non_null_type, resolve: resolve)
       end
     end
   end
