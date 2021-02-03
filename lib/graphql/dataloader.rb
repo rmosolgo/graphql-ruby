7e4f664f26b8e6d766f90624427db3ae19358827
Robert Mosolgo <rdmosolgo@gmail.com>
2021-02-03 10:04:49 -0500

    Add implementation notes to the new code

--- lib/graphql/dataloader.rb
+++ lib/graphql/dataloader.rb
@@ -59,12 +59,32 @@ module GraphQL
 
     # @api private Nothing to see here
     def append_job(&job)
+      # Given a block, queue it up to be worked through when `#run` is called.
+      # (If the dataloader is already running, than a Fiber will pick this up later.)
       @pending_jobs.push(job)
       nil
     end
 
     # @api private Move along, move along
     def run
+      # At a high level, the algorithm is:
+      #
+      #  A) Inside Fibers, run jobs from the queue one-by-one
+      #    - When one of the jobs yields to the dataloader (`Fiber.yield`), then that fiber will pause
+      #    - In that case, if there are still pending jobs, a new Fiber will be created to run jobs
+      #    - Continue until all jobs have been _started_ by a Fiber. (Any number of those Fibers may be waiting to be resumed, after their data is loaded)
+      #  B) Once all known jobs have been run until they are complete or paused for data, run all pending data sources.
+      #    - Similarly, create a Fiber to consume pending sources and tell them to load their data.
+      #    - If one of those Fibers pauses, then create a new Fiber to continue working through remaining pending sources.
+      #    - When a source causes another source to become pending, run the newly-pending source _first_, since it's a dependency of the previous one.
+      #  C) After all pending sources have been completely loaded (there are no more pending sources), resume any Fibers that were waiting for data.
+      #    - Those Fibers assume that source caches will have been populated with the data they were waiting for.
+      #    - Those Fibers may request data from a source again, in which case they will yeilded and be added to a new pending fiber list.
+      #  D) Once all pending fibers have been resumed once, return to `A` above.
+      #
+      # For whatever reason, the best implementation I could find was to order the steps `[D, A, B, C]`, with a special case for skipping `D`
+      # on the first pass. I just couldn't find a better way to write the loops in a way that was DRY and easy to read.
+      #
       pending_fibers = []
       next_fibers = []
       first_pass = true
@@ -73,6 +93,8 @@ module GraphQL
         if first_pass
           first_pass = false
         else
+          # These fibers were previously waiting for sources to load data,
+          # resume them. (They might wait again, in which case, re-enqueue them.)
           f.resume
           if f.alive?
             next_fibers << f
@@ -80,16 +102,19 @@ module GraphQL
         end
 
         while @pending_jobs.any?
+          # Create a Fiber to consume jobs until one of the jobs yields
+          # or jobs run out
           f = Fiber.new {
             while (job = @pending_jobs.shift)
               job.call
             end
           }
-          # Run it until it yields or the jobs run out
           result = f.resume
           if result.is_a?(StandardError)
             raise result
           end
+          # In this case, the job yielded. Queue it up to run again after
+          # we load whatever it's waiting for.
           if f.alive?
             next_fibers << f
           end
@@ -109,6 +134,9 @@ module GraphQL
           end
 
           if source_fiber_stack
+            # Use a stack with `.pop` here so that when a source causes another source to become pending,
+            # that newly-pending source will run _before_ the one that depends on it.
+            # (See below where the old fiber is pushed to the stack, then the new fiber is pushed on the stack.)
             while (outer_source_fiber = source_fiber_stack.pop)
               result = outer_source_fiber.resume
               if result.is_a?(StandardError)
@@ -125,6 +153,9 @@ module GraphQL
               end
             end
           end
+          # Move newly-enqueued Fibers on to the list to be resumed.
+          # Clear out the list of next-round Fibers, so that
+          # any Fibers that pause can be put on it.
           pending_fibers.concat(next_fibers)
           next_fibers.clear
         end
@@ -158,6 +189,14 @@ module GraphQL
       end
 
       if pending_sources
+        # By passing the whole array into this Fiber, it's possible that we set ourselves up for a bunch of no-ops.
+        # For example, if you have sources `[a, b, c]`, and `a` is loaded, then `b` yields to wait for `d`, then
+        # the next fiber would be dispatched with `[c, d]`. It would fulfill `c`, then `d`, then eventually
+        # the previous fiber would start up again. `c` would no longer be pending, but it would still receive `.run_pending_keys`.
+        # That method is short-circuited since it isn't pending any more, but it's still a waste.
+        #
+        # This design could probably be improved by maintaining a `@pending_sources` queue which is shared by the fibers,
+        # similar to `@pending_jobs`. That way, when a fiber is resumed, it would never pick up work that was finished by a different fiber.
         source_fiber = Fiber.new do
           pending_sources.each(&:run_pending_keys)
         end
