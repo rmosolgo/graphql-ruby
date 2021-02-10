c579503a8f541ea4ee5451d3e615bc031f81ad74
Robert Mosolgo <rdmosolgo@gmail.com>
2021-02-10 08:55:21 -0500

    update tests

--- javascript_client/src/__tests__/cliTest.ts
+++ javascript_client/src/__tests__/cliTest.ts
@@ -3,11 +3,11 @@ var childProcess = require("child_process")
 describe("CLI", () => {
   it("exits 1 on error", () => {
     expect(() => {
-      childProcess.execSync("node ./dist/cli.js sync", {stdio: "pipe"})
+      childProcess.execSync("node ./cli.js sync", {stdio: "pipe"})
     }).toThrow("Client name must be provided for sync")
   })
 
   it("exits 0 on OK", () => {
-    childProcess.execSync("node ./dist/cli.js sync -h", {stdio: "pipe"})
+    childProcess.execSync("node ./cli.js sync -h", {stdio: "pipe"})
   })
 })
