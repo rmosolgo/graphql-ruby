21571690e8ef978f9ea694c7e2da3a8089c9d9ff
Robert Mosolgo <rdmosolgo@gmail.com>
2021-02-23 11:56:48 -0500

    Add arguments reference to new connection classes

--- spec/graphql/schema/field/connection_extension_spec.rb
+++ spec/graphql/schema/field/connection_extension_spec.rb
@@ -4,12 +4,29 @@ require "spec_helper"
 describe GraphQL::Schema::Field::ConnectionExtension do
   class ConnectionShortcutSchema < GraphQL::Schema
     class ShortcutResolveExtension < GraphQL::Schema::FieldExtension
-      def resolve(**rest)
-        ["a", "b", "c", "d", "e"]
+      def resolve(arguments:, **rest)
+        collection = ["a", "b", "c", "d", "e"]
+        if (filter = arguments[:starting_with])
+          collection.select! { |x| x.start_with?(filter) }
+        end
+        collection
       end
     end
+
+    class CustomStringConnection < GraphQL::Types::Relay::BaseConnection
+      edge_type(GraphQL::Types::String.edge_type)
+      field :argument_data, [String], null: false
+
+      def argument_data
+        [object.arguments.class.name, *object.arguments.keys.map(&:inspect)]
+      end
+    end
+
     class Query < GraphQL::Schema::Object
-      field :names, GraphQL::Types::String.connection_type, null: false, extensions: [ShortcutResolveExtension]
+      field :names, CustomStringConnection, null: false, extensions: [ShortcutResolveExtension] do
+        argument :starting_with, String, required: false
+      end
+
       def names
         raise "This should never be called"
       end
@@ -21,5 +38,12 @@ describe GraphQL::Schema::Field::ConnectionExtension do
   it "implements connection handling even when resolve is shortcutted" do
     res = ConnectionShortcutSchema.execute("{ names(first: 2) { nodes } }")
     assert_equal ["a", "b"], res["data"]["names"]["nodes"]
+  end
+
+  it "assigns arguments to the connection instance" do
+    res = ConnectionShortcutSchema.execute("{ names(first: 2, startingWith: \"a\") { nodes argumentData } }")
+    assert_equal ["a"], res["data"]["names"]["nodes"]
+    # This come through as symbols
+    assert_equal ["Hash", ":first", ":starting_with"], res["data"]["names"]["argumentData"]
   end
 end
