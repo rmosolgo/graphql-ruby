2ba566427dd356a856f4cb70d8eecff6f346fb0f
coding-chimp <xarfai27@gmail.com>
2021-03-27 13:26:49 +0100

    Optimize ActiveRecordRelationConnection#has_next_page

--- lib/graphql/pagination/active_record_relation_connection.rb
+++ lib/graphql/pagination/active_record_relation_connection.rb
@@ -5,6 +5,26 @@ module GraphQL
   module Pagination
     # Customizes `RelationConnection` to work with `ActiveRecord::Relation`s.
     class ActiveRecordRelationConnection < Pagination::RelationConnection
+      def has_next_page
+        if @has_next_page.nil?
+          @has_next_page = if before_offset && before_offset > 0
+            true
+          elsif first
+            if @nodes && @nodes.count < first
+              false
+            else
+              initial_offset = sliced_nodes.offset_value || 0
+              sliced_nodes.offset(initial_offset + first).exists?
+            end
+          else
+            false
+          end
+        end
+        @has_next_page
+      end
+
+      private
+
       def relation_count(relation)
         int_or_hash = if relation.respond_to?(:unscope)
           relation.unscope(:order).count(:all)
