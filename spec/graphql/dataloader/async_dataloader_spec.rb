1a9ed4ff08f36658d118d3debdc32301d4be0b4e
Robert Mosolgo <rdmosolgo@gmail.com>
2021-05-21 10:34:39 -0400

    Add failing test for running fields and dataloader sources in branch-independent, evented flow

--- spec/graphql/dataloader/async_dataloader_spec.rb
+++ spec/graphql/dataloader/async_dataloader_spec.rb
@@ -12,6 +12,20 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
         end
       end
 
+      class WaitForSource < GraphQL::Dataloader::Source
+        def initialize(tag)
+          @tag = tag
+        end
+
+        def fetch(waits)
+          max_wait = waits.max
+          puts "[#{Time.now.to_f}] Waiting #{max_wait} for #{@tag}"
+          `sleep #{max_wait}`
+          puts "[#{Time.now.to_f}] Finished for #{@tag}"
+          waits.map { |_w| @tag }
+        end
+      end
+
       class Sleeper < GraphQL::Schema::Object
         field :sleeper, Sleeper, null: false, resolver_method: :sleep do
           argument :duration, Float, required: true
@@ -26,6 +40,22 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
         def duration; object; end
       end
 
+      class Waiter < GraphQL::Schema::Object
+        field :wait_for, Waiter, null: false do
+          argument :tag, String, required: true
+          argument :wait, Float, required: true
+        end
+
+        def wait_for(tag:, wait:)
+          dataloader.with(WaitForSource, tag).load(wait)
+        end
+
+        field :tag, String, null: false
+        def tag
+          object
+        end
+      end
+
       class Query < GraphQL::Schema::Object
         field :sleep, Float, null: false do
           argument :duration, Float, required: true
@@ -38,6 +68,15 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
         def sleep(duration:)
           `sleep #{duration}`
           duration
+        end
+
+        field :wait_for, Waiter, null: false do
+          argument :tag, String, required: true
+          argument :wait, Float, required: true
+        end
+
+        def wait_for(tag:, wait:)
+          dataloader.with(WaitForSource, tag).load(wait)
         end
       end
 
@@ -127,6 +166,52 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
       }
       assert_equal expected_data, res["data"]
       assert_in_delta 0.3, ended_at - started_at, 0.05, "Fields ran without any waiting"
+    end
+
+    it "runs dataloaders in parallel across branches" do
+      query_str = <<-GRAPHQL
+      {
+        w1: waitFor(tag: "a", wait: 0.2) {
+          waitFor(tag: "b", wait: 0.2) {
+            waitFor(tag: "c", wait: 0.2) {
+              tag
+            }
+          }
+        }
+        # After the first, these are returned eagerly from cache
+        w2: waitFor(tag: "a", wait: 0.2) {
+          waitFor(tag: "a", wait: 0.2) {
+            waitFor(tag: "a", wait: 0.2) {
+              tag
+            }
+          }
+        }
+        w3: waitFor(tag: "a", wait: 0.2) {
+          waitFor(tag: "b", wait: 0.2) {
+            waitFor(tag: "d", wait: 0.2) {
+              tag
+            }
+          }
+        }
+        w4: waitFor(tag: "e", wait: 0.6) {
+          tag
+        }
+      }
+      GRAPHQL
+      started_at = Time.now
+      res = AsyncSchema.execute(query_str)
+      ended_at = Time.now
+
+      expected_data = {
+        "w1" => { "waitFor" => { "waitFor" => { "tag" => "c" } } },
+        "w2" => { "waitFor" => { "waitFor" => { "tag" => "a" } } },
+        "w3" => { "waitFor" => { "waitFor" => { "tag" => "d" } } },
+        "w4" => { "tag" => "e" }
+      }
+      assert_equal expected_data, res["data"]
+      # TODO: currently, it's not resuming execution _until_ all dataloaders are finished.
+      # The two calls to scheduler.run have to be united into one, called at the very end of `#run`
+      assert_in_delta 0.6, ended_at - started_at, 0.05, "Sources were executed in parallel"
     end
   end
 end
