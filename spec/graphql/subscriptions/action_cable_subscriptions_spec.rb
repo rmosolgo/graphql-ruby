e453f3031a92fdf2625bd3f21319aa309d997aef
Robert Mosolgo <rmosolgo@github.com>
2021-08-02 09:52:00 -0400

    Add multi-tenant support for subscriptions

--- spec/graphql/subscriptions/action_cable_subscriptions_spec.rb
+++ spec/graphql/subscriptions/action_cable_subscriptions_spec.rb
@@ -155,4 +155,134 @@ describe GraphQL::Subscriptions::ActionCableSubscriptions do
     res = ActionCableTestSchema.subscriptions.execute_update("nonsense-id", {}, {})
     assert_nil res
   end
+
+  if defined?(GlobalID)
+    class MultiTenantSchema < GraphQL::Schema
+      module Data
+        class Player
+          include GlobalID::Identification
+
+          attr_reader :name, :id
+
+          def initialize(id, name)
+            @id = id
+            @name = name
+          end
+
+          def self.find(id)
+            Data.find(id)
+          end
+        end
+
+        OBJECTS_BY_TENANT = {
+          "tenant-1" => { 1 => Player.new(1, "player-1") },
+          "tenant-2" => { 2 => Player.new(2, "player-2") },
+        }
+
+        def self.find(id)
+          if @current_tenant
+            id = id.to_i # It's stringified by GlobalId
+            @current_tenant[id] || raise("Didn't find `#{id.inspect}` in #{@current_tenant}")
+          else
+            raise("Use .switch to pick a tenant first")
+          end
+        end
+
+        def self.switch(tenant)
+          @current_tenant = OBJECTS_BY_TENANT.fetch(tenant)
+          yield
+        ensure
+          @current_tenant = nil
+        end
+      end
+
+      class Player < GraphQL::Schema::Object
+        field :name, String, null: false
+      end
+
+      class PointScored < GraphQL::Schema::Subscription
+        field :score, Int, null: false
+        field :player, Player, null: false
+        subscription_scope :tenant
+
+        def update
+          {
+            score: object[:score],
+            player: object[:player] || Data.find(object[:player_id])
+          }
+        end
+      end
+
+      class Subscription < GraphQL::Schema::Object
+        field :point_scored, subscription: PointScored
+      end
+
+      class TenantTracer
+        def self.trace(event, data)
+          case event
+          when "execute_multiplex"
+            tenant = data[:multiplex].queries.first.context[:tenant]
+            Data.switch(tenant) do
+              yield
+            end
+          else
+            yield
+          end
+        end
+      end
+
+      query(Player)
+      subscription(Subscription)
+      tracer(TenantTracer)
+
+      module Serialize
+        def self.load(message, ctx)
+          Data.switch(ctx[:tenant]) do
+            GraphQL::Subscriptions::Serialize.load(message)
+          end
+        end
+
+        def self.dump(obj)
+          GraphQL::Subscriptions::Serialize.dump(obj)
+        end
+      end
+
+      use GraphQL::Subscriptions::ActionCableSubscriptions,
+        action_cable: MockActionCable,
+        action_cable_coder: JSON,
+        serializer: Serialize
+    end
+
+    it "works with multi-tenant architecture" do
+      mock_channel_1 = MockActionCable.get_mock_channel
+      ctx_1 = { channel: mock_channel_1, tenant: "tenant-1" }
+      MultiTenantSchema.execute("subscription { pointScored { score player { name } } }", context: ctx_1)
+
+      mock_channel_2 = MockActionCable.get_mock_channel
+      ctx_2 = { channel: mock_channel_2, tenant: "tenant-2" }
+      MultiTenantSchema.execute("subscription { pointScored { score player { name } } }", context: ctx_2)
+      # This will use the `.find` in `def update`:
+      MultiTenantSchema.subscriptions.trigger(:point_scored, {}, { score: 5, player_id: 1 }, scope: "tenant-1")
+      # This will use GlobalId in `Serialize`:
+      MultiTenantSchema.subscriptions.trigger(:point_scored, {}, { score: 3, player: MultiTenantSchema::Data::Player.new(2, nil) }, scope: "tenant-2")
+
+
+      expected_msg_1 = subscription_update({
+        "pointScored" => {
+          "score" => 5,
+          "player" => { "name" => "player-1" },
+        }
+      })
+
+      expected_msg_2 = subscription_update({
+        "pointScored" => {
+          "score" => 3,
+          "player" => { "name" => "player-2" }
+        },
+      })
+
+      assert_equal [expected_msg_1], mock_channel_1.mock_broadcasted_messages
+      assert_equal [expected_msg_2], mock_channel_2.mock_broadcasted_messages
+    end
+  end
 end
