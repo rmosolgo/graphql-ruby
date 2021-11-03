3f8499be68b37fdb1a685690b5111df43833362e
Robert Mosolgo <rmosolgo@github.com>
2021-11-03 10:33:15 -0400

    Oops, skip libev_scheduler on truffle ruby instead

--- spec/graphql/dataloader/async_dataloader_spec.rb
+++ spec/graphql/dataloader/async_dataloader_spec.rb
@@ -240,18 +240,18 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
       include AsyncDataloaderAssertions
     end
 
-    describe "With libev_scheduler" do
-      require "libev_scheduler"
-      let(:scheduler_class) { Libev::Scheduler }
-      include AsyncDataloaderAssertions
-    end
-
     if RUBY_ENGINE == "ruby"
-      describe "with evt" do
-        require "evt"
-        let(:scheduler_class) { Evt::Scheduler }
+      describe "With libev_scheduler" do
+        require "libev_scheduler"
+        let(:scheduler_class) { Libev::Scheduler }
         include AsyncDataloaderAssertions
       end
+    end
+
+    describe "with evt" do
+      require "evt"
+      let(:scheduler_class) { Evt::Scheduler }
+      include AsyncDataloaderAssertions
     end
   end
 end
