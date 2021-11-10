ced7c5018419624c48fbcdb4354ef534b7ceb016
Robert Mosolgo <rmosolgo@github.com>
2021-11-10 09:40:12 -0500

    Tell Sources to wait when one dataloader.yield didn't finish the load

---
+++ spec/graphql/dataloader/source_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+describe GraphQL::Dataloader::Source do
+  class FailsToLoadSource < GraphQL::Dataloader::Source
+    def fetch(keys)
+      dataloader.with(FailsToLoadSource).load_all(keys)
+    end
+  end
+
+  it "raises an error when it tries too many times to sync" do
+    dl = GraphQL::Dataloader.new
+    dl.append_job { dl.with(FailsToLoadSource).load(1) }
+    err = assert_raises RuntimeError do
+      dl.run
+    end
+    expected_message = "FailsToLoadSource#sync tried 1000 times to load pending keys ([1]), but they still weren't loaded. There is likely a circular dependency."
+    assert_equal expected_message, err.message
+  end
+end
