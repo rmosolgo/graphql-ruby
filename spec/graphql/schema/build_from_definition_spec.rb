acb9cffe9a7a75f0e9f6ae4186544ab78e385047
Joel Warrington <joel.w@getjobber.com>
2021-11-30 10:22:59 -0700

    test(BuildFromDefinition): added test to confirm changes to handle frozen strings in Lexer

--- spec/graphql/schema/build_from_definition_spec.rb
+++ spec/graphql/schema/build_from_definition_spec.rb
@@ -233,6 +233,17 @@ union U = Hello
       assert_equal 63, built_schema.types["U"].ast_node.definition_line
     end
 
+    it 'supports descriptions and definition_line' do
+      schema = <<-SCHEMA
+"""
+"""
+type Query {
+  f1: Int
+}
+      SCHEMA
+      refute_nil GraphQL::Schema.from_definition(schema)
+    end
+
     it 'maintains built-in directives' do
       schema = <<-SCHEMA
 schema {
