c7f3912848896544e47992d4ff6cf695d4d5f82e
Robert Mosolgo <rdmosolgo@gmail.com>
2022-01-04 11:07:23 -0500

    Get the head ref, not the merge branch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ if testing_coverage? && !ENV["TEST"]
           `git config --global user.name "GraphQL-Ruby CI"`
           `git config --global user.email "<>"`
         end
-        current_branch = ENV["GITHUB_REF"].sub("refs/heads/", "")
+        current_branch = ENV["GITHUB_HEAD_REF"].sub("refs/heads/", "")
         `git checkout #{current_branch}`
         current_sha = `git rev-parse HEAD`.chomp
         new_branch = "update-artifacts-on-#{current_branch}-#{current_sha[0, 10]}"
