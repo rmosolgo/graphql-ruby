d28924eacac1d22b0580260c2016ea20228c1a3f
Robert Mosolgo <rdmosolgo@gmail.com>
2022-01-11 12:08:24 -0500

    Fix crash on NodeField

--- spec/graphql/types/relay/has_node_field_spec.rb
+++ spec/graphql/types/relay/has_node_field_spec.rb
@@ -17,4 +17,29 @@ describe GraphQL::Types::Relay::HasNodeField do
     assert_equal query, nodes_field.owner
     assert_equal query, nodes_field.owner_type
   end
+
+  it "warns when accessing legacy classes" do
+    result = nil
+    stdout, stderr = capture_io do
+      result = GraphQL::Types::Relay::NodeField
+    end
+
+    assert_equal GraphQL::Types::Relay::DeprecatedNodeField, result
+    assert_equal "", stdout
+    expected_warning = "NodeField is deprecated, use `include GraphQL::Types::Relay::HasNodeField` instead.
+(referenced from /Users/rmosolgo/code/graphql-ruby/spec/graphql/types/relay/has_node_field_spec.rb:24:in `block (3 levels) in <top (required)>')
+"
+    assert_equal expected_warning, stderr
+
+    stdout, stderr = capture_io do
+      result = GraphQL::Types::Relay::NodesField
+    end
+
+    assert_equal GraphQL::Types::Relay::DeprecatedNodesField, result
+    assert_equal "", stdout
+    expected_warning = "NodesField is deprecated, use `include GraphQL::Types::Relay::HasNodesField` instead.
+(referenced from /Users/rmosolgo/code/graphql-ruby/spec/graphql/types/relay/has_node_field_spec.rb:35:in `block (3 levels) in <top (required)>')
+"
+    assert_equal expected_warning, stderr
+  end
 end
