9a4629520e44991ed817668145b763b3cb1b161b
Robert Mosolgo <rdmosolgo@gmail.com>
2022-03-21 09:15:30 -0400

    Don't use a magic value, use an equality check instead

--- lib/graphql/schema/field/scope_extension.rb
+++ lib/graphql/schema/field/scope_extension.rb
@@ -11,12 +11,10 @@ module GraphQL
             ret_type = @field.type.unwrap
             if ret_type.respond_to?(:scope_items)
               scoped_items = ret_type.scope_items(value, context)
-              if scoped_items == Schema::Member::Scoped::NOT_SCOPED
-                value
-              else
+              if !scoped_items.eql?(value) # A different object was returned
                 context.namespace(:interpreter)[:was_scoped] = true
-                scoped_items
               end
+              scoped_items
             else
               value
             end
--- lib/graphql/schema/member/scoped.rb
+++ lib/graphql/schema/member/scoped.rb
@@ -4,11 +4,6 @@ module GraphQL
   class Schema
     class Member
       module Scoped
-        class NotScoped
-        end
-        # This is returned by default, telling the runtime that no authorization was performed.
-        NOT_SCOPED = NotScoped.new
-
         # This is called when a field has `scope: true`.
         # The field's return type class receives this call.
         #
@@ -18,7 +13,7 @@ module GraphQL
         # @param context [GraphQL::Query::Context]
         # @return [Object] Another list-like object, scoped to the current context
         def scope_items(items, context)
-          NOT_SCOPED
+          items
         end
       end
     end
