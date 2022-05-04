4e9e3f95c1ff26cccdff1d3fd11ba7f4cf7b1c4e
Daniel Dao <daniel.dao@chime.com>
2022-05-04 13:21:30 -0700

    🐛  Move it back to being a bit safer.
    Co-authored-by: George Xu <george.xu@chime.com>

--- lib/graphql/schema/field.rb
+++ lib/graphql/schema/field.rb
@@ -643,13 +643,7 @@ module GraphQL
               inner_object = obj.object
 
               if defined?(@hash_key)
-<<<<<<< HEAD
                 inner_object[@hash_key].nil? ? inner_object[@hash_key_str] : inner_object[@hash_key]
-=======
-                inner_object.fetch(@hash_key) {
-                  inner_object[@hash_key_str]
-                }
->>>>>>> parent of 059a5b7ea (🐛  Move the `hash_keys` call into `Hash` check)
               elsif @dig_keys
                 inner_object.dig(*@dig_keys)
               elsif obj.respond_to?(resolver_method)
@@ -662,10 +656,16 @@ module GraphQL
                   obj.public_send(resolver_method)
                 end
               elsif inner_object.is_a?(Hash)
-                if inner_object.key?(@method_sym)
-                  inner_object[@method_sym]
+                if defined?(@hash_key)
+                  inner_object.fetch(@hash_key) {
+                    inner_object[@hash_key_str]
+                  }
                 else
-                  inner_object[@method_str]
+                  if inner_object.key?(@method_sym)
+                    inner_object[@method_sym]
+                  else
+                    inner_object[@method_str]
+                  end
                 end
               elsif inner_object.respond_to?(@method_sym)
                 method_to_call = @method_sym
