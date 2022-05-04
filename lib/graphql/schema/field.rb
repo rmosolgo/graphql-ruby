30711e0ace73203ed7dc13f756097dcb60cf193d
Daniel Dao <daniel.dao@chime.com>
2022-05-04 13:09:53 -0700

    🐛  move it back and use something safer.
    Co-authored-by: georgexu22@chime.com

--- lib/graphql/schema/field.rb
+++ lib/graphql/schema/field.rb
@@ -642,6 +642,8 @@ module GraphQL
 
               inner_object = obj.object
 
+              if defined?(@hash_key)
+                inner_object[@hash_key].nil? ? inner_object[@hash_key_str] : inner_object[@hash_key]
               elsif @dig_keys
                 inner_object.dig(*@dig_keys)
               elsif obj.respond_to?(resolver_method)
@@ -654,16 +656,10 @@ module GraphQL
                   obj.public_send(resolver_method)
                 end
               elsif inner_object.is_a?(Hash)
-                if defined?(@hash_key)
-                  inner_object.fetch(@hash_key) {
-                    inner_object[@hash_key_str]
-                  }
+                if inner_object.key?(@method_sym)
+                  inner_object[@method_sym]
                 else
-                  if inner_object.key?(@method_sym)
-                    inner_object[@method_sym]
-                  else
-                    inner_object[@method_str]
-                  end
+                  inner_object[@method_str]
                 end
               elsif inner_object.respond_to?(@method_sym)
                 method_to_call = @method_sym
