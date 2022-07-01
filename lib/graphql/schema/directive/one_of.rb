df3b520a4297e4954b2e9a93a99431803ed8cf5e
Erik Kessler <ekessler@salsify.com>
2022-07-01 12:06:31 -0400

    Validate fields are nullable without a default

--- lib/graphql/schema/directive/one_of.rb
+++ lib/graphql/schema/directive/one_of.rb
@@ -10,6 +10,47 @@ module GraphQL
         )
 
         default_directive true
+
+        module OneOfInputObjectSchema
+          def argument(*args, **kwargs, &block)
+            kwargs[:required] = false if kwargs[:required].nil?
+
+            argument = super(*args, **kwargs, &block)
+            validate_argument_non_null(argument)
+            validate_argument_no_default(argument)
+
+            argument
+          end
+
+          private
+
+          def validate_argument_non_null(argument)
+            return if argument.type.is_a?(Schema::LateBoundType)
+            return unless argument.type.kind.non_null?
+
+            raise ArgumentError, "Argument '#{graphql_name}.#{argument.name}' must be nullable " \
+                                 "as it is part of a OneOf Type."
+          end
+
+          def validate_argument_no_default(argument)
+            return unless argument.default_value?
+
+            raise ArgumentError, "Argument '#{graphql_name}.#{argument.name}' cannot have a default value " \
+                                 "as it is part of a OneOf Type."
+          end
+        end
+
+        module OneOfInputObjectExecution
+        end
+
+        def initialize(target, **options)
+          if !target.ancestors.include?(OneOfInputObjectExecution)
+            target.extend(OneOfInputObjectSchema)
+            target.include(OneOfInputObjectExecution)
+          end
+
+          super
+        end
       end
     end
   end
