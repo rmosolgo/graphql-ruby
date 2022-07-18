b7b6dc2c64c1348e75a104d29473a60b21c8b908
merten <merten.falk@crossengage.io>
2022-07-18 13:08:10 +0200

    Fix condition check order to most probable option first

--- lib/graphql/schema/field.rb
+++ lib/graphql/schema/field.rb
@@ -666,10 +666,10 @@ module GraphQL
               inner_object = obj.object
 
               if defined?(@hash_key)
-                hash_value = if inner_object.respond_to?(@hash_key)
-                  inner_object.public_send(@hash_key)
-                elsif inner_object.is_a?(Hash)
+                hash_value = if inner_object.is_a?(Hash)
                   inner_object.key?(@hash_key) ? inner_object[@hash_key] : inner_object[@hash_key_str]
+                elsif inner_object.respond_to?(@hash_key)
+                  inner_object.public_send(@hash_key)
                 else
                   nil
                 end
