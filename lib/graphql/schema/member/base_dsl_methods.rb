67478904458948c29bfa28163b5d8dec82362cc7
Jean Boussier <jean.boussier@gmail.com>
2023-01-16 14:21:21 +0100

    Revert @instrospection eager definition
    It breaks a large part of the test suite.

--- lib/graphql/schema/member/base_dsl_methods.rb
+++ lib/graphql/schema/member/base_dsl_methods.rb
@@ -27,7 +27,7 @@ module GraphQL
         end
 
         def overridden_graphql_name
-          @graphql_name
+          defined?(@graphql_name) ? @graphql_name : nil
         end
 
         # Just a convenience method to point out that people should use graphql_name instead
@@ -47,8 +47,10 @@ module GraphQL
         def description(new_description = nil)
           if new_description
             @description = new_description
-          else
+          elsif defined?(@description)
             @description
+          else
+            nil
           end
         end
 
@@ -69,8 +71,10 @@ module GraphQL
         def introspection(new_introspection = nil)
           if !new_introspection.nil?
             @introspection = new_introspection
-          else
+          elsif defined?(@introspection)
             @introspection
+          else
+            false
           end
         end
 
@@ -83,8 +87,10 @@ module GraphQL
         def mutation(mutation_class = nil)
           if mutation_class
             @mutation = mutation_class
-          else
+          elsif defined?(@mutation)
             @mutation
+          else
+            nil
           end
         end
 
@@ -111,19 +117,6 @@ module GraphQL
 
         def authorized?(object, context)
           true
-        end
-
-        private
-
-        def inherited(subclass)
-          super
-          subclass.class_eval do
-            @graphql_name = nil
-            @default_graphql_name = nil
-            @mutation = nil
-            @introspection = false
-            @description = nil
-          end
         end
       end
     end
