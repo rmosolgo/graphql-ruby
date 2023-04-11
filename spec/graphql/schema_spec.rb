e9b9de2d3e8b8d06a4458cb46fb27d6688a77ab1
Robert Mosolgo <rdmosolgo@gmail.com>
2023-04-11 12:06:08 -0400

    update test

--- spec/graphql/schema_spec.rb
+++ spec/graphql/schema_spec.rb
@@ -150,8 +150,12 @@ describe GraphQL::Schema do
       assert_equal base_schema.query_analyzers + [query_analyzer], schema.query_analyzers
       assert_equal base_schema.multiplex_analyzers + [multiplex_analyzer], schema.multiplex_analyzers
       assert_equal [GraphQL::Backtrace, GraphQL::Subscriptions::ActionCableSubscriptions, CustomSubscriptions], schema.plugins.map(&:first)
-      assert_equal [GraphQL::Tracing::DataDogTracing, GraphQL::Backtrace::Tracer], base_schema.tracers
-      assert_equal [GraphQL::Tracing::DataDogTracing, GraphQL::Backtrace::Tracer, GraphQL::Tracing::NewRelicTracing], schema.tracers
+      assert_equal [GraphQL::Tracing::DataDogTracing], base_schema.tracers
+      assert_includes base_schema.trace_class.ancestors, GraphQL::Tracing::LegacyTrace
+      assert_equal [GraphQL::Tracing::DataDogTracing, GraphQL::Tracing::NewRelicTracing], schema.tracers
+      assert_includes schema.trace_class.ancestors, GraphQL::Tracing::LegacyTrace
+
+
       assert_instance_of CustomSubscriptions, schema.subscriptions
     end
   end
