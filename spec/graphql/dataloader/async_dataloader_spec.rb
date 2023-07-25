c106bd1bb8fe6295c3557a86e9ec57a856ac85b8
Robert Mosolgo <rdmosolgo@gmail.com>
2023-07-25 14:28:50 -0400

    Try fiber_scheduler on GH actions instead

--- spec/graphql/dataloader/async_dataloader_spec.rb
+++ spec/graphql/dataloader/async_dataloader_spec.rb
@@ -249,7 +249,7 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
       include AsyncDataloaderAssertions
     end
 
-    if RUBY_ENGINE == "ruby"
+    if !ENV["GITHUB_ACTIONS"] && RUBY_ENGINE == "ruby"
       describe "With libev_scheduler" do
         require "libev_scheduler"
         let(:scheduler_class) { Libev::Scheduler }
@@ -265,7 +265,7 @@ if Fiber.respond_to?(:scheduler) # Ruby 3+
       include AsyncDataloaderAssertions
     end
 
-    if !ENV["GITHUB_ACTIONS"] && RUBY_VERSION < "3.2.0"
+    if RUBY_VERSION < "3.2.0"
       describe "with fiber_scheduler" do
         require "fiber_scheduler"
         let(:scheduler_class) { FiberScheduler }
