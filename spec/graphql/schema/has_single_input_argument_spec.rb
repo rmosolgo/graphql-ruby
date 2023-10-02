1a93dccc4fed6535809bcdb0d04163471178dec7
tueda0703 <ueda.takeshi@moneyforward.co.jp>
2023-10-02 16:56:23 +0900

    add mutation for test

--- spec/graphql/schema/has_single_input_argument_spec.rb
+++ spec/graphql/schema/has_single_input_argument_spec.rb
@@ -40,49 +40,141 @@ describe GraphQL::Schema::HasSingleInputArgument do
   end
 
   describe "execution" do
-    after do
-      Jazz::Models.reset
+    module HasSingleArgument
+      class TestInput < GraphQL::Schema::InputObject
+        argument :name, String
+      end
+
+      class NoArgumentMutation < GraphQL::Schema::Mutation
+        include GraphQL::Schema::HasSingleInputArgument
+
+        null true
+
+        field :name, String, null: false
+        def resolve
+          { name: "name" }
+        end
+      end
+
+      class InputObjectMutation < GraphQL::Schema::Mutation
+        include GraphQL::Schema::HasSingleInputArgument
+
+        argument :test, TestInput
+
+        field :name, String
+
+        def resolve(test:)
+          { name: test[:name] }
+        end
+      end
+
+      class SupportExtrasMutation < GraphQL::Schema::Mutation
+        include GraphQL::Schema::HasSingleInputArgument
+
+        argument :name, String
+        extras [:ast_node]
+
+        field :node_class, String
+        field :name, String
+
+        def resolve(name:, ast_node:)
+          {
+            name: name,
+            node_class: ast_node.class.name
+          }
+        end
+      end
+
+      class SupportFieldExtrasMutation < GraphQL::Schema::Mutation
+        include GraphQL::Schema::HasSingleInputArgument
+
+        null true
+
+        argument :name, String, required: false
+
+        field :lookahead_class, String, null: false
+        field :name, String
+
+        def resolve(name: nil, lookahead:)
+          {
+            name: name,
+            lookahead_class: lookahead.class.name
+          }
+        end
+      end
+
+      class CanStripOutExtrasMutation < GraphQL::Schema::Mutation
+        include GraphQL::Schema::HasSingleInputArgument
+
+        extras [:lookahead]
+
+        field :name, String, null: false
+
+        def resolve_with_support(lookahead: , **rest)
+          context[:has_lookahead] = !!lookahead
+          super(**rest)
+        end
+
+        def authorized?
+          true
+        end
+
+        def resolve
+          {
+            name: 'name',
+          }
+        end
+
+      end
+      class Mutation < GraphQL::Schema::Object
+        field_class GraphQL::Schema::Field
+
+        field :no_argument, mutation: NoArgumentMutation
+        field :input_object, mutation: InputObjectMutation
+        field :support_extras, mutation: SupportExtrasMutation
+        field :support_field_extras, mutation: SupportFieldExtrasMutation, extras: [:lookahead]
+        field :can_strip_out_extras, mutation: CanStripOutExtrasMutation
+      end
+      class Schema < GraphQL::Schema
+        mutation(Mutation)
+      end
     end
 
     it "works with no arguments" do
-      res = Jazz::Schema.execute <<-GRAPHQL
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL
       mutation {
-        addSitar(input: {}) {
-          instrument {
-            name
-          }
+        noArgument(input: {}) {
+          name
         }
       }
       GRAPHQL
-      assert_equal "Sitar", res["data"]["addSitar"]["instrument"]["name"]
+      assert_equal "name", res["data"]["noArgument"]["name"]
     end
 
     it "works with InputObject arguments" do
-      res = Jazz::Schema.execute <<-GRAPHQL
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL
       mutation {
-        addEnsembleRelay(input: { ensemble: { name: "Miles Davis Quartet" } }) {
-          ensemble {
-            name
-          }
+        inputObject(input: { test: { name: "test name" } }) {
+          name
         }
       }
       GRAPHQL
 
-      assert_equal "Miles Davis Quartet", res["data"]["addEnsembleRelay"]["ensemble"]["name"]
+      assert_equal "test name", res["data"]["inputObject"]["name"]
     end
 
     it "supports extras" do
-      res = Jazz::Schema.execute <<-GRAPHQL
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL
       mutation {
-        hasExtras(input: {}) {
+        supportExtras(input: {name: "name"}) {
           nodeClass
-          int
+          name
         }
       }
       GRAPHQL
 
-      assert_equal "GraphQL::Language::Nodes::Field", res["data"]["hasExtras"]["nodeClass"]
-      assert_nil res["data"]["hasExtras"]["int"]
+      assert_equal "GraphQL::Language::Nodes::Field", res["data"]["supportExtras"]["nodeClass"]
+      assert_equal "name", res["data"]["supportExtras"]["name"]
 
       # Also test with given args
       res = Jazz::Schema.execute <<-GRAPHQL
@@ -98,42 +190,42 @@ describe GraphQL::Schema::HasSingleInputArgument do
     end
 
     it "supports field extras" do
-      res = Jazz::Schema.execute <<-GRAPHQL
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL
       mutation {
-        hasFieldExtras(input: {}) {
+        supportFieldExtras(input: {}) {
           lookaheadClass
-          int
+          name
         }
       }
       GRAPHQL
 
-      assert_equal "GraphQL::Execution::Lookahead", res["data"]["hasFieldExtras"]["lookaheadClass"]
-      assert_nil res["data"]["hasFieldExtras"]["int"]
+      assert_equal "GraphQL::Execution::Lookahead", res["data"]["supportFieldExtras"]["lookaheadClass"]
+      assert_nil res["data"]["supportFieldExtras"]["name"]
 
       # Also test with given args
-      res = Jazz::Schema.execute <<-GRAPHQL
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL
       mutation {
-        hasFieldExtras(input: {int: 5}) {
+        supportFieldExtras(input: {name: "name"}) {
           lookaheadClass
-          int
+          name
         }
       }
       GRAPHQL
-      assert_equal "GraphQL::Execution::Lookahead", res["data"]["hasFieldExtras"]["lookaheadClass"]
-      assert_equal 5, res["data"]["hasFieldExtras"]["int"]
+      assert_equal "GraphQL::Execution::Lookahead", res["data"]["supportFieldExtras"]["lookaheadClass"]
+      assert_equal "name", res["data"]["supportFieldExtras"]["name"]
     end
 
     it "can strip out extras" do
       ctx = {}
-      res = Jazz::Schema.execute <<-GRAPHQL, context: ctx
+      res = HasSingleArgument::Schema.execute <<-GRAPHQL, context: ctx
       mutation {
-        hasExtrasStripped(input: {}) {
-          int
+        canStripOutExtras(input: {}) {
+          name
         }
       }
       GRAPHQL
       assert_equal true, ctx[:has_lookahead]
-      assert_equal 51, res["data"]["hasExtrasStripped"]["int"]
+      assert_equal "name", res["data"]["canStripOutExtras"]["name"]
     end
   end
 end
