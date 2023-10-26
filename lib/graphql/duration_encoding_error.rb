0cfb7c3f8334c86e4c6cd3b1f1071733629ccea6
Ruby Nealon <ruby@ruby.sh>
2023-10-26 18:13:41 +0900

    Fix typo in comment in DurationEncodingError

--- lib/graphql/duration_encoding_error.rb
+++ lib/graphql/duration_encoding_error.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module GraphQL
   # This error is raised when `Types::ISO8601Duration` is asked to return a value
-  # that cannot be parsed as an ISO8601-formatted duration by ctiveSupport::Duration.
+  # that cannot be parsed as an ISO8601-formatted duration by ActiveSupport::Duration.
   #
   # @see GraphQL::Types::ISO8601Duration which raises this error
   class DurationEncodingError < GraphQL::RuntimeTypeError
