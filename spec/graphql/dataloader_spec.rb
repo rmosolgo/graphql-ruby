d95ad01bb43a27f307c36da0ffb2662e888822df
Robert Mosolgo <rdmosolgo@gmail.com>
2023-12-26 14:18:11 -0500

    Better message

--- spec/graphql/dataloader_spec.rb
+++ spec/graphql/dataloader_spec.rb
@@ -1018,7 +1018,11 @@ describe GraphQL::Dataloader do
           end
           all_fibers.delete(Fiber.current)
           if schema.dataloader_class == GraphQL::Dataloader::AsyncDataloader
-            skip "TODO: AsyncDataloader leaves orphan suspended fibers :'("
+            skip <<~ERR
+              TODO: AsyncDataloader leaves orphan suspended fibers :'(
+
+                - #{all_fibers.select(&:alive?).join("\n  -")}
+            ERR
           else
             assert_equal [false], all_fibers.map(&:alive?).uniq
           end
