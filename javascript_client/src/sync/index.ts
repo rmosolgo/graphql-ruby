18cd29a6bdcbdbca021b3a44f0a855689e1083d6
Robert Mosolgo <rdmosolgo@gmail.com>
2024-01-22 10:32:37 -0500

    Add preparePersistedQueryList

--- javascript_client/src/sync/index.ts
+++ javascript_client/src/sync/index.ts
@@ -10,6 +10,7 @@ interface SyncOptions {
   relayPersistedOutput?: string,
   apolloAndroidOperationOutput?: string,
   apolloCodegenJsonOutput?: string,
+  apolloPersistedQueryManifest?: string,
   secret?: string
   url?: string,
   mode?: string,
@@ -32,6 +33,7 @@ interface SyncOptions {
  * @param {String} options.path - A glob to recursively search for `.graphql` files (Default is `./`)
  * @param {String} options.relayPersistedOutput - A path to a `.json` file from `relay-compiler`'s  `--persist-output` option
  * @param {String} options.apolloCodegenJsonOutput - A path to a `.json` file from `apollo client:codegen ... --type json`
+ * @param {String} options.apolloPersistedQueryManifest - A path to a `.json` file from `generate-persisted-query-manifest`
  * @param {String} options.secret - HMAC-SHA256 key which must match the server secret (default is no encryption)
  * @param {String} options.url - Target URL for sending prepared queries. If omitted, then an outfile is generated without sending operations to the server.
  * @param {String} options.mode - If `"file"`, treat each file separately. If `"project"`, concatenate all files and extract each operation. If `"relay"`, treat it as relay-compiler output
