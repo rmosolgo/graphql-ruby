9d0a926adf6dda15b60fa0a603ac5ac1dd581bfa
Robert Mosolgo <rdmosolgo@gmail.com>
2024-02-01 12:08:27 -0500

    Remove Base64 backport for Ruby < 2.5

--- lib/graphql/schema/base_64_encoder.rb
+++ lib/graphql/schema/base_64_encoder.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
-
-require 'graphql/schema/base_64_bp'
-
+require "base64"
 module GraphQL
   class Schema
     # @api private
     module Base64Encoder
       def self.encode(unencoded_text, nonce: false)
-        Base64Bp.urlsafe_encode64(unencoded_text, padding: false)
+        Base64.urlsafe_encode64(unencoded_text, padding: false)
       end
 
       def self.decode(encoded_text, nonce: false)
         # urlsafe_decode64 is for forward compatibility
-        Base64Bp.urlsafe_decode64(encoded_text)
+        Base64.urlsafe_decode64(encoded_text)
       rescue ArgumentError
         raise GraphQL::ExecutionError, "Invalid input: #{encoded_text.inspect}"
       end
