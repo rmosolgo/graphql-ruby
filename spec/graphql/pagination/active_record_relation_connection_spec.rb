9752c4cec345c23deabbb3b3341c9f94ae5a3597
Oliver Chang <oliver.chang@moderntreasury.com>
2024-04-10 16:22:12 -0700

    override load_nodes

--- spec/graphql/pagination/active_record_relation_connection_spec.rb
+++ spec/graphql/pagination/active_record_relation_connection_spec.rb
@@ -172,10 +172,9 @@ if testing_rails?
           }
         }")
       end
-      # This currently runs one query to load the nodes, then another one to count _just beyond_ the nodes.
-      # A better implementation would load `first + 1` nodes and use that to set `has_next_page`.
       assert_equal ["Item", "Item", "Item"], results["data"]["items"]["nodes"].map { |i| i["__typename"] }
-      assert_equal 2, log.split("\n").size, "It runs two queries -- TODO this could be better"
+      assert_equal 1, log.split("\n").size, "It runs only one query"
+      assert_equal 0, log.scan("COUNT(").size, "It runs no count query"
     end
 
     describe "already-loaded ActiveRecord relations" do
