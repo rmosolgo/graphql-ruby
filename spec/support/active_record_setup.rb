029b15e0e9a350dc5fa114e242c52f64e193e241
Robert Mosolgo <rdmosolgo@gmail.com>
2024-05-03 14:04:02 -0400

    Improve postgres test setup

--- spec/support/active_record_setup.rb
+++ spec/support/active_record_setup.rb
@@ -24,17 +24,21 @@ if testing_rails?
       ActiveRecord::Base.connection.execute("create database graphql_ruby_test;")
     end
 
-    ActiveRecord::Base.establish_connection(ar_connection_options)
+    ActiveRecord::Base.configurations = {
+      starwars: ar_connection_options,
+      starwars_replica: ar_connection_options,
+    }
+
     SequelDB = Sequel.connect("postgres://postgres:#{ENV["PGPASSWORD"]}@localhost:5432/graphql_ruby_test")
   else
     ActiveRecord::Base.configurations = {
       starwars: { adapter: "sqlite3", database: "./_test_.db" },
       starwars_replica: { adapter: "sqlite3", database: "./_test_.db" },
     }
-    ActiveRecord::Base.establish_connection(:starwars)
     SequelDB = Sequel.sqlite("./_test_.db")
   end
 
+  ActiveRecord::Base.establish_connection(:starwars)
   ActiveRecord::Schema.define do
     self.verbose = true
     create_table :bases, force: true do |t|
