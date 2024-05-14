2438bfda94f62d21f431e549550f71ab0d79759e
Victor Pellan <victor.pellan@datadoghq.com>
2024-05-14 11:36:39 +0200

    Factorised Datadog tracer code

--- lib/graphql/tracing/data_dog_trace.rb
+++ lib/graphql/tracing/data_dog_trace.rb
@@ -20,32 +20,30 @@ module GraphQL
         super
       end
 
-      def lex(query_string:)
-        @tracer.trace('graphql.lex', resource: query_string, service: @service_name, type: 'graphql') do |span|
+      def trace_helper(trace_key, resource, super_proc, **kwargs)
+        @tracer.trace("graphql.#{trace_key}", resource: resource, service: @service_name, type: 'graphql') do |span|
+          yield(span) if block_given?
           if @has_prepare_span
-            prepare_span('lex', {query_string: query_string}, span)
+            prepare_span(trace_key, kwargs, span)
           end
-          super
+          # caller method's super
+          super_proc.call
         end
       end
 
+      def lex(query_string:)
+        trace_helper('lex', query_string, proc { super }, query_string: query_string)
+      end
+
       def parse(query_string:)
-        @tracer.trace('graphql.parse', resource: query_string, service: @service_name, type: 'graphql') do |span|
+        trace_helper('parse', query_string, proc { super }, query_string: query_string) do |span|
           span.set_tag('graphql.source', query_string)
-          if @has_prepare_span
-            prepare_span('parse', {query_string: query_string}, span)
-          end
-          super
         end
       end
 
       def validate(query:, validate:)
-        @tracer.trace('graphql.validate', resource: query.selected_operation_name, service: @service_name, type: 'graphql') do |span|
+        trace_helper('validate', query.selected_operation_name, proc { super }, query: query, validate: validate) do |span|
           span.set_tag('graphql.source', query.query_string)
-          if @has_prepare_span
-            prepare_span('validate', {query: query, validate: validate}, span)
-          end
-          super
         end
       end
       
@@ -57,21 +55,11 @@ module GraphQL
         else
           operations
         end
-        @tracer.trace('graphql.analyze_multiplex', resource: resource, service: @service_name, type: 'graphql') do |span|
-          if @has_prepare_span
-            prepare_span('analyze_multiplex', {multiplex: multiplex}, span)
-          end
-          super
-        end
+        trace_helper('analyze_multiplex', resource, proc { super }, multiplex: multiplex)
       end
 
       def analyze_query(query:)
-        @tracer.trace('graphql.analyze', resource: query.query_string, service: @service_name, type: 'graphql') do |span|
-          if @has_prepare_span
-            prepare_span('analyze_query', {query: query}, span)
-          end
-          super
-        end
+        trace_helper('analyze', query.query_string, proc { super }, query: query)
       end
 
       def execute_multiplex(multiplex:)
@@ -82,27 +70,19 @@ module GraphQL
         else
           operations
         end
-        @tracer.trace('graphql.execute_multiplex', resource: resource, service: @service_name, type: 'graphql') do |span|
+        trace_helper('execute_multiplex', resource, proc { super }, multiplex: multiplex) do |span|
           span.set_tag('graphql.source', "Multiplex[#{multiplex.queries.map(&:query_string).join(', ')}]")
-          if @has_prepare_span
-            prepare_span("execute_multiplex", {multiplex: multiplex}, span)
-          end
-          super
         end
       end
 
       def execute_query(query:)
-        @tracer.trace('graphql.execute', resource: query.selected_operation_name, service: @service_name, type: 'graphql') do |span|
+        trace_helper('execute', query.selected_operation_name, proc { super }, query: query) do |span|
           span.set_tag('graphql.source', query.query_string)
           span.set_tag('graphql.operation.type', query.selected_operation.operation_type)
           span.set_tag('graphql.operation.name', query.selected_operation_name) if query.selected_operation_name
           query.provided_variables.each do |key, value|
             span.set_tag("graphql.variables.#{key}", value)
           end
-          if @has_prepare_span
-            prepare_span('execute_query', {query: query}, span)
-          end
-          super
         end
       end
 
@@ -120,97 +100,64 @@ module GraphQL
             operations
           end
         end
-        @tracer.trace('graphql.execute_lazy', resource: resource, service: @service_name, type: 'graphql') do |span|
-          if @has_prepare_span
-            prepare_span('execute_lazy', {query: query, multiplex: multiplex}, span)
-          end
-          super
-        end
+        trace_helper('execute_lazy', resource, proc { super }, query: query, multiplex: multiplex)
       end
 
-      def execute_field_span(span_key, query, field, ast_node, arguments, object)
-        return_type = field.type.unwrap
+      def execute_field_span(span_key, super_proc, **kwargs)
+        return_type = kwargs[:field].type.unwrap
         trace_field = if return_type.kind.scalar? || return_type.kind.enum?
-          (field.trace.nil? && @trace_scalars) || field.trace
+          (kwargs[:field].trace.nil? && @trace_scalars) || kwargs[:field].trace
         else
           true
         end
         platform_key = if trace_field
-          @platform_key_cache[DataDogTrace].platform_field_key_cache[field]
+          @platform_key_cache[DataDogTrace].platform_field_key_cache[kwargs[:field]]
         else
           nil
         end
         if platform_key && trace_field
-          @tracer.trace("graphql.#{span_key}", resource: platform_key, service: @service_name, type: 'graphql') do |span|
-            query.provided_variables.each do |key, value|
+          trace_helper(span_key, platform_key, super_proc, **kwargs) do |span|
+            kwargs[:query].provided_variables.each do |key, value|
               span.set_tag("graphql.variables.#{key}", value)
             end
-
-            if @has_prepare_span
-              prepare_span_data = { query: query, field: field, ast_node: ast_node, arguments: arguments, object: object }
-              prepare_span(span_key, prepare_span_data, span)
-            end
-            yield
           end
         else
-          yield
+          super_proc.call
         end
       end
 
-      def execute_field(query:, field:, ast_node:, arguments:, object:)
-        execute_field_span('resolve', query, field, ast_node, arguments, object) do
-          super(query: query, field: field, ast_node: ast_node, arguments: arguments, object: object)
-        end
+      def execute_field(**kwargs)
+        execute_field_span('resolve', proc { super(**kwargs) }, **kwargs)
       end
 
-      def execute_field_lazy(query:, field:, ast_node:, arguments:, object:)
-        execute_field_span('resolve_lazy', query, field, ast_node, arguments, object) do
-          super(query: query, field: field, ast_node: ast_node, arguments: arguments, object: object)
-        end
+      def execute_field_lazy(**kwargs)
+        execute_field_span('resolve_lazy', proc { super(**kwargs) }, **kwargs)
       end
 
-      def authorized_span(span_key, object, type, query)
-        platform_key = @platform_key_cache[DataDogTrace].platform_authorized_key_cache[type]
-        @tracer.trace("graphql.#{span_key}", resource: platform_key, service: @service_name, type: 'graphql') do |span|
-          if @has_prepare_span
-            prepare_span(span_key, {object: object, type: type, query: query}, span)
-          end
-          yield
-        end
+      def authorized_span(span_key, super_proc, **kwargs)
+        platform_key = @platform_key_cache[DataDogTrace].platform_authorized_key_cache[kwargs[:type]]
+        trace_helper(span_key, platform_key, super_proc, **kwargs)
       end
 
-      def authorized(query:, type:, object:)
-        authorized_span('authorized', object, type, query) do
-          super(query: query, type: type, object: object)
-        end
+      def authorized(**kwargs)
+        authorized_span('authorized', proc { super(**kwargs) }, **kwargs)
       end
 
-      def authorized_lazy(object:, type:, query:)
-        authorized_span('authorized_lazy', object, type, query) do
-          super(query: query, type: type, object: object)
-        end
+      def authorized_lazy(**kwargs)
+        authorized_span('authorized_lazy', proc { super(**kwargs) }, **kwargs)
       end
 
-      def resolve_type_span(span_key, object, type, query)
-        platform_key = @platform_key_cache[DataDogTrace].platform_resolve_type_key_cache[type]
-        @tracer.trace("graphql.#{span_key}", resource: platform_key, service: @service_name, type: 'graphql') do |span|
-          if @has_prepare_span
-            prepare_span(span_key, {object: object, type: type, query: query}, span)
-          end
-          yield
-        end
+      def resolve_type_span(span_key, super_proc, **kwargs)
+        platform_key = @platform_key_cache[DataDogTrace].platform_resolve_type_key_cache[kwargs[:type]]
+        trace_helper(span_key, platform_key, super_proc, **kwargs)
       end
 
-      def resolve_type(object:, type:, query:)
-        resolve_type_span('resolve_type', object, type, query) do
-          super(object: object, query: query, type: type)
-        end
+      def resolve_type(**kwargs)
+        resolve_type_span('resolve_type', proc { super(**kwargs) }, **kwargs)
       end
 
-      def resolve_type_lazy(object:, type:, query:)
-        resolve_type_span('resolve_type_lazy', object, type, query) do
-          super(object: object, query: query, type: type)
-        end
+      def resolve_type_lazy(**kwargs)
+        resolve_type_span('resolve_type_lazy', proc { super(**kwargs) }, **kwargs)
       end
 
       include PlatformTrace
