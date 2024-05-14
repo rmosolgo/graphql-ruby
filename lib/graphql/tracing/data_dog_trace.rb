86d07fb57b1fcfa572e6682a91b116fc7896e484
Victor Pellan <victor.pellan@datadoghq.com>
2024-05-14 15:10:22 +0200

    Datadog tracer code quality improvements

--- lib/graphql/tracing/data_dog_trace.rb
+++ lib/graphql/tracing/data_dog_trace.rb
@@ -20,63 +20,38 @@ module GraphQL
         super
       end
 
-      def trace_helper(trace_key, resource, super_proc, **kwargs)
-        @tracer.trace("graphql.#{trace_key}", resource: resource, service: @service_name, type: 'graphql') do |span|
-          yield(span) if block_given?
-          if @has_prepare_span
-            prepare_span(trace_key, kwargs, span)
-          end
-          # caller method's super
-          super_proc.call
-        end
-      end
-
       def lex(query_string:)
-        trace_helper('lex', query_string, proc { super }, query_string: query_string)
+        trace(proc { super }, 'lex', query_string, query_string: query_string)
       end
 
       def parse(query_string:)
-        trace_helper('parse', query_string, proc { super }, query_string: query_string) do |span|
+        trace(proc { super }, 'parse', query_string, query_string: query_string) do |span|
           span.set_tag('graphql.source', query_string)
         end
       end
 
       def validate(query:, validate:)
-        trace_helper('validate', query.selected_operation_name, proc { super }, query: query, validate: validate) do |span|
+        trace(proc { super }, 'validate', query.selected_operation_name, query: query, validate: validate) do |span|
           span.set_tag('graphql.source', query.query_string)
         end
       end
       
       def analyze_multiplex(multiplex:)
-        operations = multiplex.queries.map(&:selected_operation_name).compact.join(', ')
-        resource = if operations.empty?
-          first_query = multiplex.queries.first
-          fallback_transaction_name(first_query && first_query.context)
-        else
-          operations
-        end
-        trace_helper('analyze_multiplex', resource, proc { super }, multiplex: multiplex)
+        trace(proc { super }, 'analyze_multiplex', multiplex_resource(multiplex), multiplex: multiplex)
       end
 
       def analyze_query(query:)
-        trace_helper('analyze', query.query_string, proc { super }, query: query)
+        trace(proc { super }, 'analyze', query.query_string, query: query)
       end
 
       def execute_multiplex(multiplex:)
-        operations = multiplex.queries.map(&:selected_operation_name).compact.join(', ')
-        resource = if operations.empty?
-          first_query = multiplex.queries.first
-          fallback_transaction_name(first_query && first_query.context)
-        else
-          operations
-        end
-        trace_helper('execute_multiplex', resource, proc { super }, multiplex: multiplex) do |span|
+        trace(proc { super }, 'execute_multiplex', multiplex_resource(multiplex), multiplex: multiplex) do |span|
           span.set_tag('graphql.source', "Multiplex[#{multiplex.queries.map(&:query_string).join(', ')}]")
         end
       end
 
       def execute_query(query:)
-        trace_helper('execute', query.selected_operation_name, proc { super }, query: query) do |span|
+        trace(proc { super }, 'execute', query.selected_operation_name, query: query) do |span|
           span.set_tag('graphql.source', query.query_string)
           span.set_tag('graphql.operation.type', query.selected_operation.operation_type)
           span.set_tag('graphql.operation.name', query.selected_operation_name) if query.selected_operation_name
@@ -90,20 +65,12 @@ module GraphQL
         resource = if query
           query.selected_operation_name || fallback_transaction_name(query.context)
         else
-          operations = multiplex && multiplex.queries.map(&:selected_operation_name).compact.join(', ')
-          if operations.nil?
-            nil
-          elsif operations.empty?
-            first_query = multiplex.queries.first
-            fallback_transaction_name(first_query && first_query.context)
-          else
-            operations
-          end
+          multiplex_resource(multiplex)
         end
-        trace_helper('execute_lazy', resource, proc { super }, query: query, multiplex: multiplex)
+        trace(proc { super }, 'execute_lazy', resource, query: query, multiplex: multiplex)
       end
 
-      def execute_field_span(span_key, super_proc, **kwargs)
+      def execute_field_span(callable, span_key, **kwargs)
         return_type = kwargs[:field].type.unwrap
         trace_field = if return_type.kind.scalar? || return_type.kind.enum?
           (kwargs[:field].trace.nil? && @trace_scalars) || kwargs[:field].trace
@@ -115,49 +82,50 @@ module GraphQL
         else
           nil
         end
+        
         if platform_key && trace_field
-          trace_helper(span_key, platform_key, super_proc, **kwargs) do |span|
+          trace(callable, span_key, platform_key, **kwargs) do |span|
             kwargs[:query].provided_variables.each do |key, value|
               span.set_tag("graphql.variables.#{key}", value)
             end
           end
         else
-          super_proc.call
+          callable.call
         end
       end
 
       def execute_field(**kwargs)
-        execute_field_span('resolve', proc { super(**kwargs) }, **kwargs)
+        execute_field_span(proc { super(**kwargs) }, 'resolve', **kwargs)
       end
 
       def execute_field_lazy(**kwargs)
-        execute_field_span('resolve_lazy', proc { super(**kwargs) }, **kwargs)
+        execute_field_span(proc { super(**kwargs) }, 'resolve_lazy', **kwargs)
       end
 
-      def authorized_span(span_key, super_proc, **kwargs)
+      def authorized_span(callable, span_key, **kwargs)
         platform_key = @platform_key_cache[DataDogTrace].platform_authorized_key_cache[kwargs[:type]]
-        trace_helper(span_key, platform_key, super_proc, **kwargs)
+        trace(callable, span_key, platform_key, **kwargs)
       end
 
       def authorized(**kwargs)
-        authorized_span('authorized', proc { super(**kwargs) }, **kwargs)
+        authorized_span(proc { super(**kwargs) }, 'authorized', **kwargs)
       end
 
       def authorized_lazy(**kwargs)
-        authorized_span('authorized_lazy', proc { super(**kwargs) }, **kwargs)
+        authorized_span(proc { super(**kwargs) }, 'authorized_lazy', **kwargs)
       end
 
-      def resolve_type_span(span_key, super_proc, **kwargs)
+      def resolve_type_span(callable, span_key, **kwargs)
         platform_key = @platform_key_cache[DataDogTrace].platform_resolve_type_key_cache[kwargs[:type]]
-        trace_helper(span_key, platform_key, super_proc, **kwargs)
+        trace(callable, span_key, platform_key, **kwargs)
       end
 
       def resolve_type(**kwargs)
-        resolve_type_span('resolve_type', proc { super(**kwargs) }, **kwargs)
+        resolve_type_span(proc { super(**kwargs) }, 'resolve_type', **kwargs)
       end
 
       def resolve_type_lazy(**kwargs)
-        resolve_type_span('resolve_type_lazy', proc { super(**kwargs) }, **kwargs)
+        resolve_type_span(proc { super(**kwargs) }, 'resolve_type_lazy', **kwargs)
       end
 
       include PlatformTrace
@@ -179,6 +147,31 @@ module GraphQL
 
       def platform_resolve_type_key(type)
         "#{type.graphql_name}.resolve_type"
+      end
+
+      private
+
+      def trace(callable, trace_key, resource, **kwargs)
+        @tracer.trace("graphql.#{trace_key}", resource: resource, service: @service_name, type: 'graphql') do |span|
+          yield(span) if block_given?
+
+          if @has_prepare_span
+            prepare_span(trace_key, kwargs, span)
+          end
+
+          callable.call
+        end
+      end
+
+      def multiplex_resource(multiplex)
+        return nil unless multiplex
+        operations = multiplex.queries.map(&:selected_operation_name).compact.join(', ')
+        if operations.empty?
+          first_query = multiplex.queries.first
+          fallback_transaction_name(first_query && first_query.context)
+        else
+          operations
+        end
       end
     end
   end
