4b0efda38d955515581665d0de61c1ee16a280a5
Robert Mosolgo <rdmosolgo@gmail.com>
2024-06-18 15:05:39 -0400

    fix caching

--- lib/graphql/execution/interpreter/gather_selections.rb
+++ lib/graphql/execution/interpreter/gather_selections.rb
@@ -5,12 +5,9 @@ module GraphQL
       class GatherSelections
         def initialize(query)
           @query = query
-          default_selections_on_node = Hash.new { |h, k| h[k] = [nil, nil, {}] }.compare_by_identity
-          @selections_for_result = Hash.new { |h, graphql_result_hash|
-            selections_on_node = default_selections_on_node.dup
-            selections = graphql_result_hash.graphql_selections
-            build_cached_selections(selections, nil, selections_on_node)
-            h[graphql_result_hash] = selections_on_node
+          @selections_by_node = Hash.new { |h, ast_node|
+          # TODO dedicated class for storage
+            h[ast_node] = [nil, nil, {}, []]
           }.compare_by_identity
           @runtime_directive_names = []
           noop_resolve_owner = GraphQL::Schema::Directive.singleton_class
@@ -23,12 +20,15 @@ module GraphQL
         end
 
         def each_gathered_selections(graphql_result_hash)
-          selections_on_node = @selections_for_result[graphql_result_hash]
           object = graphql_result_hash.graphql_application_value
           type = graphql_result_hash.graphql_result_type
+          selections = graphql_result_hash.graphql_selections
+          root_selections = {}
+          all_selections = [[nil, nil, root_selections, nil]]
+          build_cached_selections(selections, root_selections, all_selections)
           single_selection = nil
           has_merged_selections = false
-          selections_on_node.each do |node, (type_condition, directives, selections)|
+          all_selections.each do |(type_condition, directives, selections, _child_selections)|
             if type_condition
               type_defn = @query.get_type(type_condition.name)
               pt = @query.warden.possible_types(type_defn)
@@ -73,37 +73,38 @@ module GraphQL
 
         private
 
-        def build_cached_selections(ast_selections, gathered_selections, selections_by_node)
-          if gathered_selections.nil?
-            _type_cond, _dirs, gathered_selections = selections_by_node[nil]
-          end
-
+        def build_cached_selections(ast_selections, gathered_selections, all_selection_groups)
           ast_selections.each do |node|
             case node
             when GraphQL::Language::Nodes::Field
               key = node.alias || node.name
               if node.directives.any?
-                next_sels = selections_by_node[node]
+                next_sels = @selections_by_node[node]
                 next_sels[1] = node.directives
                 next_sels[2][key] = node
+                all_selection_groups << next_sels
               else
                 gathered_selections[key] = node
               end
             when GraphQL::Language::Nodes::InlineFragment
               if node.directives.any? || node.type
-                next_sels_config = selections_by_node[node]
+                next_sels_config = @selections_by_node[node]
                 next_sels_config[0] = node.type
                 next_sels_config[1] = node.directives
-                build_cached_selections(node.selections, next_sels_config[2], selections_by_node)
+                build_cached_selections(node.selections, next_sels_config[2], next_sels_config[3])
+                all_selection_groups << next_sels_config
+                all_selection_groups.concat(next_sels_config[3])
               else
-                build_cached_selections(node.selections, gathered_selections, selections_by_node)
+                build_cached_selections(node.selections, gathered_selections, all_selection_groups)
               end
             when GraphQL::Language::Nodes::FragmentSpread
               fragment_def = @query.fragments[node.name]
-              next_sels_config = selections_by_node[node]
+              next_sels_config = @selections_by_node[node]
               next_sels_config[0] = fragment_def.type
               next_sels_config[1] = node.directives # Use directives from the spread, not the def
-              build_cached_selections(fragment_def.selections, next_sels_config[2], selections_by_node)
+              build_cached_selections(fragment_def.selections, next_sels_config[2], next_sels_config[3])
+              all_selection_groups << next_sels_config
+              all_selection_groups.concat(next_sels_config[3])
             end
           end
         end
