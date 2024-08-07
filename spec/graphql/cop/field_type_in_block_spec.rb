bb49ab3da7fe1110161beaf96276dbe9c565caac
Robert Mosolgo <rdmosolgo@gmail.com>
2024-08-07 15:51:46 -0400

    Add Rubocop for moving field types to blocks

---
+++ spec/graphql/cop/field_type_in_block_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+describe "GraphQL::Cop::FieldTypeInBlock" do
+  include RubocopTestHelpers
+
+  it "finds and autocorrects `null: true` field configurations" do
+    result = run_rubocop_on("spec/fixtures/cop/field_type.rb")
+    assert_equal 3, rubocop_errors(result)
+
+    assert_includes result, <<-RUBY
+  field :current_account, Types::Account, null: false, description: "The account of the current viewer"
+                          ^^^^^^^^^^^^^^
+    RUBY
+
+    assert_includes result, <<-RUBY
+  field :find_account, Types::Account do
+                       ^^^^^^^^^^^^^^
+    RUBY
+
+    assert_includes result, <<-RUBY
+  field(:all_accounts, [Types::Account, null: false]) {
+                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    RUBY
+
+    assert_rubocop_autocorrects_all("spec/fixtures/cop/field_type.rb")
+  end
+end
