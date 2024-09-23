4f16e949f8c1114ce516e44bf0fc46d95988a217
Robert Mosolgo <rdmosolgo@gmail.com>
2024-09-23 14:33:39 -0400

    Fix FieldTypeInBlock for list types

--- spec/fixtures/cop/field_type_array_corrected.rb
+++ spec/fixtures/cop/field_type_array_corrected.rb
@@ -1,6 +1,7 @@
 class Types::FooType < Types::BaseObject
   field :other, [String]
-  field :bar, [Thing], null: false do
+  field :bar, null: false do
+    type [Thing]
     argument :baz, String
   end
 end
