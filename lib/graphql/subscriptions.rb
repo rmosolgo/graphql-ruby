8fb977ad01e3851251f18e058f7abc0b71740527
Robert Mosolgo <rdmosolgo@gmail.com>
2024-12-05 13:02:30 -0500

    Address some possible race conditions, add more debugging

--- lib/graphql/subscriptions.rb
+++ lib/graphql/subscriptions.rb
@@ -89,6 +89,8 @@ module GraphQL
         scope: scope,
         context: context,
       )
+      # TODO remove this debugging
+      puts "[trigger-event] topic: #{event.topic} / fingerprint: #{event.fingerprint}"
       execute_all(event, object)
     end
 
