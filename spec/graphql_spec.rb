3d1657fe3c2e1740238e413e6b15323b4802ef79
Robert Mosolgo <rdmosolgo@gmail.com>
2025-01-03 13:56:16 -0500

    Fix quotes

--- spec/graphql_spec.rb
+++ spec/graphql_spec.rb
@@ -4,7 +4,7 @@ require "open3"
 
 describe GraphQL do
   it "loads without warnings" do
-    stderr_and_stdout, _status = Open3.capture2e(%|ruby -Ilib -e "require 'bundler/inline'; gemfile(true) { source("https://rubygems.org"); gem('fiber-storage'); gem('graphql', path: './') }; GraphQL.eager_load!"|)
+    stderr_and_stdout, _status = Open3.capture2e(%|ruby -Ilib -e "require 'bundler/inline'; gemfile(true) { source('https://rubygems.org'); gem('fiber-storage'); gem('graphql', path: './') }; GraphQL.eager_load!"|)
     puts stderr_and_stdout
     assert_equal "", stderr_and_stdout
   end
