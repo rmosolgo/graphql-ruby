638cfe6fb014230fff505e6cdf778aae5e65d7dc
Robert Mosolgo <rdmosolgo@gmail.com>
2025-02-06 13:01:50 -0500

    Multiply seconds float to support old rubies

--- lib/graphql/tracing/perfetto_trace.rb
+++ lib/graphql/tracing/perfetto_trace.rb
@@ -495,7 +495,7 @@ module GraphQL
       private
 
       def ts
-        Process.clock_gettime(:CLOCK_MONOTONIC, :nanosecond)
+        Process.clock_gettime(:CLOCK_MONOTONIC) * 1_000_000_000
       end
 
       def tid
