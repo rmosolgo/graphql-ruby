b106782f56fb3e42e1988b7088ace663bc934532
Robert Mosolgo <rdmosolgo@gmail.com>
2025-02-12 17:25:40 -0500

    Undo some interning for snapshot json

--- spec/support/perfetto_snapshot.rb
+++ spec/support/perfetto_snapshot.rb
@@ -5,16 +5,19 @@ module PerfettoSnapshot
     snapshot_dir = prev_file + "/snapshots"
     snapshot_path = "#{snapshot_dir}/#{snapshot_name}"
 
+    iid_table = { "debugAnnotationNames" => {}, "eventNames" => {}, "eventCategories" => {}, "debugAnnotationStringValues" => {} }
+    build_intern_map(data, iid_table)
+
     if ENV["UPDATE_PERFETTO"]
       puts "Updating PerfettoTrace snapshot: #{snapshot_path.inspect}"
-      snapshot_json = convert_to_snapshot(data)
+      snapshot_json = convert_to_snapshot(data, iid_table)
       FileUtils.mkdir_p(snapshot_dir)
       File.write(snapshot_path, JSON.pretty_generate(snapshot_json))
     elsif !File.exist?(snapshot_path)
       raise "Snapshot file not found: #{snapshot_path.inspect}"
     else
       snapshot_data = JSON.parse(File.read(snapshot_path))
-      cleaned_data = convert_to_snapshot(data)
+      cleaned_data = convert_to_snapshot(data, iid_table)
       deep_snap_match(snapshot_data, cleaned_data, [])
     end
   end
@@ -55,13 +58,13 @@ module PerfettoSnapshot
     end
   end
 
-  BASE64_PATTERN = /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/
+  BASE64_PATTERN = /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?\n?$/
 
   def replace_ids(str)
     str.gsub(/ #\d+/, " #1010").split(/:0x[0-9a-z]+/).first
   end
 
-  def convert_to_snapshot(value)
+  def convert_to_snapshot(value, iid_table)
     case value
     when String
       if value.match(/\D/).nil?
@@ -76,20 +79,56 @@ module PerfettoSnapshot
     when Numeric
       101010101010
     when Array
-      value.map { |v| convert_to_snapshot(v) }
+      value.map { |v| convert_to_snapshot(v, iid_table) }
     when Hash
       h2 = {}
       value.each do |k, v|
-        if k == "debugAnnotations"
+        case k
+        when "debugAnnotations"
+          v.each { |d|
+            if d.key?("nameIid")
+              d["name"] = iid_table["debugAnnotationNames"][d["nameIid"]]
+            end
+            if d.key?("stringValueIid")
+              d["stringValue"] = iid_table["debugAnnotationNames"][d["stringValueIid"]]
+            end
+          }
           v = v.sort_by { |d| d["name"] }
+        when "categoryIids"
+          h2["categories"] = v.map { |ciid| iid_table["eventCategories"][ciid] }
+        when "trackEvent"
+          if v.key?("nameIid")
+            v["name"] = iid_table["eventNames"][v["nameIid"]]
+          end
         end
-        h2[k] = convert_to_snapshot(v)
+        h2[k] = convert_to_snapshot(v, iid_table)
       end
       h2
     when true, false, nil
       value
     else
       raise ArgumentError, "Unexpected JSON value: #{value}"
+    end
+  end
+
+  def build_intern_map(data, iid_table)
+    case data
+    when Hash
+      if (id = data["internedData"])
+        id.each do |id_type, entries|
+          type_table = iid_table.fetch(id_type)
+          entries.each do |entry|
+            type_table[entry["iid"]] = entry["name"] || (entry["str"] ? Base64.decode64(entry["str"]) : nil)
+          end
+        end
+      end
+      data.each do |k, v|
+        build_intern_map(v, iid_table)
+      end
+    when Array
+      data.each { |v| build_intern_map(v, iid_table) }
+    else
+      # Done
     end
   end
 end
