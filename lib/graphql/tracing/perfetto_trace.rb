ef720875169e5f427c36edd975b6535f61a134cb
Robert Mosolgo <rdmosolgo@gmail.com>
2025-02-21 13:31:49 -0500

    Try not calling save_trace

--- lib/graphql/tracing/perfetto_trace.rb
+++ lib/graphql/tracing/perfetto_trace.rb
@@ -195,10 +195,10 @@ module GraphQL
         unsubscribe_from_active_support_notifications
         if @save_trace_mode && m.context[:trace_mode] == @save_trace_mode
           puts "Saving trace (#{@save_trace_mode.inspect} / #{m.context[:trace_mode]})"
-          duration_ms = (Time.now.to_f - @begin_time.to_f) * 1000
-          data = SecureRandom.bytes(100)
+          _duration_ms = (Time.now.to_f - @begin_time.to_f) * 1000
+          _data = SecureRandom.bytes(100)
           # data = Trace.encode(Trace.new(packet: @packets))
-          m.schema.perfetto_sampler.save_trace(@operation_name, duration_ms, @begin_time, data)
+          # m.schema.perfetto_sampler.save_trace(@operation_name, duration_ms, @begin_time, data)
         end
         super
       end
