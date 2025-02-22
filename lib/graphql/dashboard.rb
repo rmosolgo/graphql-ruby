1d11253a6da1f648f9f263227d48dd08886060bf
Robert Mosolgo <rdmosolgo@gmail.com>
2025-02-22 09:34:21 -0500

    Add CSP rules

--- lib/graphql/dashboard.rb
+++ lib/graphql/dashboard.rb
@@ -15,6 +15,19 @@ module Graphql
       protect_from_forgery with: :exception
       prepend_view_path(File.join(__FILE__, "../dashboard/views"))
 
+      content_security_policy do |policy|
+        policy.default_src(:self) if policy.default_src(*policy.default_src).blank?
+        policy.connect_src(:self) if policy.connect_src(*policy.connect_src).blank?
+        policy.base_uri(:none) if policy.base_uri(*policy.base_uri).blank?
+        policy.font_src(:self) if policy.font_src(*policy.font_src).blank?
+        policy.img_src(:self, :data) if policy.img_src(*policy.img_src).blank?
+        policy.object_src(:none) if policy.object_src(*policy.object_src).blank?
+        policy.script_src(:self) if policy.script_src(*policy.script_src).blank?
+        policy.style_src(:self) if policy.style_src(*policy.style_src).blank?
+        policy.form_action(:self) if policy.form_action(*policy.form_action).blank?
+        policy.frame_ancestors(:none) if policy.frame_ancestors(*policy.frame_ancestors).blank?
+      end
+
       def schema_class
         @schema_class ||= case params[:schema]
         when Class
@@ -50,12 +63,17 @@ module Graphql
     end
 
     class StaticsController < ApplicationController
+      skip_after_action :verify_same_origin_request
       # Use an explicit list of files to avoid any chance of reading other files from disk
       STATICS = {}
 
       [
         "icon.png",
-        "header-icon.png"
+        "header-icon.png",
+        "dashboard.css",
+        "dashboard.js",
+        "bootstrap-5.3.3.min.css",
+        "bootstrap-5.3.3.min.js",
       ].each do |static_file|
         STATICS[static_file] = File.expand_path("../dashboard/statics/#{static_file}", __FILE__)
       end
@@ -65,7 +83,7 @@ module Graphql
         if (filepath = STATICS[params[:id]])
           render file: filepath
         else
-          head :no_content
+          head :not_found
         end
       end
     end
