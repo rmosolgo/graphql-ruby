6daeb90ef5ccf972c73b8e130e2f23487e3ddd75
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2025-03-29 14:52:13 -0400

    Update Int and Float coercion logic to be spec compliant
    The spec says that scalars should return query/field errors when input
    or result coercion values don't conform to the spec.
    
    This updates the lgoic to return `GraphQL::CoercionError` (execution
    errors) instead of raised `RuntimeError`s which would not result in a
    client error by default.
    
    The logic and error messages are were taken from graphql-js.
    
    Note that this is likely a breaking change for schema developers. For
    clients, it's an improvement since they might get some sort of "internal
    srever error" otherwise.

--- lib/graphql/types/float.rb
+++ lib/graphql/types/float.rb
@@ -6,11 +6,21 @@ module GraphQL
       description "Represents signed double-precision fractional values as specified by [IEEE 754](https://en.wikipedia.org/wiki/IEEE_floating_point)."
 
       def self.coerce_input(value, _ctx)
-        value.is_a?(Numeric) ? value.to_f : nil
+        if value.is_a?(Numeric)
+          value.to_f
+        else
+          raise GraphQL::CoercionError, "Float cannot represent non numeric value: #{value.inspect}"
+        end
       end
 
       def self.coerce_result(value, _ctx)
-        value.to_f
+        coerced_value = Float(value, exception: false)
+
+        if coerced_value.nil? || !coerced_value.finite?
+          raise GraphQL::CoercionError, "Float cannot represent non numeric value: #{value.inspect}"
+        end
+
+        coerced_value
       end
 
       default_scalar true
--- spec/graphql/types/int_spec.rb
+++ spec/graphql/types/int_spec.rb
@@ -13,8 +13,14 @@ describe GraphQL::Types::Int do
       assert_nil GraphQL::Types::Int.coerce_isolated_input("55")
       assert_nil GraphQL::Types::Int.coerce_isolated_input(true)
       assert_nil GraphQL::Types::Int.coerce_isolated_input(6.1)
-      assert_nil GraphQL::Types::Int.coerce_isolated_input(2**31)
-      assert_nil GraphQL::Types::Int.coerce_isolated_input(-(2**31 + 1))
+
+      assert_raises(GraphQL::CoercionError) do
+        GraphQL::Types::Int.coerce_isolated_input(2**31)
+      end
+
+      assert_raises(GraphQL::CoercionError) do
+        GraphQL::Types::Int.coerce_isolated_input(-(2**31 + 1))
+      end
     end
 
     describe "handling boundaries" do
@@ -26,21 +32,16 @@ describe GraphQL::Types::Int do
         assert_equal(-(2**31), GraphQL::Types::Int.coerce_result(-(2**31), context))
       end
 
-      it "replaces values, if configured to do so" do
-        assert_equal Dummy::Schema::MAGIC_INT_COERCE_VALUE, GraphQL::Types::Int.coerce_result(99**99, context)
-      end
-
       it "raises on values out of bounds" do
         err_ctx = GraphQL::Query.new(Dummy::Schema, "{ __typename }").context
-        assert_raises(GraphQL::IntegerEncodingError) { GraphQL::Types::Int.coerce_result(2**31, err_ctx) }
-        err = assert_raises(GraphQL::IntegerEncodingError) { GraphQL::Types::Int.coerce_result(-(2**31 + 1), err_ctx) }
-        assert_equal "Integer out of bounds: -2147483649. Consider using ID or GraphQL::Types::BigInt instead.", err.message
+        assert_raises(GraphQL::CoercionError) { GraphQL::Types::Int.coerce_result(2**31, err_ctx) }
+        err = assert_raises(GraphQL::CoercionError) { GraphQL::Types::Int.coerce_result(-(2**31 + 1), err_ctx) }
+        assert_equal "Int cannot represent non 32-bit signed integer value: -2147483649", err.message
 
-        err = assert_raises GraphQL::IntegerEncodingError do
+        err = assert_raises GraphQL::CoercionError do
           Dummy::Schema.execute("{ hugeInteger }")
         end
-        expected_err = "Integer out of bounds: 2147483648 @ hugeInteger (Query.hugeInteger). Consider using ID or GraphQL::Types::BigInt instead."
-        assert_equal expected_err, err.message
+        assert_equal "Int cannot represent non 32-bit signed integer value: 2147483648", err.message
       end
     end
   end
