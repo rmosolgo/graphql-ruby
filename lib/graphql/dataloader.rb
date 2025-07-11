7812e220ba54278ce0b4a657f3903f7a259d4f22
Robert Mosolgo <rdmosolgo@gmail.com>
2025-07-11 06:46:23 -0400

    Return NullDataloader which can be frozen

--- lib/graphql/dataloader.rb
+++ lib/graphql/dataloader.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "graphql/dataloader/flat_dataloader"
 require "graphql/dataloader/null_dataloader"
 require "graphql/dataloader/request"
 require "graphql/dataloader/request_all"
