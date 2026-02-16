87cec95cb48be95b1e8fa3a1b81866e44aaeff8c
Robert Mosolgo <rdmosolgo@gmail.com>
2026-02-16 13:26:38 -0500

    Fix script test

--- spec/graphql/migrate_execution_spec.rb
+++ spec/graphql/migrate_execution_spec.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 require "spec_helper"
+require "open3"
 
 describe "MigrateExecution" do
   it "runs as a script" do
-    result = `bin/graphql-migrate-execution`
-    assert_includes result, "graphql-migrate-execution requires a filename or path as a first argument, please pass one."
+    stderr_and_stdout, status = Open3.capture2e(%|bin/graphql-migrate-execution|)
+    assert_equal 1, status.exitstatus
+    assert_equal "graphql-migrate-execution requires a filename or path as a first argument, please pass one.\n", stderr_and_stdout
   end
 end
