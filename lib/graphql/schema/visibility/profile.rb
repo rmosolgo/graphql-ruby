638bb5e9cee5ca8d3dd3ff452eda326be7db3444
Peter Zhu <peter@peterzhu.ca>
2026-09-22 01:20:54 +0900

    Preload arguments

--- lib/graphql/schema/visibility/profile.rb
+++ lib/graphql/schema/visibility/profile.rb
@@ -277,6 +277,13 @@ module GraphQL
                 arguments(f).each do |arg|
                   argument(f, arg.graphql_name)
                 end
+
+                resolver_class = f.resolver
+                if resolver_class
+                  arguments(resolver_class).each do |arg|
+                    argument(resolver_class, arg.graphql_name)
+                  end
+                end
               end
               @schema.introspection_system.dynamic_fields.each do |f|
                 field(type_defn, f.graphql_name)
@@ -311,6 +318,18 @@ module GraphQL
           directives.each do |directive|
             arguments(directive).each do |directive_argument|
               argument(directive, directive_argument.graphql_name)
+            end
+          end
+
+          @cached_arguments.each_value do |args|
+            args.each do |arg|
+              if (loads_type = arg.loads)
+                loadable?(loads_type, @context)
+                possible_types(loads_type)
+                if loads_type.kind.union?
+                  loadable_possible_types(loads_type, @context)
+                end
+              end
             end
           end
         end
